62fbe65526205a64ad819fa8608b8f6fae90e686
Anoop M D <anoop.md1421@gmail.com>
2021-12-06 19:05:42 +0530

    feat: use graphiql theme for query editor

--- packages/grafnode-run/src/pages/_app.js
+++ packages/grafnode-run/src/pages/_app.js
@@ -4,6 +4,7 @@ import '../styles/globals.css'
 import 'tailwindcss/dist/tailwind.min.css';
 import 'react-tabs/style/react-tabs.css';
 import 'codemirror/lib/codemirror.css';
+import 'graphiql/graphiql.min.css';
 
 function MyApp({ Component, pageProps }) {
   return (
