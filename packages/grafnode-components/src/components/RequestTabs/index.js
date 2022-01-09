3037c3564e265eaf8366bd584142920c1e761b60
Anoop M D <anoop.md1421@gmail.com>
2022-01-09 22:21:03 +0530

    feat: click on new tab icon creates a new tab

--- packages/grafnode-components/src/components/RequestTabs/index.js
+++ packages/grafnode-components/src/components/RequestTabs/index.js
@@ -42,6 +42,12 @@ const RequestTabs = ({actions, dispatch, activeRequestTabId, requestTabs}) => {
     });
   };
 
+  const createNewTab = () => {
+    dispatch({
+      type: actions.ADD_NEW_HTTP_REQUEST
+    });
+  };
+
   return (
     <StyledWrapper className="mt-3 flex items-center">
       {requestTabs && requestTabs.length ? (
@@ -63,7 +69,7 @@ const RequestTabs = ({actions, dispatch, activeRequestTabId, requestTabs}) => {
               </div>
             </li>
           }) : null}
-          <li className="select-none new-tab ml-1">
+          <li className="select-none new-tab ml-1" onClick={createNewTab}>
             <div className="flex items-center">
               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"  viewBox="0 0 16 16">
                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
