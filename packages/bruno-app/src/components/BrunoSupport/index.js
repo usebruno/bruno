6ee0a3e1e80e329ccc4287d04370566adc49860b
Anusree Subash <anusreesubash@gmail.com>
2022-10-16 17:57:25 +0530

    chore: fixed lint issues

--- packages/bruno-app/src/components/BrunoSupport/index.js
+++ packages/bruno-app/src/components/BrunoSupport/index.js
@@ -5,33 +5,33 @@ import StyledWrapper from "./StyledWrapper";
 
 const BrunoSupport = ({onClose}) => {
   return (
-      <StyledWrapper>
-        <Modal
-          size="sm"
-          title={"Support"}
-          handleCancel={onClose}
-          hideFooter={true}
-        >
-          <div className="collection-options">
-            <div className="mt-2">
-              <a href="https://github.com/usebruno/bruno/issues" target="_blank" className="flex items-center">
-              <IconSpeakerphone size={18} strokeWidth={2}/><span className="label ml-2">Report Issues</span>
-              </a>
-            </div>
-            <div className="mt-2">
-              <a href="https://github.com/usebruno/bruno" target="_blank" className="flex items-center">
-                <img src="/github.svg" style={{width: "18px"}}/>
-                <span className="label ml-2">Github</span>
-              </a>
-            </div>
-            <div className="mt-2">
-              <a href="https://twitter.com/use_bruno" target="_blank" className="flex items-center">
-                <IconBrandTwitter size={18} strokeWidth={2}/><span className="label ml-2">Twitter</span>
-              </a>
-            </div>
+    <StyledWrapper>
+      <Modal
+        size="sm"
+        title={"Support"}
+        handleCancel={onClose}
+        hideFooter={true}
+      >
+        <div className="collection-options">
+          <div className="mt-2">
+            <a href="https://github.com/usebruno/bruno/issues" target="_blank" className="flex items-center">
+            <IconSpeakerphone size={18} strokeWidth={2}/><span className="label ml-2">Report Issues</span>
+            </a>
           </div>
-        </Modal>
-      </StyledWrapper>
+          <div className="mt-2">
+            <a href="https://github.com/usebruno/bruno" target="_blank" className="flex items-center">
+              <img src="/github.svg" style={{width: "18px"}}/>
+              <span className="label ml-2">Github</span>
+            </a>
+          </div>
+          <div className="mt-2">
+            <a href="https://twitter.com/use_bruno" target="_blank" className="flex items-center">
+              <IconBrandTwitter size={18} strokeWidth={2}/><span className="label ml-2">Twitter</span>
+            </a>
+          </div>
+        </div>
+      </Modal>
+    </StyledWrapper>
   );
 }
 
