ed4dd3b59e4bf8d108e31b51079a95f6fec8f48c
Sean <seanlengws@gmail.com>
2022-10-19 13:39:38 +0800

    Added state for theme

--- packages/bruno-app/src/providers/ReduxStore/slices/app.js
+++ packages/bruno-app/src/providers/ReduxStore/slices/app.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
   isDragging: false,
@@ -6,46 +6,42 @@ const initialState = {
   leftSidebarWidth: 270,
   leftMenuBarOpen: true,
   screenWidth: 500,
-  showHomePage: false
-};
+  showHomePage: false,
+  theme: "light",
+}
 
 export const appSlice = createSlice({
-  name: 'app',
+  name: "app",
   initialState,
   reducers: {
     idbConnectionReady: (state) => {
-      state.idbConnectionReady = true;
+      state.idbConnectionReady = true
     },
     toggleLeftMenuBar: (state) => {
-      state.leftMenuBarOpen = !state.leftMenuBarOpen;
-      state.leftSidebarWidth = state.leftMenuBarOpen ? 270 : 222;
+      state.leftMenuBarOpen = !state.leftMenuBarOpen
+      state.leftSidebarWidth = state.leftMenuBarOpen ? 270 : 222
     },
     refreshScreenWidth: (state) => {
-      state.screenWidth = window.innerWidth;
+      state.screenWidth = window.innerWidth
     },
     updateLeftSidebarWidth: (state, action) => {
-      state.leftSidebarWidth = action.payload.leftSidebarWidth;
+      state.leftSidebarWidth = action.payload.leftSidebarWidth
     },
     updateIsDragging: (state, action) => {
-      state.isDragging = action.payload.isDragging;
+      state.isDragging = action.payload.isDragging
+    },
+    updateTheme: (state, action) => {
+      state.theme = action.payload.theme
     },
     showHomePage: (state) => {
-      state.showHomePage = true;
+      state.showHomePage = true
     },
     hideHomePage: (state) => {
-      state.showHomePage = false;
-    }
-  }
-});
+      state.showHomePage = false
+    },
+  },
+})
 
-export const {
-  idbConnectionReady,
-  toggleLeftMenuBar,
-  refreshScreenWidth,
-  updateLeftSidebarWidth,
-  updateIsDragging,
-  showHomePage,
-  hideHomePage
-} = appSlice.actions;
+export const { idbConnectionReady, toggleLeftMenuBar, updateTheme, refreshScreenWidth, updateLeftSidebarWidth, updateIsDragging, showHomePage, hideHomePage } = appSlice.actions
 
-export default appSlice.reducer;
+export default appSlice.reducer
