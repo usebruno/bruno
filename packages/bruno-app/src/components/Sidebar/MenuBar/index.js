5ee7111816133fdba3325b7cf0c7fb0d580d0d2b
Sean <seanlengws@gmail.com>
2022-10-19 13:59:47 +0800

    Added sidebar icon for theme switcher

--- packages/bruno-app/src/components/Sidebar/MenuBar/index.js
+++ packages/bruno-app/src/components/Sidebar/MenuBar/index.js
@@ -1,36 +1,39 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { IconCode, IconFiles, IconUser, IconUsers, IconSettings, IconChevronsLeft, IconLifebuoy} from '@tabler/icons';
-import { useDispatch } from 'react-redux';
-import { toggleLeftMenuBar } from 'providers/ReduxStore/slices/app';
-import BrunoSupport from 'components/BrunoSupport';
-import { isElectron } from 'utils/common/platform';
-import StyledWrapper from './StyledWrapper';
+import React, { useState } from "react"
+import { useRouter } from "next/router"
+import { IconCode, IconFiles, IconMoon, IconChevronsLeft, IconLifebuoy } from "@tabler/icons"
+import { useDispatch } from "react-redux"
+import { toggleLeftMenuBar } from "providers/ReduxStore/slices/app"
+import { isElectron } from "utils/common/platform"
+
+import Link from "next/link"
+import StyledWrapper from "./StyledWrapper"
+import BrunoSupport from "components/BrunoSupport"
+import ThemeSwitcher from "components/ThemeSwitcher/index"
 
 const MenuBar = () => {
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const [openBrunoSupport, setOpenBrunoSupport] = useState(false);
-  const isPlatformElectron = isElectron();
+  const router = useRouter()
+  const dispatch = useDispatch()
+  const [openBrunoSupport, setOpenBrunoSupport] = useState(false)
+  const [openThemeSwitcher, setOpenThemeSwitcher] = useState(false)
+  const isPlatformElectron = isElectron()
 
   const getClassName = (menu) => {
-    return router.pathname === menu ? "active menu-item": "menu-item";
-  };
+    return router.pathname === menu ? "active menu-item" : "menu-item"
+  }
 
   return (
     <StyledWrapper className="h-full flex flex-col">
       <div className="flex flex-col">
         <Link href="/">
-          <div className={getClassName('/')}>
-            <IconCode size={28} strokeWidth={1.5}/>
+          <div className={getClassName("/")}>
+            <IconCode size={28} strokeWidth={1.5} />
           </div>
         </Link>
         {!isPlatformElectron ? (
           <Link href="/collections">
-          <div className={getClassName('/collections')}>
-            <IconFiles size={28} strokeWidth={1.5}/>
-          </div>
+            <div className={getClassName("/collections")}>
+              <IconFiles size={28} strokeWidth={1.5} />
+            </div>
           </Link>
         ) : null}
         {/* <div className="menu-item">
@@ -44,15 +47,21 @@ const MenuBar = () => {
           </div>
         </Link> */}
         <div className="menu-item">
-          <IconLifebuoy size={28} strokeWidth={1.5} onClick={() => setOpenBrunoSupport(true)}/>
+          <IconLifebuoy size={28} strokeWidth={1.5} onClick={() => setOpenBrunoSupport(true)} />
         </div>
+
+        <div className="menu-item">
+          <IconMoon size={28} strokeWidth={1.5} onClick={() => setOpenThemeSwitcher(true)} />
+        </div>
+
         <div className="menu-item" onClick={() => dispatch(toggleLeftMenuBar())}>
-          <IconChevronsLeft size={28} strokeWidth={1.5}/>
+          <IconChevronsLeft size={28} strokeWidth={1.5} />
         </div>
       </div>
-      {openBrunoSupport && <BrunoSupport onClose={() => setOpenBrunoSupport(false)}/>}
+      {openBrunoSupport && <BrunoSupport onClose={() => setOpenBrunoSupport(false)} />}
+      {openThemeSwitcher && <ThemeSwitcher onClose={() => setOpenThemeSwitcher(false)} />}
     </StyledWrapper>
-  );
-};
+  )
+}
 
-export default MenuBar;
+export default MenuBar
