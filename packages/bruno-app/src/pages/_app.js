824d6e85fd58c11d73261b4c732a5662bed35c09
Sean <seanlengws@gmail.com>
2022-10-19 10:48:42 +0800

    Added light theme and variables.scss

--- packages/bruno-app/src/pages/_app.js
+++ packages/bruno-app/src/pages/_app.js
@@ -1,38 +1,34 @@
-import { HotkeysProvider } from 'providers/Hotkeys';
-import { AuthProvider } from 'providers/Auth';
-import { AppProvider } from 'providers/App';
-import ReduxStore from 'providers/ReduxStore';
-import { Provider } from 'react-redux';
-import { Toaster } from 'react-hot-toast';
+import { HotkeysProvider } from "providers/Hotkeys";
+import { AuthProvider } from "providers/Auth";
+import { AppProvider } from "providers/App";
+import ReduxStore from "providers/ReduxStore";
+import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
 
-import '../styles/globals.css'
-import 'tailwindcss/dist/tailwind.min.css';
-import 'react-tabs/style/react-tabs.css';
-import 'codemirror/lib/codemirror.css';
-import 'graphiql/graphiql.min.css';
+import "../styles/globals.scss";
+import "tailwindcss/dist/tailwind.min.css";
+import "react-tabs/style/react-tabs.css";
+import "codemirror/lib/codemirror.css";
+import "graphiql/graphiql.min.css";
 
-import '../styles/app.scss';
+import "../styles/app.scss";
 
 function SafeHydrate({ children }) {
   return (
     <div suppressHydrationWarning>
-      {typeof window === 'undefined' ? null : children}
+      {typeof window === "undefined" ? null : children}
     </div>
-  )
+  );
 }
 
 function NoSsr({ children }) {
-  const SERVER_RENDERED = typeof navigator === 'undefined';
+  const SERVER_RENDERED = typeof navigator === "undefined";
 
-  if(SERVER_RENDERED) {
+  if (SERVER_RENDERED) {
     return null;
   }
 
-  return (
-    <>
-      {children}
-    </>
-  )
+  return <>{children}</>;
 }
 
 function MyApp({ Component, pageProps }) {
@@ -42,7 +38,7 @@ function MyApp({ Component, pageProps }) {
         <Provider store={ReduxStore}>
           <AppProvider>
             <HotkeysProvider>
-              <Toaster toastOptions={{duration: 2000}}/>
+              <Toaster toastOptions={{ duration: 2000 }} />
               <Component {...pageProps} />
             </HotkeysProvider>
           </AppProvider>
@@ -52,4 +48,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp
+export default MyApp;
