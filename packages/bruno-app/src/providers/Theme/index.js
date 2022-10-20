dfe326572700e6d51f70ee988df4aa6679accd05
Sean <seanlengws@gmail.com>
2022-10-20 21:25:05 +0800

    Added theme provider

--- packages/bruno-app/src/providers/Theme/index.js
+++ packages/bruno-app/src/providers/Theme/index.js
@@ -1,23 +1,25 @@
-import ThemeManager from "../../../public/theme/index";
-import useLocalStorage from "src/hooks/useLocalStorage/index";
+import ThemeManager from '../../../public/theme/index';
+import useLocalStorage from 'src/hooks/useLocalStorage/index';
 
-import { ThemeProvider as SCThemeProvider } from "styled-components";
-import { createContext, useContext } from "react";
+import { createContext, useContext } from 'react';
+import { ThemeProvider as SCThemeProvider } from 'styled-components';
 
-const ThemeContext = createContext();
-
+export const ThemeContext = createContext();
 export const ThemeProvider = (props) => {
-  const [storedTheme, setStoredTheme] = useLocalStorage("bruno.theme", "light");
-  const value = {
-    storedTheme,
-    setStoredTheme,
-  };
+  const [storedTheme, setStoredTheme] = useLocalStorage('bruno.theme', 'Light');
 
   const theme = ThemeManager[storedTheme];
+  const themeKeys = Object.keys(ThemeManager);
+  const value = {
+    theme,
+    themeKeys,
+    storedTheme,
+    setStoredTheme
+  };
 
   return (
     <ThemeContext.Provider value={value}>
-      <SCThemeProvider theme={theme} {...props} />
+      <SCThemeProvider theme={value} {...props} />
     </ThemeContext.Provider>
   );
 };
