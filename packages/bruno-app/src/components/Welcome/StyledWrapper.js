9d3e85e4a73cc895dfbd396c6829077ea93b2267
Sean <seanlengws@gmail.com>
2022-10-20 22:23:43 +0800

    Added dark theme for main content area

--- packages/bruno-app/src/components/Welcome/StyledWrapper.js
+++ packages/bruno-app/src/components/Welcome/StyledWrapper.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
+  color: ${(props) => props.theme.theme['primary-text']};
+
   .create-request {
     color: #737373;
     font-size: 0.75rem;
