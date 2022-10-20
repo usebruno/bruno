21d1efb925cc5e800a0daf35084cc28f0fe0169b
Sean <seanlengws@gmail.com>
2022-10-20 18:16:08 +0800

    Added use local storage hook

---
+++ packages/bruno-app/src/hooks/useLocalStorage/index.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+export default function useLocalStorage(key, defaultValue) {
+  const [value, setValue] = useState(() => {
+    try {
+      const saved = localStorage.getItem(key);
+      if (saved !== null) {
+        return JSON.parse(saved);
+      }
+      return defaultValue;
+    } catch {
+      return defaultValue;
+    }
+  });
+
+  useEffect(() => {
+    const rawValue = JSON.stringify(value);
+    localStorage.setItem(key, rawValue);
+  }, [key, value]);
+
+  return [value, setValue];
+}
