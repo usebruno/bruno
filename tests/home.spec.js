3eceb0f4190f950a6d4705a18213e0888a21026d
depapp <depa.article@gmail.com>
2022-10-24 18:56:41 +0700

    add e2e test using playwright

---
+++ tests/home.spec.js
@@ -0,0 +1,34 @@
+const { test, expect } = require('@playwright/test');
+const { HomePage } = require('../tests/pages/home.page');
+
+test.describe('bruno e2e test', () => {
+  let homePage;
+
+  test.beforeEach(async ({ page }) => {  
+    homePage = new HomePage(page);
+
+    await homePage.open();
+    await expect(page).toHaveURL('/');
+    await expect(page).toHaveTitle(/bruno/);
+  });
+
+  test('user should be able to load & use sample collection', async () => {
+    await homePage.loadSampleCollection();
+
+    await homePage.getUsers();
+    await expect(homePage.statusRequestSuccess).toBeVisible();
+
+    await homePage.getSingleUser();
+    await expect(homePage.statusRequestSuccess).toBeVisible();
+
+    await homePage.getUserNotFound();
+    await expect(homePage.statusRequestNotFound).toBeVisible();
+
+    await homePage.createUser();
+    await expect(homePage.statusRequestCreated).toBeVisible();
+
+    await homePage.updateUser();
+    await expect(homePage.statusRequestSuccess).toBeVisible();
+  });
+
+});
