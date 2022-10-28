06fcb73c64e9cb6f02243dc484bb73e7030d5ed7
depapp <depa.article@gmail.com>
2022-10-28 16:40:48 +0700

    add selector ID

--- packages/bruno-app/src/components/RequestTabs/index.js
+++ packages/bruno-app/src/components/RequestTabs/index.js
@@ -119,7 +119,7 @@ const RequestTabs = () => {
                   </div>
                 </li>
               ) : null}
-              <li className="select-none short-tab" onClick={createNewTab}>
+              <li className="select-none short-tab" id="create-new-tab" onClick={createNewTab}>
                 <div className="flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
