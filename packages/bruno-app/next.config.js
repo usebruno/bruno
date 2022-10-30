f283df2a1b6784d6f19af7b01b680e2062590521
Anoop M D <anoop.md1421@gmail.com>
2022-10-30 17:48:36 +0530

    feat: posthog telemetry

--- packages/bruno-app/next.config.js
+++ packages/bruno-app/next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    CI: process.env.CI
+  },
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
--- packages/bruno-app/src/providers/App/index.js
+++ packages/bruno-app/src/providers/App/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import useIdb from './useIdb';
+import useTelemetry from './useTelemetry';
 import useLocalCollectionTreeSync from './useLocalCollectionTreeSync';
 import { useDispatch } from 'react-redux';
 import { refreshScreenWidth } from 'providers/ReduxStore/slices/app';
@@ -9,6 +10,7 @@ export const AppContext = React.createContext();
 
 export const AppProvider = (props) => {
   useIdb();
+  useTelemetry();
   useLocalCollectionTreeSync();
 
   const dispatch = useDispatch();
