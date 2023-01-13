55f85e37282054054f4b2ab99a27232b985e0366
Anoop M D <anoop.md1421@gmail.com>
2023-01-14 04:21:57 +0530

    chore: updated bru files

--- packages/bruno-lang/src/index.js
+++ packages/bruno-lang/src/index.js
@@ -1,6 +1,47 @@
 const bruToJson = (input) => {
 };
 
+const a = {
+  "ver": "1.0",
+  "type": "http-request",
+  "name": "Send Bulk SMS",
+  "method": "GET",
+  "url": "https://api.textlocal.in/bulk_json?apiKey=secret=&numbers=919988776655&message=hello&sender=600010",
+
+  "params": [{
+    "enabled": true,
+    "key": "apiKey",
+    "value": "secret"
+  }, {
+    "enabled": true,
+    "key": "numbers",
+    "value": "998877665"
+  }, {
+    "enabled": true,
+    "key": "message",
+    "value": "hello"
+  }],
+  "headers": [{
+    "enabled": true,
+    "key": "Content-Type",
+    "value": "application/json"
+  }, {
+    "enabled": true,
+    "key": "Accept-Language",
+    "value": "en-US,en;q=0.9,hi;q=0.8"
+  }, {
+    "enabled": false,
+    "key": "transaction-id",
+    "value": "{{transactionId}}"
+  }],
+  "body": {
+    "mode": "json",
+    "json": "{\n  \"apikey: \"secret\",\n  \"numbers: \"+91998877665\",\n  \"data: {\n    \"sender: \"TXTLCL\",\n    \"messages: [{\n      \"numbers: \"+91998877665\",\n      \"message: \"Hello World\"\n    }]\n  }\n}",
+    "graphql": "{\n  launchesPast {\n    launch_site {\n      site_name\n    }\n    launch_success\n  }\n}",
+  }
+}
+
+
 
 export {
   bruToJson
