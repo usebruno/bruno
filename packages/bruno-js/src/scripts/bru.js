905f459ed03c91653cc29bd93074d7ca66a6806b
Anoop M D <anoop.md1421@gmail.com>
2023-01-29 11:12:34 +0530

    feat: support for inbuilt libraries during scripting

--- packages/bruno-js/src/scripts/bru.js
+++ packages/bruno-js/src/scripts/bru.js
@@ -1,6 +1,34 @@
+
+// Inbuilt Library Support
+const atob = require('atob');
+const btoa = require('btoa');
+const _ = require('lodash');
+const moment = require('moment');
+const uuid = require('uuid');
+const nanoid = require('nanoid');
+
 class Bru {
   constructor(environment) {
     this._environment = environment;
+  }
+
+  require(module) {
+    switch(module) {
+      case 'atob':
+        return atob;
+      case 'btoa':
+        return btoa;
+      case 'lodash':
+        return _;
+      case 'moment':
+        return moment;
+      case 'uuid':
+        return uuid;
+      case 'nanoid':
+        return nanoid;
+      default:
+        throw new Error(`Module ${module} is not supported`);
+    }
   }
 
   getEnvVar(key) {
