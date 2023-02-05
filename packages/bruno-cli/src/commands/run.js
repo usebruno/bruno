404a516fef912e00108892edb3c7dc92c6d9b3cd
Anoop M D <anoop.md1421@gmail.com>
2023-02-06 02:57:59 +0530

    chore: bruno cli accept request filename

--- packages/bruno-cli/src/commands/run.js
+++ packages/bruno-cli/src/commands/run.js
@@ -1,18 +1,43 @@
 const chalk = require('chalk');
+const {
+  CLI_EPILOGUE,
+} = require('../constants');
 
 const command = 'run';
-const desc = 'Run request';
+const desc = 'Run a request';
+
+const cmdArgs = {
+  filename: {
+    desc: 'Run a request',
+    type: 'string',
+  }
+};
+
 
 const builder = async (yargs) => {
-	try {
-		console.log(chalk.yellow('Running request'));
-	} catch (err) {
-		console.error(err);
-	}
+  yargs.options(cmdArgs).epilogue(CLI_EPILOGUE).help();
+  yargs.example('$0 filename', 'Run a request');
 };
+
+const handler = async function (argv) {
+  try {
+    if (!argv.filename) {
+      console.log(chalk.cyan('Please specify a filename'));
+      console.log(`Example: ${argv.$0} run request.bru`);
+
+      return;
+    }
+    console.log("here");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 
 module.exports = {
 	command,
 	desc,
-	builder
+	builder,
+  cmdArgs,
+  handler
 };
