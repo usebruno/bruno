b38c25ca70326f16c8251f286ffad9dc598b24ef
Anoop M D <anoop.md1421@gmail.com>
2023-02-22 19:15:37 +0530

    feat: mac signinging and notarization

---
+++ packages/bruno-electron/notarize.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const electron_notarize = require('electron-notarize');
+
+const notarize = async function (params) {
+  if (process.platform !== 'darwin') {
+    return;
+  }
+
+  let appId = 'com.usebruno.app';
+
+  let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+  if (!fs.existsSync(appPath)) {
+    console.error(`Cannot find application at: ${appPath}`);
+    return;
+  }
+
+  console.log(`Notarizing ${appId} found at ${appPath} using Apple ID ${process.env.APPLE_ID}`);
+
+  try {
+    await electron_notarize.notarize({
+        appBundleId: appId,
+        appPath: appPath,
+        appleId: process.env.APPLE_ID,
+        appleIdPassword: process.env.APPLE_ID_PASSWORD,
+        ascProvider: 'W7LPPWA48L'
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log(`Done notarizing ${appId}`);
+};
+
+module.exports = notarize;
