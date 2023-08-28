d62881fe0d8048d5fb36d94ebca2fafe9aca17d0
Anoop M D <anoop.md1421@gmail.com>
2023-08-28 23:14:40 +0530

    feat: updated electron builder config to use js based config

---
+++ packages/bruno-electron/electron-builder-config.js
@@ -0,0 +1,56 @@
+require('dotenv');
+
+const config = {
+  "appId": "com.usebruno.app",
+  "productName": "Bruno",
+  "electronVersion": "21.1.1",
+  "directories": {
+    "buildResources": "resources",
+    "output": "out"
+  },
+  "files": [
+    "**/*"
+  ],
+  "afterSign": "notarize.js",
+  "mac": {
+    "artifactName": "${name}_${version}_${arch}_${os}.${ext}",
+    "category": "public.app-category.developer-tools",
+    "target": [
+      {
+        "target": "dmg",
+        "arch": [
+          "x64",
+          "arm64"
+        ]
+      },
+      {
+        "target": "zip",
+        "arch": [
+          "x64",
+          "arm64"
+        ]
+      }
+    ],
+    "icon": "resources/icons/mac/icon.icns",
+    "hardenedRuntime": true,
+    "identity": "Anoop MD (W7LPPWA48L)",
+    "entitlements": "resources/entitlements.mac.plist",
+    "entitlementsInherit": "resources/entitlements.mac.plist"
+  },
+  "linux": {
+    "artifactName": "${name}_${version}_${arch}_linux.${ext}",
+    "icon": "resources/icons/png",
+    "target": [
+      "AppImage",
+      "deb"
+    ]
+  },
+  "win": {
+    "artifactName": "${name}_${version}_${arch}_win.${ext}",
+    "icon": "resources/icons/png",
+    "certificateFile": `${process.env.WIN_CERT_FILEPATH}`,
+    "certificatePassword": `${process.env.WIN_CERT_PASSWORD}`,
+  }
+};
+
+module.exports = config;
