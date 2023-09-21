bd0738198cb13f8a82c9935310d7f398d96abd42
Anoop M D <anoop.md1421@gmail.com>
2023-09-22 00:44:47 +0530

    chore(#197): ran prettier on packages/bruno-testbench

--- packages/bruno-testbench/src/index.js
+++ packages/bruno-testbench/src/index.js
@@ -13,40 +13,40 @@ app.use(cors());
 app.use(xmlparser());
 app.use(bodyParser.text());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/ping", function(req, res) {
-  return res.send("pong");
+app.get('/ping', function (req, res) {
+  return res.send('pong');
 });
 
-app.get("/headers", function(req, res) {
+app.get('/headers', function (req, res) {
   return res.json(req.headers);
 });
 
-app.get("/query", function(req, res) {
+app.get('/query', function (req, res) {
   return res.json(req.query);
 });
 
-app.get("/echo/json", function(req, res) {
-  return res.json({ping: "pong"});
+app.get('/echo/json', function (req, res) {
+  return res.json({ ping: 'pong' });
 });
 
-app.post("/echo/json", function(req, res) {
+app.post('/echo/json', function (req, res) {
   return res.json(req.body);
 });
 
-app.post("/echo/text", function(req, res) {
+app.post('/echo/text', function (req, res) {
   return res.send(req.body);
 });
 
-app.post("/echo/xml", function(req, res) {
+app.post('/echo/xml', function (req, res) {
   return res.send(req.body);
 });
 
-app.post("/echo/multipartForm", upload.none(), function(req, res) {
+app.post('/echo/multipartForm', upload.none(), function (req, res) {
   return res.json(req.body);
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Testbench started on port: ${port}`);
 });
