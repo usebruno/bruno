7e5e22cfcfd129594f15209432247b4ff0205aec
Anoop M D <anoop.md1421@gmail.com>
2023-09-25 00:58:24 +0530

    chore: release v0.15.0

--- packages/bruno-app/src/components/Sidebar/index.js
+++ packages/bruno-app/src/components/Sidebar/index.js
@@ -116,7 +116,7 @@ const Sidebar = () => {
                   </GitHubButton>
                 )}
               </div>
-              <div className="flex flex-grow items-center justify-end text-xs mr-2">v0.14.1</div>
+              <div className="flex flex-grow items-center justify-end text-xs mr-2">v0.15.0</div>
             </div>
           </div>
         </div>
