5d01c0a765ffd9d3360bd2b3e4202d652f03b842
Anoop M D <anoop.md1421@gmail.com>
2023-09-28 18:25:15 +0530

    chore: bump version to 0.16.2

--- packages/bruno-app/src/components/Sidebar/index.js
+++ packages/bruno-app/src/components/Sidebar/index.js
@@ -116,7 +116,7 @@ const Sidebar = () => {
                   </GitHubButton>
                 )}
               </div>
-              <div className="flex flex-grow items-center justify-end text-xs mr-2">v0.16.1</div>
+              <div className="flex flex-grow items-center justify-end text-xs mr-2">v0.16.2</div>
             </div>
           </div>
         </div>
