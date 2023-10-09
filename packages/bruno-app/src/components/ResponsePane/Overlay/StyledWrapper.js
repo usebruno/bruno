60d819b9f0cd8900aea14978165bfa60e5eef38c
Dipin Jagadish <djagadish@outlook.com>
2023-10-09 18:57:56 +0100

    fix: remove unwanted scrollbar

--- packages/bruno-app/src/components/ResponsePane/Overlay/StyledWrapper.js
+++ packages/bruno-app/src/components/ResponsePane/Overlay/StyledWrapper.js
@@ -3,15 +3,9 @@ import styled from 'styled-components';
 const StyledWrapper = styled.div`
   position: absolute;
   z-index: 1;
-  height: 100vh;
   background-color: ${(props) => props.theme.requestTabPanel.responseOverlayBg};
 
   div.overlay {
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
     z-index: 9;
     display: flex;
     flex-direction: column;
