8a0f7c6b8fe53da6cd03ccbbf268b4404b2c9631
Jarne <dev@jarne.rocks>
2023-10-11 19:22:50 +0200

    Save window maximized state

--- packages/bruno-electron/src/store/window-state.js
+++ packages/bruno-electron/src/store/window-state.js
@@ -4,6 +4,8 @@ const Store = require('electron-store');
 const DEFAULT_WINDOW_WIDTH = 1280;
 const DEFAULT_WINDOW_HEIGHT = 768;
 
+const DEFAULT_MAXIMIZED = false;
+
 class WindowStateStore {
   constructor() {
     this.store = new Store({
@@ -25,6 +27,14 @@ class WindowStateStore {
 
   setBounds(bounds) {
     this.store.set('window-bounds', bounds);
+  }
+
+  getMaximized() {
+    return this.store.get('maximized') || DEFAULT_MAXIMIZED;
+  }
+
+  setMaximized(isMaximized) {
+    this.store.set('maximized', isMaximized);
   }
 }
 
