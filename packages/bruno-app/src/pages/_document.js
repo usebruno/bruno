93c92aeb72b0ec2b7d3a7b8de858f7fb1871261f
Jan Monschke <janmonschke@fastmail.com>
2023-10-20 17:08:45 +0200

    fix: add missing <Head />

--- packages/bruno-app/src/pages/_document.js
+++ packages/bruno-app/src/pages/_document.js
@@ -30,6 +30,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
+        <Head />
         <body id="bruno-app-body">
           <Main />
           <NextScript />
