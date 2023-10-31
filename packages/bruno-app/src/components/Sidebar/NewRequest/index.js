bbcab8d33860dd71813190afe396eb65e4c74e96
Dipin Jagadish <djagadish@outlook.com>
2023-10-31 19:32:31 +0000

    feat: add new request from curl feature

--- packages/bruno-app/src/components/Sidebar/NewRequest/index.js
+++ packages/bruno-app/src/components/Sidebar/NewRequest/index.js
@@ -11,6 +11,7 @@ import { addTab } from 'providers/ReduxStore/slices/tabs';
 import HttpMethodSelector from 'components/RequestPane/QueryUrl/HttpMethodSelector';
 import { getDefaultRequestPaneTab } from 'utils/collections';
 import StyledWrapper from './StyledWrapper';
+import { getRequestFromCurlCommand } from 'utils/curl';
 
 const NewRequest = ({ collection, item, isEphemeral, onClose }) => {
   const dispatch = useDispatch();
@@ -21,7 +22,8 @@ const NewRequest = ({ collection, item, isEphemeral, onClose }) => {
       requestName: '',
       requestType: 'http-request',
       requestUrl: '',
-      requestMethod: 'GET'
+      requestMethod: 'GET',
+      curlCommand: ''
     },
     validationSchema: Yup.object({
       requestName: Yup.string()
@@ -61,6 +63,23 @@ const NewRequest = ({ collection, item, isEphemeral, onClose }) => {
             onClose();
           })
           .catch((err) => toast.error(err ? err.message : 'An error occurred while adding the request'));
+      } else if (values.requestType === 'from-curl') {
+        getRequestFromCurlCommand(values.curlCommand).then((request) =>
+          dispatch(
+            newHttpRequest({
+              requestName: values.requestName,
+              requestType: 'http-request',
+              requestUrl: request.url,
+              requestMethod: request.method,
+              collectionUid: collection.uid,
+              itemUid: item ? item.uid : null,
+              headers: request.headers,
+              body: request.body
+            })
+          )
+            .then(() => onClose())
+            .catch((err) => toast.error(err ? err.message : 'An error occurred while adding the request'))
+        );
       } else {
         dispatch(
           newHttpRequest({
@@ -124,9 +143,22 @@ const NewRequest = ({ collection, item, isEphemeral, onClose }) => {
               <label htmlFor="graphql-request" className="ml-1 cursor-pointer select-none">
                 GraphQL
               </label>
+
+              <input
+                id="from-curl"
+                className="cursor-pointer ml-auto"
+                type="radio"
+                name="requestType"
+                onChange={formik.handleChange}
+                value="from-curl"
+                checked={formik.values.requestType === 'from-curl'}
+              />
+
+              <label htmlFor="from-curl" className="ml-1 cursor-pointer select-none">
+                From cURL
+              </label>
             </div>
           </div>
-
           <div className="mt-4">
             <label htmlFor="requestName" className="block font-semibold">
               Name
@@ -148,38 +180,55 @@ const NewRequest = ({ collection, item, isEphemeral, onClose }) => {
               <div className="text-red-500">{formik.errors.requestName}</div>
             ) : null}
           </div>
+          {formik.values.requestType !== 'from-curl' ? (
+            <>
+              <div className="mt-4">
+                <label htmlFor="request-url" className="block font-semibold">
+                  URL
+                </label>
 
-          <div className="mt-4">
-            <label htmlFor="request-url" className="block font-semibold">
-              URL
-            </label>
-
-            <div className="flex items-center mt-2 ">
-              <div className="flex items-center h-full method-selector-container">
-                <HttpMethodSelector
-                  method={formik.values.requestMethod}
-                  onMethodSelect={(val) => formik.setFieldValue('requestMethod', val)}
-                />
+                <div className="flex items-center mt-2 ">
+                  <div className="flex items-center h-full method-selector-container">
+                    <HttpMethodSelector
+                      method={formik.values.requestMethod}
+                      onMethodSelect={(val) => formik.setFieldValue('requestMethod', val)}
+                    />
+                  </div>
+                  <div className="flex items-center flex-grow input-container h-full">
+                    <input
+                      id="request-url"
+                      type="text"
+                      name="requestUrl"
+                      className="px-3 w-full "
+                      autoComplete="off"
+                      autoCorrect="off"
+                      autoCapitalize="off"
+                      spellCheck="false"
+                      onChange={formik.handleChange}
+                      value={formik.values.requestUrl || ''}
+                    />
+                  </div>
+                </div>
+                {formik.touched.requestUrl && formik.errors.requestUrl ? (
+                  <div className="text-red-500">{formik.errors.requestUrl}</div>
+                ) : null}
               </div>
-              <div className="flex items-center flex-grow input-container h-full">
-                <input
-                  id="request-url"
-                  type="text"
-                  name="requestUrl"
-                  className="px-3 w-full "
-                  autoComplete="off"
-                  autoCorrect="off"
-                  autoCapitalize="off"
-                  spellCheck="false"
-                  onChange={formik.handleChange}
-                  value={formik.values.requestUrl || ''}
-                />
-              </div>
+            </>
+          ) : (
+            <div className="mt-4">
+              <label htmlFor="request-url" className="block font-semibold">
+                cURL Command
+              </label>
+              <textarea
+                name="curlCommand"
+                placeholder="Enter cURL request here.."
+                className="block textbox w-full mt-4"
+                style={{ resize: 'none' }}
+                value={formik.values.curlCommand}
+                onChange={formik.handleChange}
+              ></textarea>
             </div>
-            {formik.touched.requestUrl && formik.errors.requestUrl ? (
-              <div className="text-red-500">{formik.errors.requestUrl}</div>
-            ) : null}
-          </div>
+          )}
         </form>
       </Modal>
     </StyledWrapper>
---
+++ packages/bruno-app/src/utils/curl/index.js
@@ -0,0 +1,65 @@
+import * as parse from 'parse-curl';
+import { BrunoError } from 'utils/common/error';
+import { parseQueryParams } from 'utils/url';
+
+export const getRequestFromCurlCommand = (command) => {
+  const parseFormData = (parsedBody) => {
+    parseQueryParams(parsedBody);
+  };
+  return new Promise((resolve, reject) => {
+    try {
+      const request = parse(command);
+      const parsedHeader = request?.header;
+      const headers =
+        parsedHeader &&
+        Object.keys(parsedHeader).map((key) => ({ name: key, value: parsedHeader[key], enabled: true }));
+
+      const contentType = headers?.find((h) => h.name.toLowerCase() === 'content-type');
+      const body = {
+        mode: 'none',
+        json: null,
+        text: null,
+        xml: null,
+        sparql: null,
+        multipartForm: null,
+        formUrlEncoded: null
+      };
+      const parsedBody = request?.body;
+      if (parsedBody && contentType) {
+        switch (contentType.value.toLowerCase()) {
+          case 'application/json':
+            body.mode = 'json';
+            body.json = parsedBody;
+            break;
+          case 'text/xml':
+            body.mode = 'xml';
+            body.xml = parsedBody;
+            break;
+          case 'application/x-www-form-urlencoded':
+            body.mode = 'formUrlEncoded';
+            body.formUrlEncoded = parseFormData(parsedBody);
+            break;
+          case 'multipart/form-data':
+            body.mode = 'multipartForm';
+            body.multipartForm = parsedBody;
+            break;
+          case 'text/plain':
+          default:
+            body.mode = 'text';
+            body.text = parsedBody;
+            break;
+        }
+      }
+      debugger;
+      console.log(request);
+      return resolve({
+        url: request.url,
+        method: request.method,
+        body,
+        headers: headers
+      });
+    } catch (error) {
+      return reject(new BrunoError('Unable to parse the cURL command'));
+    }
+  });
+};
