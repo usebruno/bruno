b2872de18deaa20866c38f719709b1b614882972
BT <149745752+btgs-0@users.noreply.github.com>
2023-11-04 09:39:06 +1030

    Add from collection dropdown option to request auth mode

--- packages/bruno-app/src/components/RequestPane/Auth/AuthMode/index.js
+++ packages/bruno-app/src/components/RequestPane/Auth/AuthMode/index.js
@@ -39,6 +39,15 @@ const AuthMode = ({ item, collection }) => {
             className="dropdown-item"
             onClick={() => {
               dropdownTippyRef.current.hide();
+              onModeChange('parent');
+            }}
+          >
+            From Collection
+          </div>
+          <div
+            className="dropdown-item"
+            onClick={() => {
+              dropdownTippyRef.current.hide();
               onModeChange('awsv4');
             }}
           >
