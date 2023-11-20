9e1e9bf3eb8ee94b8a98207c4e2b7f9f2efb8b4d
Maikel Hofman <m.hofman@chronos-technology.nl>
2023-11-20 16:33:27 +0100

    Add electron shortcuts

--- packages/bruno-app/src/providers/Hotkeys/keyMapping.js
+++ packages/bruno-app/src/providers/Hotkeys/keyMapping.js
@@ -4,5 +4,7 @@ export const KeyMapping = {
   sendRequest: { keys: ['command+enter', 'ctrl+enter'], name: 'Send Request' },
   editEnvironment: { keys: ['command+e', 'ctrl+e'], name: 'Edit Environment' },
   newRequest: { keys: ['command+b', 'ctrl+b'], name: 'New Request' },
-  closeTab: { keys: ['command+w', 'ctrl+w'], name: 'Close Tab' }
+  closeTab: { keys: ['command+w', 'ctrl+w'], name: 'Close Tab' },
+  openPreferences: { keys: ['command+,', 'ctrl+,'], name: 'Open preferences' },
+  minimizeWindow: { keys: ['command+Shift+Q', 'control+Shift+Q'], name: 'Minimize window' }
 };
