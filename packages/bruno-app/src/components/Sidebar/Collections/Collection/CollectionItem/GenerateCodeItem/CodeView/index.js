89c5fc2f03a08ad7089e3cbba0774564f51da8b7
Sebastien Dionne <survivant00@gmail.com>
2023-12-10 15:12:04 -0500

    Add copy to clipboard icon

--- packages/bruno-app/src/components/Sidebar/Collections/Collection/CollectionItem/GenerateCodeItem/CodeView/index.js
+++ packages/bruno-app/src/components/Sidebar/Collections/Collection/CollectionItem/GenerateCodeItem/CodeView/index.js
@@ -4,6 +4,10 @@ import { HTTPSnippet } from 'httpsnippet';
 import { useTheme } from 'providers/Theme/index';
 import { buildHarRequest } from 'utils/codegenerator/har';
 import { useSelector } from 'react-redux';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import toast from 'react-hot-toast';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
 
 const CodeView = ({ language, item }) => {
   const { storedTheme } = useTheme();
@@ -20,13 +24,20 @@ const CodeView = ({ language, item }) => {
   }
 
   return (
-    <CodeEditor
-      readOnly
-      value={snippet}
-      font={get(preferences, 'font.codeFont', 'default')}
-      theme={storedTheme}
-      mode={lang}
-    />
+    <>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <CopyToClipboard text={snippet} onCopy={() => toast.success('Copied to clipboard!')}>
+          <FontAwesomeIcon icon={faCopy} style={{ cursor: 'pointer' }} />
+        </CopyToClipboard>
+      </div>
+      <CodeEditor
+        readOnly
+        value={snippet}
+        font={get(preferences, 'font.codeFont', 'default')}
+        theme={storedTheme}
+        mode={lang}
+      />
+    </>
   );
 };
 
