732045a9bebec7a8fc9bf5a6a1d5969884d9ac22
Baptiste POULAIN <baptistepoulain@MAC882.local>
2023-12-11 12:14:07 +0100

    bugfix(request_disappearing): encode URI only when field isn't a password, checks all tests

--- packages/bruno-lang/v2/src/bruToJson.js
+++ packages/bruno-lang/v2/src/bruToJson.js
@@ -105,7 +105,7 @@ const mapPairListToKeyValPairs = (pairList = [], parseEnabled = true) => {
   }
   return _.map(pairList[0], (pair) => {
     let name = _.keys(pair)[0];
-    let value = pair[name].startsWith('http') ? decodeURIComponent(pair[name]) : pair[name];
+    let value = name === 'password' ? pair[name] : decodeURIComponent(pair[name]);
 
     if (!parseEnabled) {
       return {
