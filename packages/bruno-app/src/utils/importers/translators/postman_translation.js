8bcb0baefa7a40f7fb41c7b12bb31cfd36e607ab
Baptiste POULAIN <baptistepoulain@MAC882.local>
2023-12-20 10:38:14 +0100

    feature(postman_tests_scripts): roll back into .js, replace set/get Env by EnvVar

--- packages/bruno-app/src/utils/importers/translators/postman_translation.js
+++ packages/bruno-app/src/utils/importers/translators/postman_translation.js
@@ -1,11 +1,11 @@
-const replacements: Record<string, string> = {
-  'pm\\.environment\\.get\\(([\'"])([^\'"]*)\\1\\)': 'bru.getEnv($1$2$1)',
-  'pm\\.environment\\.set\\(([\'"])([^\'"]*)\\1, ([\'"])([^\'"]*)\\3\\)': 'bru.setEnv($1$2$1, $3$4$3)',
+const replacements = {
+  'pm\\.environment\\.get\\(([\'"])([^\'"]*)\\1\\)': 'bru.getEnvVar($1$2$1)',
+  'pm\\.environment\\.set\\(([\'"])([^\'"]*)\\1, ([\'"])([^\'"]*)\\3\\)': 'bru.setEnvVar($1$2$1, $3$4$3)',
   'pm\\.variables\\.get\\(([\'"])([^\'"]*)\\1\\)': 'bru.getVar($1$2$1)',
   'pm\\.variables\\.set\\(([\'"])([^\'"]*)\\1, ([\'"])([^\'"]*)\\3\\)': 'bru.setVar($1$2$1, $3$4$3)'
 };
 
-export const postmanTranslation = (script: string) => {
+export const postmanTranslation = (script) => {
   const modifiedScript = Object.entries(replacements)
     .map(([pattern, replacement]) => {
       const regex = new RegExp(pattern, 'g');
