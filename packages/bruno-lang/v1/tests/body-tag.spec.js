4f00bac59d18ad965d89204de1ef18700fb090d2
Ricardo Silverio <ricardo.silverio.souza@gmail.com>
2024-01-14 19:31:52 -0300

    Additional fix

--- packages/bruno-lang/v1/tests/body-tag.spec.js
+++ packages/bruno-lang/v1/tests/body-tag.spec.js
@@ -1,7 +1,7 @@
 const { bodyJsonTag } = require('../src/body-tag');
 
 describe('bodyJsonTag', () => {
-  const testbodyJson = (input, expected) => {
+  const testbodyJson = (input) => {
     const result = bodyJsonTag.run(input);
     expect(result.isError).toBe(false);
     expect(result.result.body.json).toEqual('{ "foo": "bar" }');
@@ -10,25 +10,25 @@ describe('bodyJsonTag', () => {
   // simple case
   it('should parse json body tag - 1', () => {
     const input = 'body(type=json)\n{ "foo": "bar" }\n/body';
-    testbodyJson(input, '{ "foo": "bar" }\n');
+    testbodyJson(input);
   });
 
   // space between body and args
   it('should parse json body tag - 2', () => {
     const input = 'body (type = json)\n{ "foo": "bar" }\n/body';
-    testbodyJson(input, '{ "foo": "bar" }\n');
+    testbodyJson(input);
   });
 
   // space after body tag
   it('should parse json body tag - 3', () => {
     const input = 'body (type = json)  \n{ "foo": "bar" }\n/body';
-    testbodyJson(input, '{ "foo": "bar" }\n');
+    testbodyJson(input);
   });
 
   // space after body tag
   it('should parse json body tag - 4', () => {
     const input = 'body (type = json)  \n{ "foo": "bar" }\n/body ';
-    testbodyJson(input, '{ "foo": "bar" }\n');
+    testbodyJson(input);
   });
 
   it('should fail to parse when body tag is missing', () => {
