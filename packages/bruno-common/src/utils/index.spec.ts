f2a187d5fe7a45b09206a11c3f7e0bfc97a2d903
Anoop M D <anoop.md1421@gmail.com>
2024-01-28 18:07:25 +0530

    feat(#1460): string interpolation (#1461)
    * feat(#1460): string interpolation
    
    * chore: deleted file commited accidentally

---
+++ packages/bruno-common/src/utils/index.spec.ts
@@ -0,0 +1,39 @@
+import { flattenObject } from './index';
+
+describe('flattenObject', () => {
+  it('should flatten a simple object', () => {
+    const input = { a: 1, b: { c: 2, d: { e: 3 } } };
+    const output = flattenObject(input);
+    expect(output).toEqual({ a: 1, 'b.c': 2, 'b.d.e': 3 });
+  });
+
+  it('should flatten an object with arrays', () => {
+    const input = { a: 1, b: { c: [2, 3, 4], d: { e: 5 } } };
+    const output = flattenObject(input);
+    expect(output).toEqual({ a: 1, 'b.c[0]': 2, 'b.c[1]': 3, 'b.c[2]': 4, 'b.d.e': 5 });
+  });
+
+  it('should flatten an object with arrays having objects', () => {
+    const input = { a: 1, b: { c: [{ d: 2 }, { e: 3 }], f: { g: 4 } } };
+    const output = flattenObject(input);
+    expect(output).toEqual({ a: 1, 'b.c[0].d': 2, 'b.c[1].e': 3, 'b.f.g': 4 });
+  });
+
+  it('should handle null values', () => {
+    const input = { a: 1, b: { c: null, d: { e: 3 } } };
+    const output = flattenObject(input);
+    expect(output).toEqual({ a: 1, 'b.c': null, 'b.d.e': 3 });
+  });
+
+  it('should handle an empty object', () => {
+    const input = {};
+    const output = flattenObject(input);
+    expect(output).toEqual({});
+  });
+
+  it('should handle an object with nested empty objects', () => {
+    const input = { a: { b: {}, c: { d: {} } } };
+    const output = flattenObject(input);
+    expect(output).toEqual({});
+  });
+});
