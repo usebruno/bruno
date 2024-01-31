bd2760f7b678ef553868bbd1458c61302bb8b801
Bruno Braga <brunobragaw8t@gmail.com>
2024-01-31 22:17:46 +0000

    feature: add search results count to CodeMirror

--- packages/bruno-app/src/components/CodeEditor/StyledWrapper.js
+++ packages/bruno-app/src/components/CodeEditor/StyledWrapper.js
@@ -13,6 +13,24 @@ const StyledWrapper = styled.div`
     background: #d2d7db;
   }
 
+  .CodeMirror-dialog {
+    overflow: visible;
+  }
+
+  #search-results-count {
+    display: inline-block;
+    position: absolute;
+    top: calc(100% + 1px);
+    right: 0;
+    border-width: 0 0 1px 1px;
+    border-style: solid;
+    border-color: ${(props) => props.theme.codemirror.border};
+    padding: 0.1em 0.8em;
+    background-color: ${(props) => props.theme.codemirror.bg};
+    color: rgb(102, 102, 102);
+    white-space: nowrap;
+  }
+
   textarea.cm-editor {
     position: relative;
   }
