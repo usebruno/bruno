2feaee25a669423ce815d20efa74383749068e4e
Eugen Soliar <eugen.soliar@exxeta.com>
2024-02-08 18:10:50 +0100

    got rid of useless debug log

--- packages/bruno-electron/src/index.js
+++ packages/bruno-electron/src/index.js
@@ -36,7 +36,7 @@ let watcher;
 // Prepare the renderer once the app is ready
 app.on('ready', async () => {
   const { maximized, x, y, width, height, isFullResize } = loadWindowState();
-  console.log('isFullResize', isFullResize);
+
   mainWindow = new BrowserWindow({
     x,
     y,
