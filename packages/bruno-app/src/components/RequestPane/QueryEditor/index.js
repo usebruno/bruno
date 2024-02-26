d1a8f59c79a9e8ed6940b0f55f3fd6b4b17cc844
Jeremy Benoist <jeremy.benoist@gmail.com>
2024-02-26 17:02:04 +0100

    Add "Prettify GraphQL" button
    The goal is to achieve the same behavior from Insomnia which allow to format a GraphQL query using prettier (see https://github.com/Kong/insomnia/blob/264177b56fcf0a663bebfc8325eb95adb2a2f70e/packages/insomnia/src/ui/components/editors/body/graph-ql-editor.tsx#L260-L266).
    
    I moved the `prettier` deps from `devDependencies` to `dependencies` because it's now used within the application.
    I was forced to import `prettier/standalone` & `prettier/parser-graphql` (as it is explained here: https://prettier.io/docs/en/browser.html) otherwise I got that error:
    
    > Couldn't resolve parser "graphql". Parsers must be explicitly added to the standalone bundle.

--- packages/bruno-app/src/components/RequestPane/QueryEditor/index.js
+++ packages/bruno-app/src/components/RequestPane/QueryEditor/index.js
@@ -8,8 +8,11 @@
 import React from 'react';
 import isEqual from 'lodash/isEqual';
 import MD from 'markdown-it';
+import { format } from 'prettier/standalone';
+import prettierPluginGraphql from 'prettier/parser-graphql';
 import { getAllVariables } from 'utils/collections';
 import { defineCodeMirrorBrunoVariablesMode } from 'utils/common/codemirror';
+import toast from 'react-hot-toast';
 import StyledWrapper from './StyledWrapper';
 
 import onHasCompletion from './onHasCompletion';
@@ -178,6 +181,19 @@ export default class QueryEditor extends React.Component {
     }
   }
 
+  beautifyRequestBody = () => {
+    try {
+      const prettyQuery = format(this.props.value, {
+        parser: 'graphql',
+        plugins: [prettierPluginGraphql]
+      });
+
+      this.editor.setValue(prettyQuery);
+    } catch (e) {
+      toast.error('Error occurred while prettifying GraphQL query');
+    }
+  };
+
   // Todo: Overlay is messing up with schema hint
   // Fix this
   addOverlay = () => {
@@ -189,13 +205,19 @@ export default class QueryEditor extends React.Component {
 
   render() {
     return (
-      <StyledWrapper
-        className="h-full w-full"
-        aria-label="Query Editor"
-        ref={(node) => {
-          this._node = node;
-        }}
-      />
+      <>
+        <StyledWrapper
+          className="h-full w-full"
+          aria-label="Query Editor"
+          ref={(node) => {
+            this._node = node;
+          }}
+        >
+          <button className="btn-add-param text-link pr-2 py-3 mt-2 select-none" onClick={this.beautifyRequestBody}>
+            Prettify GraphQL
+          </button>
+        </StyledWrapper>
+      </>
     );
   }
 
