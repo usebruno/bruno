6188a86315ffc3943eb789fa85961cd42282b407
bpoulaindev <bpoulainpro@gmail.com>
2024-03-18 17:15:04 +0100

    feat(collection-variables) : remove tailwindcss forms

--- packages/bruno-app/tailwind.config.js
+++ packages/bruno-app/tailwind.config.js
@@ -1,22 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
-  ],
-  prefix: "",
+  darkMode: ['class'],
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}', './app/**/*.{js,jsx}', './src/**/*.{js,jsx}'],
+  prefix: '',
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
-      },
+        '2xl': '1400px'
+      }
     },
-    extend: {},
+    extend: {}
   },
-  plugins: [require("@tailwindcss/forms")],
-}
+  plugins: []
+};
