4de7b5889b08b5e15744ac5704bfcb71ff075c69
Its-treason <timon.sch71@gmail.com>
2024-03-19 22:02:11 +0100

    feat: Remove Bruno links from support page

--- packages/bruno-app/src/components/Preferences/Support/index.js
+++ packages/bruno-app/src/components/Preferences/Support/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconSpeakerphone, IconBrandTwitter, IconBrandGithub, IconBrandDiscord, IconBook } from '@tabler/icons';
+import { IconSpeakerphone, IconBrandDiscord } from '@tabler/icons';
 import StyledWrapper from './StyledWrapper';
 
 const Support = () => {
@@ -7,33 +7,15 @@ const Support = () => {
     <StyledWrapper>
       <div className="rows">
         <div className="mt-2">
-          <a href="https://docs.usebruno.com" target="_blank" className="flex items-end">
-            <IconBook size={18} strokeWidth={2} />
-            <span className="label ml-2">Documentation</span>
-          </a>
-        </div>
-        <div className="mt-2">
-          <a href="https://github.com/usebruno/bruno/issues" target="_blank" className="flex items-end">
+          <a href="https://github.com/its-treason/bruno/issues" target="_blank" className="flex items-end">
             <IconSpeakerphone size={18} strokeWidth={2} />
             <span className="label ml-2">Report Issues</span>
           </a>
         </div>
         <div className="mt-2">
-          <a href="https://discord.com/invite/KgcZUncpjq" target="_blank" className="flex items-end">
+          <a href="https://discordapp.com/users/139058134596583424" target="_blank" className="flex items-end">
             <IconBrandDiscord size={18} strokeWidth={2} />
             <span className="label ml-2">Discord</span>
-          </a>
-        </div>
-        <div className="mt-2">
-          <a href="https://github.com/usebruno/bruno" target="_blank" className="flex items-end">
-            <IconBrandGithub size={18} strokeWidth={2} />
-            <span className="label ml-2">GitHub</span>
-          </a>
-        </div>
-        <div className="mt-2">
-          <a href="https://twitter.com/use_bruno" target="_blank" className="flex items-end">
-            <IconBrandTwitter size={18} strokeWidth={2} />
-            <span className="label ml-2">Twitter</span>
           </a>
         </div>
       </div>
