2163067a35376b96469a39206f2dfd4fda818df9
Mateusz Pietryga <pietryga.m@gmail.com>
2024-04-14 23:37:14 +0200

    Fix: OAuth2: Supported at the request level, but not at the collection level
    #1704

--- packages/bruno-electron/src/ipc/network/prepare-request.js
+++ packages/bruno-electron/src/ipc/network/prepare-request.js
@@ -59,6 +59,9 @@ const setAuthHeaders = (axiosRequest, request, collectionRoot) => {
           password: get(collectionAuth, 'digest.password')
         };
         break;
+      case 'oauth2':
+        request.auth = collectionAuth;
+        break;
     }
   }
 
