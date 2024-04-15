c8c3312d292c44237ea1336e5a8b8c1dd7c9207a
Bijin AB <bijin.balakrishnan@allegiantair.com>
2024-04-16 01:28:44 +0530

    fix: handle null values

--- packages/bruno-app/src/components/RequestPane/QueryUrl/index.js
+++ packages/bruno-app/src/components/RequestPane/QueryUrl/index.js
@@ -14,7 +14,7 @@ const QueryUrl = ({ item, collection, handleRun }) => {
   const { theme, storedTheme } = useTheme();
   const dispatch = useDispatch();
   const method = item.draft ? get(item, 'draft.request.method') : get(item, 'request.method');
-  const url = item.draft ? get(item, 'draft.request.url') : get(item, 'request.url');
+  const url = item.draft ? get(item, 'draft.request.url', '') : get(item, 'request.url', '');
   const isMac = isMacOS();
   const saveShortcut = isMac ? 'Cmd + S' : 'Ctrl + S';
 
