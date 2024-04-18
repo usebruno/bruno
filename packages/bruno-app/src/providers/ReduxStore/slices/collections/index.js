3deb126359934f6b18fbed0e0fa1ca9cffd527a4
Nicolas PUZIN <nicolas.puzin@cartier.com>
2024-04-18 12:32:39 +0200

    feature: Multi-part requests: user should be able to set content-type for each part in a multi-part request. #1602

--- packages/bruno-app/src/providers/ReduxStore/slices/collections/index.js
+++ packages/bruno-app/src/providers/ReduxStore/slices/collections/index.js
@@ -625,6 +625,7 @@ export const collectionsSlice = createSlice({
             name: '',
             value: '',
             description: '',
+            contentType: '',
             enabled: true
           });
         }
@@ -646,6 +647,7 @@ export const collectionsSlice = createSlice({
             param.name = action.payload.param.name;
             param.value = action.payload.param.value;
             param.description = action.payload.param.description;
+            param.contentType = action.payload.param.contentType;
             param.enabled = action.payload.param.enabled;
           }
         }
