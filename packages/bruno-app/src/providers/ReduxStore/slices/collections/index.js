12f49a03c562bc28f1107cd8ecda8d9518cff5e5
Nicolas PUZIN <nicolas.puzin@cartier.com>
2024-04-18 11:22:15 +0200

    implementing filesystem load/save of content-type (wip)

--- packages/bruno-app/src/providers/ReduxStore/slices/collections/index.js
+++ packages/bruno-app/src/providers/ReduxStore/slices/collections/index.js
@@ -625,6 +625,7 @@ export const collectionsSlice = createSlice({
             name: '',
             value: '',
             description: '',
+            contentType: 'application/xml',
             enabled: true
           });
         }
@@ -646,6 +647,7 @@ export const collectionsSlice = createSlice({
             param.name = action.payload.param.name;
             param.value = action.payload.param.value;
             param.description = action.payload.param.description;
+            param.contentType = 'application/xml';
             param.enabled = action.payload.param.enabled;
           }
         }
--- packages/bruno-lang/v2/src/bruToJson.js
+++ packages/bruno-lang/v2/src/bruToJson.js
@@ -41,7 +41,7 @@ const grammar = ohm.grammar(`Bru {
   pair = st* key st* ":" st* value st*
   key = keychar*
   value = valuechar*
-  
+
   // Dictionary for Assert Block
   assertdictionary = st* "{" assertpairlist? tagend
   assertpairlist = optionalnl* assertpair (~tagend stnl* assertpair)* (~tagend space)*
@@ -129,16 +129,29 @@ const mapPairListToKeyValPairs = (pairList = [], parseEnabled = true) => {
   });
 };
 
+const multipartExtractContentType = (pair) => {
+  if (_.isString(pair.value)) {
+    const match = pair.value.match(/(.*?)\s*\(Content-Type=(.*?)\)/);
+    if (match != null && match.length > 2) {
+      pair.value = match[1];
+      pair.contentType = match[2];
+    }
+  }
+};
+
 const mapPairListToKeyValPairsMultipart = (pairList = [], parseEnabled = true) => {
   const pairs = mapPairListToKeyValPairs(pairList, parseEnabled);
 
   return pairs.map((pair) => {
     pair.type = 'text';
+    multipartExtractContentType(pair);
+
     if (pair.value.startsWith('@file(') && pair.value.endsWith(')')) {
       let filestr = pair.value.replace(/^@file\(/, '').replace(/\)$/, '');
       pair.type = 'file';
       pair.value = filestr.split('|');
     }
+
     return pair;
   });
 };
