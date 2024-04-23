0080b4f83597a626a06e4eb800d588b9213d6b9c
Mateusz Pietryga <pietryga.m@gmail.com>
2024-04-24 01:20:01 +0200

    feat: OAuth 2.0 Client Credentials as Basic Auth - bru data model
    #2106

--- packages/bruno-lang/v2/src/bruToJson.js
+++ packages/bruno-lang/v2/src/bruToJson.js
@@ -443,6 +443,7 @@ const sem = grammar.createSemantics().addAttribute('ast', {
     const accessTokenUrlKey = _.find(auth, { name: 'access_token_url' });
     const clientIdKey = _.find(auth, { name: 'client_id' });
     const clientSecretKey = _.find(auth, { name: 'client_secret' });
+    const clientSecretMethodKey = _.find(auth, { name: 'client_secret_method' });
     const scopeKey = _.find(auth, { name: 'scope' });
     const stateKey = _.find(auth, { name: 'state' });
     const pkceKey = _.find(auth, { name: 'pkce' });
@@ -457,6 +458,7 @@ const sem = grammar.createSemantics().addAttribute('ast', {
                 password: passwordKey ? passwordKey.value : '',
                 clientId: clientIdKey ? clientIdKey.value : '',
                 clientSecret: clientSecretKey ? clientSecretKey.value : '',
+                clientSecretMethod: clientSecretMethodKey ? clientSecretMethodKey.value : '',
                 scope: scopeKey ? scopeKey.value : ''
               }
             : grantTypeKey?.value && grantTypeKey?.value == 'authorization_code'
@@ -467,6 +469,7 @@ const sem = grammar.createSemantics().addAttribute('ast', {
                 accessTokenUrl: accessTokenUrlKey ? accessTokenUrlKey.value : '',
                 clientId: clientIdKey ? clientIdKey.value : '',
                 clientSecret: clientSecretKey ? clientSecretKey.value : '',
+                clientSecretMethod: clientSecretMethodKey ? clientSecretMethodKey.value : '',
                 scope: scopeKey ? scopeKey.value : '',
                 state: stateKey ? stateKey.value : '',
                 pkce: pkceKey ? JSON.parse(pkceKey?.value || false) : false
@@ -477,6 +480,7 @@ const sem = grammar.createSemantics().addAttribute('ast', {
                 accessTokenUrl: accessTokenUrlKey ? accessTokenUrlKey.value : '',
                 clientId: clientIdKey ? clientIdKey.value : '',
                 clientSecret: clientSecretKey ? clientSecretKey.value : '',
+                clientSecretMethod: clientSecretMethodKey ? clientSecretMethodKey.value : '',
                 scope: scopeKey ? scopeKey.value : ''
               }
             : {}
