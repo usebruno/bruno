d074a5195cd61d0b2595509bce121d3960567a96
Its-treason <timon.sch71@gmail.com>
2024-04-26 00:17:19 +0200

    Revert "revert package-lock.json"
    This reverts commit 710fb2aee4553952c1319b81a02af3ebf0d3074e.

--- packages/bruno-app/src/components/Modal/index.js
+++ packages/bruno-app/src/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import StyledWrapper from './StyledWrapper';
 import { IconAlertTriangle } from '@tabler/icons';
 
