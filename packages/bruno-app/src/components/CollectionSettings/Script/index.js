2bbd0352b07f716c6f744b7497c8fefb9712cf40
Max Bauer <krummbar@pm.me>
2024-05-01 10:11:45 +0200

    set inherit mode if unset

--- packages/bruno-app/src/components/CollectionSettings/Script/index.js
+++ packages/bruno-app/src/components/CollectionSettings/Script/index.js
@@ -52,7 +52,7 @@ const Script = ({ collection }) => {
           mode="javascript"
           onSave={handleSave}
           font={get(preferences, 'font.codeFont', 'default')}
-          fontSize={get(preferences, 'font.codeFontSize', '14')}
+          fontSize={get(preferences, 'font.codeFontSize')}
         />
       </div>
       <div className="flex-1 mt-6">
@@ -65,7 +65,7 @@ const Script = ({ collection }) => {
           mode="javascript"
           onSave={handleSave}
           font={get(preferences, 'font.codeFont', 'default')}
-          fontSize={get(preferences, 'font.codeFontSize', '14')}
+          fontSize={get(preferences, 'font.codeFontSize')}
         />
       </div>
 
