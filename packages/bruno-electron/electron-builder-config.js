1ceb53db6e9510cc9950550f50a1ee6df2eae8ac
vagrant <logan.davies181@gmail.com>
2024-05-07 21:34:42 +1200

    bru electron: bump to version that supports node_extra_ca_certs

--- packages/bruno-electron/electron-builder-config.js
+++ packages/bruno-electron/electron-builder-config.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: process.env.DOTENV_PATH });
 const config = {
   appId: 'com.usebruno.app',
   productName: 'Bruno',
-  electronVersion: '21.1.1',
+  electronVersion: '30.0.2',
   directories: {
     buildResources: 'resources',
     output: 'out'
