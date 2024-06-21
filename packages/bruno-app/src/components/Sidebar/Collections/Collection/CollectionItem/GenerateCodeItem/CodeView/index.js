94effd1562d6bc6698a1d9732717289d7e96b8b3
jean_lethiec <Jean_LETHIEC@connect-tech.sncf>
2024-06-21 17:06:18 +0200

    Add collection var to CodeEditor props

--- packages/bruno-app/src/components/Sidebar/Collections/Collection/CollectionItem/GenerateCodeItem/CodeView/index.js
+++ packages/bruno-app/src/components/Sidebar/Collections/Collection/CollectionItem/GenerateCodeItem/CodeView/index.js
@@ -43,6 +43,7 @@ const CodeView = ({ language, item }) => {
         </CopyToClipboard>
         <CodeEditor
           readOnly
+          collection={collection}
           value={snippet}
           font={get(preferences, 'font.codeFont', 'default')}
           theme={displayedTheme}
