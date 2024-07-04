8eff5dc810bf521e4ccd3ba674080ce589ee3a3b
Sanjai Kumar <sanjailucifer666@gmail.com>
2024-07-04 11:21:56 +0530

    chore: fix markdown component

--- packages/bruno-app/src/components/MarkDown/index.jsx
+++ packages/bruno-app/src/components/MarkDown/index.jsx
@@ -1,15 +1,31 @@
 import MarkdownIt from 'markdown-it';
 import StyledWrapper from './StyledWrapper';
-import * as React from 'react';
+import React, { useCallback } from 'react';
 
 const md = new MarkdownIt();
 
 const Markdown = ({ onDoubleClick, content }) => {
-  const handleOnDoubleClick = (event) => {
-    if (event?.detail === 2) {
-      onDoubleClick();
+  const handleOnClick = useCallback((event) => {
+    const target = event.target;
+    if (target.tagName === 'A') {
+      event.preventDefault();
+      const href = target.getAttribute('href');
+      if (href) {
+        window.open(href, '_blank');
+        return;
+      }
     }
-  };
+  }, []);
+
+  const handleOnDoubleClick = useCallback(
+    (event) => {
+      if (event.detail === 2) {
+        onDoubleClick();
+      }
+    },
+    [onDoubleClick]
+  );
+
   const htmlFromMarkdown = md.render(content || '');
 
   return (
@@ -17,7 +33,8 @@ const Markdown = ({ onDoubleClick, content }) => {
       <div
         className="markdown-body"
         dangerouslySetInnerHTML={{ __html: htmlFromMarkdown }}
-        onClick={handleOnDoubleClick}
+        onClick={handleOnClick}
+        onDoubleClick={handleOnDoubleClick}
       />
     </StyledWrapper>
   );
