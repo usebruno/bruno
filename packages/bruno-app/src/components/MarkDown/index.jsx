23a997e0f5f0f93ecc6f7dc35ef4e25ff5a79182
Sanjai Kumar <sanjailucifer666@gmail.com>
2024-07-04 11:24:22 +0530

    Refactor MarkDown component to remove unnecessary useCallback hooks

--- packages/bruno-app/src/components/MarkDown/index.jsx
+++ packages/bruno-app/src/components/MarkDown/index.jsx
@@ -1,11 +1,11 @@
 import MarkdownIt from 'markdown-it';
 import StyledWrapper from './StyledWrapper';
-import React, { useCallback } from 'react';
+import React from 'react';
 
 const md = new MarkdownIt();
 
 const Markdown = ({ onDoubleClick, content }) => {
-  const handleOnClick = useCallback((event) => {
+  const handleOnClick = (event) => {
     const target = event.target;
     if (target.tagName === 'A') {
       event.preventDefault();
@@ -15,16 +15,13 @@ const Markdown = ({ onDoubleClick, content }) => {
         return;
       }
     }
-  }, []);
+  };
 
-  const handleOnDoubleClick = useCallback(
-    (event) => {
-      if (event.detail === 2) {
-        onDoubleClick();
-      }
-    },
-    [onDoubleClick]
-  );
+  const handleOnDoubleClick = (event) => {
+    if (event.detail === 2) {
+      onDoubleClick();
+    }
+  };
 
   const htmlFromMarkdown = md.render(content || '');
 
