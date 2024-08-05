60a8647e7c5ddbdbc54547c08af15468ce2da3b4
Rinku Chaudhari <76877078+therealrinku@users.noreply.github.com>
2024-08-05 18:12:19 +0545

    fix: enter key not submitting new request form (#2630)

--- packages/bruno-app/src/components/Sidebar/NewRequest/index.js
+++ packages/bruno-app/src/components/Sidebar/NewRequest/index.js
@@ -161,7 +161,16 @@ const NewRequest = ({ collection, item, isEphemeral, onClose }) => {
   return (
     <StyledWrapper>
       <Modal size="md" title="New Request" confirmText="Create" handleConfirm={onSubmit} handleCancel={onClose}>
-        <form className="bruno-form" onSubmit={formik.handleSubmit}>
+        <form
+          className="bruno-form"
+          onSubmit={formik.handleSubmit}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              formik.handleSubmit();
+            }
+          }}
+        >
           <div>
             <label htmlFor="requestName" className="block font-semibold">
               Type
