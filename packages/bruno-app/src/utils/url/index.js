0dd45a5541db1c38f357ff419520a677aede9442
Mateusz Pietryga <pietryga.m@gmail.com>
2024-08-22 20:53:10 +0200

    fix: Failed to construct 'URL': Invalid URL everytime the URL is modified
    A non-parsable URL should be an acceptable state while the text is being typed.

--- packages/bruno-app/src/utils/url/index.js
+++ packages/bruno-app/src/utils/url/index.js
@@ -44,7 +44,8 @@ export const parsePathParams = (url) => {
   try {
     uri = new URL(uri);
   } catch (e) {
-    throw e;
+    // URL is non-parsable, is it incomplete? Ignore.
+    return;
   }
 
   let paths = uri.pathname.split('/');
