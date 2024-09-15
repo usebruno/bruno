7dd639192cb3e9d724b2a88192fd0aca8dd28217
Huynh Tien <huynhqtienvtag@gmail.com>
2024-09-16 01:36:06 +0700

    Support more languages in Generate Code (#2991)
    * generate languages for all targets
    
    * change target client name
    
    * add scroll bar
    
    * remove debug log

---
+++ packages/bruno-app/src/utils/codegenerator/targets.js
@@ -0,0 +1,23 @@
+import { targets } from 'httpsnippet';
+
+export const getLanguages = () => {
+  const allLanguages = [];
+  for (const target of Object.values(targets)) {
+    const { key, title } = target.info;
+    const clients = Object.keys(target.clientsById);
+    const languages =
+      (clients.length === 1)
+        ? [{
+          name: title,
+          target: key,
+          client: clients[0]
+        }]
+        : clients.map(client => ({
+          name: `${title}-${client}`,
+          target: key,
+          client
+        }));
+    allLanguages.push(...languages);
+  }
+  return allLanguages;
+};
