4ae1347493222c2a5695c6ab601abffa48d6c000
Pragadesh-45 <temporaryg7904@gmail.com>
2024-09-19 16:57:20 +0530

    add reference link

--- packages/bruno-app/src/hooks/useFocusTrap/index.js
+++ packages/bruno-app/src/hooks/useFocusTrap/index.js
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from 'react';
 
 const useFocusTrap = (modalRef) => {
+
+  // refer to this implementation for modal focus: https://stackoverflow.com/a/38865836
   const focusableSelector = 'a[href], area[href], input:not([disabled]):not([type="hidden"]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, *[tabindex]:not([tabindex="-1"]), *[contenteditable]';
   
   useEffect(() => {
@@ -21,7 +23,6 @@ const useFocusTrap = (modalRef) => {
     // Set focus to the first focusable element in the modal
     const firstElement = modalFocusableElements[0];
     const lastElement = modalFocusableElements[modalFocusableElements.length - 1];
-    if (firstElement) firstElement.focus();
 
     const handleKeyDown = (event) => {
       if (event.key === 'Tab') {
