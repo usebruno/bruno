fcba69c25189d1c78887c072d20536aeb2da1998
Pragadesh-45 <temporaryg7904@gmail.com>
2024-09-26 10:34:34 +0530

    refactor: code cleanup

--- packages/bruno-electron/src/ipc/collection.js
+++ packages/bruno-electron/src/ipc/collection.js
@@ -340,7 +340,7 @@ const registerRendererEventHandlers = (mainWindow, watcher, lastOpenedCollection
       }
 
       if (!safeToRename(oldPath, newPath)) {
-        throw new Error(`2path: ${newPath} exist`);
+        throw new Error(`path: ${newPath} exist`);
       }
 
       if (isDirectory(oldPath)) {
