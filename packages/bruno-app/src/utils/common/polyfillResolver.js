b2dda436186a2cde96d701c8e5c18bfc4d19cd51
lohxt1 <lohit.jiddimani@gmail.com>
2024-10-10 16:18:39 +0530

    fix: upgrade libraries and code cleanup

---
+++ packages/bruno-app/src/utils/common/polyfillResolver.js
@@ -0,0 +1,23 @@
+export const polyfillResolver = () => {
+  if (typeof Promise.withResolvers === "undefined") {
+    if (typeof window !== 'undefined') {
+      window.Promise.withResolvers = function () {
+        let resolve, reject
+        const promise = new Promise((res, rej) => {
+          resolve = res
+          reject = rej
+        })
+        return { promise, resolve, reject }
+      }
+    } else {
+      global.Promise.withResolvers = function () {
+        let resolve, reject
+        const promise = new Promise((res, rej) => {
+          resolve = res
+          reject = rej
+        })
+        return { promise, resolve, reject }
+      }
+    }
+  }
+}
