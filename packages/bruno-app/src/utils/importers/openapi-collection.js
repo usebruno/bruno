a99dd504f80d004dc3a1255953805060d0127e67
lohxt1 <lohit.jiddimani@gmail.com>
2024-11-06 19:15:35 +0530

    fix: openapi baseUrl env value should remove trailing slash

--- packages/bruno-app/src/utils/importers/openapi-collection.js
+++ packages/bruno-app/src/utils/importers/openapi-collection.js
@@ -316,7 +316,7 @@ const getDefaultUrl = (serverObject) => {
       url = url.replace(`{${variableName}}`, sub);
     });
   }
-  return url.endsWith('/') ? url : `${url}/`;
+  return url.endsWith('/') ? url.slice(0, -1) : url;
 };
 
 const getSecurity = (apiSpec) => {
