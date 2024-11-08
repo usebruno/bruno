d573d6e0a806eabb704478cca3c50beb222892e1
lohxt1 <lohit.jiddimani@gmail.com>
2024-11-08 16:29:44 +0530

    feat: updates

--- packages/bruno-cli/src/runner/run-single-request.js
+++ packages/bruno-cli/src/runner/run-single-request.js
@@ -246,7 +246,7 @@ const runSingleRequest = async function (
             data: null,
             responseTime: 0
           },
-          error: err?.errors?.map(e => e?.message)?.at(0) || err?.code || 'Request Failed!',
+          error: err?.message || err?.errors?.map(e => e?.message)?.at(0) || err?.code || 'Request Failed!',
           assertionResults: [],
           testResults: [],
           nextRequestName: nextRequestName
