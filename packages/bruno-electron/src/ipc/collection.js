9b79765d86a12d221dc3b4bc4eaae016aa43defd
Pragadesh-45 <temporaryg7904@gmail.com>
2024-11-26 19:16:52 +0530

    fix: implement custom pathname validation #2193

--- packages/bruno-electron/src/ipc/collection.js
+++ packages/bruno-electron/src/ipc/collection.js
@@ -73,7 +73,7 @@ const registerRendererEventHandlers = (mainWindow, watcher, lastOpenedCollection
         }
 
         if (!isValidPathname(dirPath)) {
-          throw new Error(`collection: invalid pathname - ${dir}`);
+          throw new Error(`collection: invalid pathname - ${dirPath}`);
         }
 
         if (!fs.existsSync(dirPath)) {
