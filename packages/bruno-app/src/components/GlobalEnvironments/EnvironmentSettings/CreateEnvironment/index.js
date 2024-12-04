4b1fd6fd83eaa94d984370bb6502fb21b6ab5004
Sanjai Kumar <sanjai@usebruno.com>
2024-12-04 16:16:41 +0530

    fix: enhance environment name validation for global environments

--- packages/bruno-app/src/components/GlobalEnvironments/EnvironmentSettings/CreateEnvironment/index.js
+++ packages/bruno-app/src/components/GlobalEnvironments/EnvironmentSettings/CreateEnvironment/index.js
@@ -2,12 +2,18 @@ import React, { useEffect, useRef } from 'react';
 import toast from 'react-hot-toast';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Portal from 'components/Portal';
 import Modal from 'components/Modal';
 import { addGlobalEnvironment } from 'providers/ReduxStore/slices/global-environments';
 
 const CreateEnvironment = ({ onClose }) => {
+  const global_envs = useSelector((state) => state?.globalEnvironments.globalEnvironments);
+  
+  const validateEnvironmentName = (name) => {
+    return !global_envs.some((env) => env?.name?.toLowerCase().trim() === name?.toLowerCase().trim());
+  };
+
   const dispatch = useDispatch();
   const inputRef = useRef();
   const formik = useFormik({
@@ -17,9 +23,10 @@ const CreateEnvironment = ({ onClose }) => {
     },
     validationSchema: Yup.object({
       name: Yup.string()
-        .min(1, 'must be at least 1 character')
-        .max(50, 'must be 50 characters or less')
-        .required('name is required')
+        .min(1, 'Must be at least 1 character')
+        .max(50, 'Must be 50 characters or less')
+        .required('Name is required')
+        .test('duplicate-name', 'Global Environment already exists', validateEnvironmentName)
     }),
     onSubmit: (values) => {
       dispatch(addGlobalEnvironment({ name: values.name }))
