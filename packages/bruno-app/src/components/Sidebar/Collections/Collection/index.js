09dc23389e3ce4afb16342db30d72805f2145d22
naman-bruno <naman@usebruno.com>
2025-01-17 15:03:26 +0530

    updated handleCollapseCollection

--- packages/bruno-app/src/components/Sidebar/Collections/Collection/index.js
+++ packages/bruno-app/src/components/Sidebar/Collections/Collection/index.js
@@ -182,8 +182,6 @@ const Collection = ({ collection, searchText }) => {
       <div className="flex py-1 collection-name items-center" ref={drop}>
         <div
           className="flex flex-grow items-center overflow-hidden"
-          onClick={handleCollapseCollection}
-          onContextMenu={handleRightClick}
         >
           <IconChevronRight
             size={16}
