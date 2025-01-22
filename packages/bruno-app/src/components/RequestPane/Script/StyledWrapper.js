3d6ebe67b736838947a13b0290367527a688a906
Pooja Belaramani <pooja@usebruno.com>
2025-01-22 14:07:58 +0530

    improve: script ui

--- packages/bruno-app/src/components/RequestPane/Script/StyledWrapper.js
+++ packages/bruno-app/src/components/RequestPane/Script/StyledWrapper.js
@@ -1,12 +1,41 @@
 import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
-  div.CodeMirror {
-    height: inherit;
+  height: 100%;
+
+  .script-tabs {
+    display: flex;
+    gap: 1px;
+    padding: 0 1px;
+    border-bottom: 1px solid ${(props) => props.theme.requestTabs.bottomBorder};
   }
 
-  div.title {
-    color: var(--color-tab-inactive);
+  .tab {
+    padding: 8px 16px;
+    font-size: 13px;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    color: ${(props) => props.theme.requestTabs.color};
+    border-bottom: 2px solid transparent;
+    transition: all 0.2s ease;
+    position: relative;
+
+    &:hover {
+      color: ${(props) => props.theme.requestTabs.icon.hoverColor};
+      background: ${(props) => props.theme.requestTabs.icon.hoverBg};
+    }
+
+    &.active {
+      color: ${(props) => props.theme.tabs.active.color};
+      border-bottom: solid 2px ${(props) => props.theme.tabs.active.border};
+      background: ${(props) => props.theme.requestTabs.active.bg};
+    }
+  }
+
+  div.CodeMirror {
+    height: calc(100vh - 290px);
+    background: ${(props) => props.theme.codemirror.bg};
   }
 `;
 
