880ce19f1d58986fc22e73dad2cba7cd1bc14db8
Pooja Belaramani <pooja@usebruno.com>
2025-03-04 17:00:37 +0530

    add: header for apiKey and oauth2 auth

--- packages/bruno-app/src/utils/codegenerator/auth.js
+++ packages/bruno-app/src/utils/codegenerator/auth.js
@@ -24,6 +24,29 @@ export const getAuthHeaders = (collectionRootAuth, requestAuth) => {
           value: `Bearer ${get(auth, 'bearer.token', '')}`
         }
       ];
+    case 'apikey':
+      const apiKeyName = get(auth, 'apikey.key', '');
+      
+      if (!apiKeyName) {
+        return [];
+      }
+      
+      return [
+        {
+          enabled: true,
+          name: apiKeyName,
+          value: get(auth, 'apikey.value', '')
+        }
+      ];
+
+    case 'oauth2':
+      return [
+        {
+          enabled: true,
+          name: 'Authorization',
+          value: `Bearer ${get(auth, 'oauth2.token', '')}`
+        }
+      ];
     default:
       return [];
   }
