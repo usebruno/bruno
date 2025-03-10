9f27ea42715680b93612cb10f02f794a66da51fd
ramki-bruno <ramki@usebruno.com>
2025-03-11 00:16:15 +0530

    Fix: Revert interpreting Assert RHS-value wrapped in quotes literally
    - Revert "Merge pull request #3806 from Pragadesh-45/fix/handle-assert-results"
      - 63d3cb380d02e56753ff9926b5789533d7ddac1f
    - Revert "Merge pull request #3805 from Pragadesh-45/fix/handle-assert-results"
      - 6abd063749926fc66e57b1bdbcabbf2e43ec29ac

--- packages/bruno-js/src/sandbox/quickjs/index.js
+++ packages/bruno-js/src/sandbox/quickjs/index.js
@@ -22,6 +22,12 @@ const toNumber = (value) => {
   return Number.isInteger(num) ? parseInt(value, 10) : parseFloat(value);
 };
 
+const removeQuotes = (str) => {
+  if ((str.startsWith('"') && str.endsWith('"')) || (str.startsWith("'") && str.endsWith("'"))) {
+    return str.slice(1, -1);
+  }
+  return str;
+};
 
 const executeQuickJsVm = ({ script: externalScript, context: externalContext, scriptType = 'template-literal' }) => {
   if (!externalScript?.length || typeof externalScript !== 'string') {
@@ -38,6 +44,7 @@ const executeQuickJsVm = ({ script: externalScript, context: externalContext, sc
   if (externalScript === 'null') return null;
   if (externalScript === 'undefined') return undefined;
 
+  externalScript = removeQuotes(externalScript);
 
   const vm = QuickJSSyncContext;
 
@@ -87,6 +94,7 @@ const executeQuickJsVmAsync = async ({ script: externalScript, context: external
   if (externalScript === 'null') return null;
   if (externalScript === 'undefined') return undefined;
 
+  externalScript = removeQuotes(externalScript);
 
   try {
     const module = await newQuickJSWASMModule();
