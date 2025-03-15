cbb8cce03fd715fdf037800f82575108578221ba
Alfonso Presa <alfonso-presa@users.noreply.github.com>
2025-03-15 12:23:23 +0000

    fix: support vars interpolation on mixed multi-part

--- packages/bruno-cli/src/runner/interpolate-vars.js
+++ packages/bruno-cli/src/runner/interpolate-vars.js
@@ -85,7 +85,7 @@ const interpolateVars = (request, envVariables = {}, runtimeVariables = {}, proc
         });
       } catch (err) {}
     }
-  } else if (contentType === 'multipart/form-data') {
+  } else if (contentType.indexOf('multipart/') === 0) {
     if (Array.isArray(request?.data) && !(request.data instanceof FormData)) {
       try {
         request.data = request?.data?.map(d => ({
