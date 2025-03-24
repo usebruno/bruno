5badaf38b9190d519e9cd0b25262096510502dca
sanish-bruno <sanish@usebruno.com>
2025-03-24 13:57:50 +0530

    fix: update Postman translation for response headers and add test scripts

--- packages/bruno-app/src/utils/importers/translators/postman_translation.js
+++ packages/bruno-app/src/utils/importers/translators/postman_translation.js
@@ -20,7 +20,7 @@ const replacements = {
   'pm\\.response\\.responseTime': 'res.getResponseTime()',
   'pm\\.environment\\.name': 'bru.getEnvName()',
   'pm\\.response\\.status': 'res.statusText',
-  'pm\\.response\\.headers': 'res.headers',
+  'pm\\.response\\.headers': 'req.getHeaders()',
   'pm\\.response\\.size': 'res.getSize',
   "tests\\['([^']+)'\\]\\s*=\\s*([^;]+);": 'test("$1", function() { expect(Boolean($2)).to.be.true; });',
   'pm\\.request\\.url': 'req.getUrl()',
