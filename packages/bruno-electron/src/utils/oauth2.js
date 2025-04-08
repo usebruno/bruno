3a792a021c1e3486a87d77e8957bae961a77a11b
lohxt1 <lohit.jiddimani@gmail.com>
2025-04-08 16:31:27 +0530

    oauth2 refresh token under request pane  creates dup network logs

--- packages/bruno-electron/src/utils/oauth2.js
+++ packages/bruno-electron/src/utils/oauth2.js
@@ -625,6 +625,7 @@ const refreshOauth2Token = async ({ requestCopy, collectionUid, certsAndProxyCon
     requestCopy.data = qs.stringify(data);
     requestCopy.url = url;
     requestCopy.responseType = 'arraybuffer';
+    let debugInfo = { data: [] };
     try {
       const { proxyMode, proxyConfig, httpsAgentRequestFields, interpolationOptions } = certsAndProxyConfig;
       const axiosInstance = makeAxiosInstance({ proxyMode, proxyConfig, httpsAgentRequestFields, interpolationOptions });
