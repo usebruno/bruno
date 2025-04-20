f1c2a622316eb6a632f4e0702c65d530c8a71c8b
lohit jiddimani <lohitjiddimani@lohits-MacBook-Air.local>
2025-04-21 03:35:33 +0530

    removed unsed functions

--- packages/bruno-common/src/reports/html/utils/index.ts
+++ packages/bruno-common/src/reports/html/utils/index.ts
@@ -1,15 +1,3 @@
-export const encodeBase64 = (str: string) => {
-  const bytes = new TextEncoder().encode(str);
-  const binary = bytes.reduce((acc, byte) => acc + String.fromCharCode(byte), '');
-  return btoa(binary);
-}
-
-export const decodeBase64 = (base64: string) => {
-  const binary = atob(base64); // Base64 → binary string
-  const bytes = Uint8Array.from(binary, c => c.charCodeAt(0)); // binary → bytes
-  return new TextDecoder().decode(bytes); // bytes → UTF-8 string
-}
-
 export const getContentType = (headers: Record<string, string | number | undefined>): string => {
   if (!headers || typeof headers !== 'object') {
     return '';
@@ -22,7 +10,6 @@ export const getContentType = (headers: Record<string, string | number | undefin
 export const isHtmlContentType = (contentType: string) => {
   return contentType?.includes("html");
 };
-
 
 export const redactImageData = (data: string | object | number | boolean, contentType: string) => {
   if (contentType?.includes("image")) {
