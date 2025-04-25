8914d66db8d8963c14b295dbf35e2e9803b8e0a0
sanish-bruno <sanish@usebruno.com>
2025-04-25 13:58:51 +0530

    test: add unit tests for scoped variable handling in jscode-shift-translator

---
+++ packages/bruno-converters/src/utils/tests/scoped-variables.test.js
@@ -0,0 +1,51 @@
+import translateCode from '../jscode-shift-translator';
+
+describe('Scoped Variables', () => {
+ it.skip('should handle scoped variables correctly', () => {
+    const code = `
+    const response = pm.response;
+    const status = response.status;
+
+    function test() {
+        const response = delta.response;
+        const status = response.status;
+        console.log(status);
+    }
+    `
+    const result = translateCode(code);
+    console.log(result);
+    expect(result).toBe(`
+    const status = res.statusText;
+
+    function test() {
+        const response = delta.response;
+        const status = response.status;
+        console.log(status);
+    }
+    `)
+  })
+
+  it.skip('should handle scoped variables correctly', () => {
+    const code = `
+    const response = delta.response;
+    const status = response.status;
+
+    function test() {
+        const response = pm.response;
+        const status = response.status;
+        console.log(status);
+    }
+    `
+    const result = translateCode(code);
+    console.log(result);
+    expect(result).toBe(`
+    const response = delta.response;
+    const status = response.status;
+
+    function test() {
+        const status = res.statusText;
+        console.log(status);
+    }
+    `)
+  })
+})
