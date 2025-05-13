0c24655d69b937aab72ed5744e0f6e0a7cb0d04d
Miika Tuominen <miika.km.tuominen@gmail.com>
2025-05-13 13:59:38 +0300

    Fix countSearchResults for regular expressions

--- packages/bruno-app/src/components/CodeEditor/index.js
+++ packages/bruno-app/src/components/CodeEditor/index.js
@@ -427,8 +427,16 @@ export default class CodeEditor extends React.Component {
     const searchInput = document.querySelector('.CodeMirror-search-field');
 
     if (searchInput && searchInput.value.length > 0) {
-      // Escape special characters in search input to prevent RegExp crashes. Fixes #3051
-      const text = new RegExp(escapeRegExp(searchInput.value), 'gi');
+      let text;
+      // CoreMirror5 allows the user to use /re/ syntax for regexp search, so we handle both cases
+      if (searchInput.value.at(0) == "/" && searchInput.value.at(-1) == "/" && searchInput.value.length >= 3) {
+        try {
+          text = new RegExp(searchInput.value.slice(1, -1), "g")
+        } catch {}
+      } else {        
+        // Escape special characters in search input to prevent RegExp crashes. Fixes #3051
+        text = new RegExp(escapeRegExp(searchInput.value), 'gi');
+      }
       const matches = this.editor.getValue().match(text);
       count = matches ? matches.length : 0;
     }
