eaca61f71117f3e449671807a4adb3e2bfb1c359
pooja-bruno <pooja@usebruno.com>
2025-05-21 12:06:05 +0530

    fix

--- packages/bruno-app/src/components/SingleLineEditor/index.js
+++ packages/bruno-app/src/components/SingleLineEditor/index.js
@@ -129,7 +129,7 @@ class SingleLineEditor extends Component {
     }
     if (this.props.value !== prevProps.value && this.props.value !== this.cachedValue && this.editor) {
       this.cachedValue = String(this.props.value);
-      this.editor.setValue(String(this.props.value) || '');
+      this.editor.setValue(String(this.props.value) ?? '');
     }
     if (!isEqual(this.props.isSecret, prevProps.isSecret)) {
       // If the secret flag has changed, update the editor to reflect the change
