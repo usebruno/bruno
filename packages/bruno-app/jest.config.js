a0e14edf1411e250a9a5f1f32464630c4a71c362
sreelakshmi-bruno <sreelakshmi@usebruno.com>
2025-05-23 16:14:22 +0530

    Tests for responseSize component

--- packages/bruno-app/jest.config.js
+++ packages/bruno-app/jest.config.js
@@ -12,5 +12,47 @@ module.exports = {
   },
   clearMocks: true,
   moduleDirectories: ['node_modules', 'src'],
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest'
+  },
+  projects: [
+    {
+      displayName: 'DOM',
+      testMatch: ['<rootDir>/src/components/**/*.{spec,test}.[jt]s?(x)'],
+      testEnvironment: 'jsdom',
+      transform: {
+        '^.+\\.[jt]sx?$': 'babel-jest'
+      },
+      moduleNameMapper: {
+        '^assets/(.*)$': '<rootDir>/src/assets/$1',
+        '^components/(.*)$': '<rootDir>/src/components/$1',
+        '^hooks/(.*)$': '<rootDir>/src/hooks/$1',
+        '^themes/(.*)$': '<rootDir>/src/themes/$1',
+        '^api/(.*)$': '<rootDir>/src/api/$1',
+        '^pageComponents/(.*)$': '<rootDir>/src/pageComponents/$1',
+        '^providers/(.*)$': '<rootDir>/src/providers/$1',
+        '^utils/(.*)$': '<rootDir>/src/utils/$1'
+      }
+    },
+    {
+      displayName: 'NODE',
+      testMatch: ['<rootDir>/src/**/*.{spec,test}.[jt]s?(x)'],
+      testEnvironment: 'node',
+      testPathIgnorePatterns: ['/src/components/'],
+      transform: {
+        '^.+\\.[jt]sx?$': 'babel-jest'
+      },
+      moduleNameMapper: {
+        '^assets/(.*)$': '<rootDir>/src/assets/$1',
+        '^components/(.*)$': '<rootDir>/src/components/$1',
+        '^hooks/(.*)$': '<rootDir>/src/hooks/$1',
+        '^themes/(.*)$': '<rootDir>/src/themes/$1',
+        '^api/(.*)$': '<rootDir>/src/api/$1',
+        '^pageComponents/(.*)$': '<rootDir>/src/pageComponents/$1',
+        '^providers/(.*)$': '<rootDir>/src/providers/$1',
+        '^utils/(.*)$': '<rootDir>/src/utils/$1'
+      }
+    }
+  ]
 };
