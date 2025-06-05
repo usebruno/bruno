0199b32b6c0f85c4d4e129a77c9ae46c3f35b7e5
lohit <lohit@usebruno.com>
2025-06-05 18:16:47 +0530

    Update jest.config.js

--- packages/bruno-app/jest.config.js
+++ packages/bruno-app/jest.config.js
@@ -12,36 +12,14 @@ module.exports = {
   },
   clearMocks: true,
   moduleDirectories: ['node_modules', 'src'],
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest'
   },
   transformIgnorePatterns: [
     '/node_modules/(?!(nanoid|xml-formatter)/)'
   ],
-  projects: [
-    {
-      displayName: 'DOM',
-      testMatch: [
-        '<rootDir>/src/**/*.spec.[jt]s?(x)'
-      ],
-      testEnvironment: 'jsdom',
-      transform: {
-        '^.+\\.[jt]sx?$': 'babel-jest'
-      },
-      transformIgnorePatterns: [
-        '/node_modules/(?!(nanoid|xml-formatter)/)'
-      ],
-      moduleNameMapper: {
-        '^assets/(.*)$': '<rootDir>/src/assets/$1',
-        '^components/(.*)$': '<rootDir>/src/components/$1',
-        '^hooks/(.*)$': '<rootDir>/src/hooks/$1',
-        '^themes/(.*)$': '<rootDir>/src/themes/$1',
-        '^api/(.*)$': '<rootDir>/src/api/$1',
-        '^pageComponents/(.*)$': '<rootDir>/src/pageComponents/$1',
-        '^providers/(.*)$': '<rootDir>/src/providers/$1',
-        '^utils/(.*)$': '<rootDir>/src/utils/$1'
-      }
-    }
+  testMatch: [
+    '<rootDir>/src/**/*.spec.[jt]s?(x)'
   ]
 };
