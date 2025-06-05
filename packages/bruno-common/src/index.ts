debaceec8a812c60f8b56ed44b8d0403fce86d55
sanjai0py <sanjailucifer666@gmail.com>
2025-06-05 11:07:37 +0530

    Refactor HttpMethodSelector to improve input handling and dropdown integration
    Modified custom http method handing in bruToJson and jsonToBru
    
    refactor: added support for custom methods in pm import, fixed tests
    
    refactor: update HttpMethodSelector styling and improve method selection handling
    
    refactor: added review changes
    
    refactor: update HttpMethodSelector to include TRACE and CONNECT methods, enhance input styling
    
    refactor: simplify method assignment in parseCurlCommand to support custom methods
    
    fix: ensure proper syntax in bruToJson.js and maintain consistent formatting in parse-curl.js

--- packages/bruno-common/src/index.ts
+++ packages/bruno-common/src/index.ts
@@ -2,5 +2,6 @@ export { mockDataFunctions } from './utils/faker-functions';
 export { default as interpolate } from './interpolate';
 export { default as isRequestTagsIncluded } from './tags';
 export { default as cookies } from './cookies';
+export { isValidHttpToken } from './utils/http-token';
 
 export * as utils from './utils';
