8086b934a47890582c62dc74e1377d825972583b
sanjai0py <sanjailucifer666@gmail.com>
2025-06-05 11:07:37 +0530

    Refactor HttpMethodSelector to improve input handling and dropdown integration
    Modified custom http method handing in bruToJson and jsonToBru
    
    refactor: added support for custom methods in pm import, fixed tests
    
    refactor: update HttpMethodSelector styling and improve method selection handling
    
    refactor: added review changes
    
    refactor: update HttpMethodSelector to include TRACE and CONNECT methods, enhance input styling
    
    refactor: simplify method assignment in parseCurlCommand to support custom methods
    
    fix: ensure proper syntax in bruToJson.js and maintain consistent formatting in parse-curl.js
    
    refactor: streamline HTTP method handling in jsonToBru function
    
    refactor: update request schema validation tests to use strict boolean checks
    
    Revert "refactor: update request schema validation tests to use strict boolean checks"
    
    This reverts commit b9358bb92122febe4f0d69a517760188662eec2d.
    
    refactor: formatted the code in `HttpMethodSelector` and removed URL check in jsonToBru
    
    refactor: streamline formatting and improve readability in HttpMethodSelector component
    
    refactor: allow any custom HTTP method by removing token validation
    
    Trigger Build

--- packages/bruno-lang/v2/src/bruToJson.js
+++ packages/bruno-lang/v2/src/bruToJson.js
@@ -86,9 +86,8 @@ const grammar = ohm.grammar(`Bru {
   meta = "meta" dictionary
   settings = "settings" dictionary
 
-  http = get | post | put | delete | patch | options | head | connect | trace
+  http = get | post | put | delete | patch | options | head | connect | trace | httpcustom
   grpc = "grpc" dictionary
-
   get = "get" dictionary
   post = "post" dictionary
   put = "put" dictionary
@@ -98,6 +97,7 @@ const grammar = ohm.grammar(`Bru {
   head = "head" dictionary
   connect = "connect" dictionary
   trace = "trace" dictionary
+  httpcustom = "http" dictionary
 
 
   headers = "headers" dictionary
@@ -481,6 +481,26 @@ const sem = grammar.createSemantics().addAttribute('ast', {
       http: {
         method: 'connect',
         ...mapPairListToKeyValPair(dictionary.ast)
+      }
+    };
+  },
+  trace(_1, dictionary) {
+    return {
+      http: {
+        method: 'trace',
+        ...mapPairListToKeyValPair(dictionary.ast)
+      }
+    };
+  },
+  httpcustom(_1, dictionary) {
+    const dict = mapPairListToKeyValPair(dictionary.ast);
+    const method = dict.method;
+    const rest = { ...dict };
+    delete rest.method;
+    return {
+      http: {
+        method,
+        ...rest
       }
     };
   },
