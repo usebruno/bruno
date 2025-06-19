3d56797ab74e8cd6a4a02ebba06d9b957a28dd37
pooja-bruno <pooja@usebruno.com>
2025-06-19 12:35:12 +0530

    fixes

--- packages/bruno-app/src/components/ResponsePane/RunnerTimeline/index.js
+++ packages/bruno-app/src/components/ResponsePane/RunnerTimeline/index.js
@@ -1,45 +1,7 @@
 import React, { useMemo } from 'react';
 import TimelineItem from '../Timeline/TimelineItem';
 import StyledWrapper from './StyledWrapper';
-import { findItemInCollection, findParentItemInCollection } from 'utils/collections';
-import { get } from 'lodash';
-
-const getEffectiveAuthSource = (collection, item) => {
-  const authMode = item.draft ? get(item, 'draft.request.auth.mode') : get(item, 'request.auth.mode');
-  if (authMode !== 'inherit') return null;
-
-  const collectionAuth = get(collection, 'root.request.auth');
-  let effectiveSource = {
-    type: 'collection',
-    uid: collection.uid,
-    auth: collectionAuth,
-  };
-
-  // Build path from collection root to current item
-  let path = [];
-  let currentItem = findItemInCollection(collection, item?.uid);
-  while (currentItem) {
-    path.unshift(currentItem);
-    currentItem = findParentItemInCollection(collection, currentItem?.uid);
-  }
-
-  // Traverse from closest folder upwards to find auth config
-  for (let i of [...path].reverse()) {
-    if (i.type === 'folder') {
-      const folderAuth = get(i, 'root.request.auth');
-      if (folderAuth && folderAuth.mode && folderAuth.mode !== 'none' && folderAuth.mode !== 'inherit') {
-        effectiveSource = {
-          type: 'folder',
-          uid: i.uid,
-          auth: folderAuth,
-        };
-        break;
-      }
-    }
-  }
-
-  return effectiveSource;
-};
+import { getEffectiveAuthSource } from 'utils/collections';
 
 const RunnerTimeline = ({ request = {}, response = {}, item, collection, width }) => {
   const combinedTimeline = useMemo(() => {
