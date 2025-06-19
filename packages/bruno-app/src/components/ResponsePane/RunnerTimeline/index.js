d9960bcc9c7df5c1de4ab431311d763302733267
pooja-bruno <pooja@usebruno.com>
2025-06-19 15:35:29 +0530

    fix

--- packages/bruno-app/src/components/ResponsePane/RunnerTimeline/index.js
+++ packages/bruno-app/src/components/ResponsePane/RunnerTimeline/index.js
@@ -18,6 +18,7 @@ const RunnerTimeline = ({ request, response, item, collection, width }) => {
 
   return (
     <StyledWrapper className="pb-4 w-full">
+      {/* Show the main request/response timeline item */}
       <TimelineItem
         request={request}
         response={response}
@@ -26,6 +27,42 @@ const RunnerTimeline = ({ request, response, item, collection, width }) => {
         width={width}
         hideTimestamp={true}
       />
+      
+      {/* Show OAuth2 debug info if available for this specific request */}
+      {collection.timeline && collection.timeline
+        .filter(event => event.type === 'oauth2' && event.itemUid === item.uid)
+        .map((event, index) => {
+          const { data, timestamp } = event;
+          const { debugInfo } = data;
+          return (
+            <div key={`oauth2-${index}`} className="timeline-event mt-4">
+              <div className="timeline-event-header cursor-pointer flex items-center">
+                <div className="flex items-center">
+                  <span className="font-bold">OAuth2.0 Calls</span>
+                </div>
+              </div>
+              <div className="mt-2">
+                {debugInfo && debugInfo.length > 0 ? (
+                  debugInfo.map((data, idx) => (
+                    <div key={idx} className='ml-4'>
+                      <TimelineItem
+                        timestamp={timestamp}
+                        request={data?.request}
+                        response={data?.response}
+                        item={item}
+                        collection={collection}
+                        width={width - 50}
+                        isOauth2={true}
+                      />
+                    </div>
+                  ))
+                ) : (
+                  <div>No debug information available.</div>
+                )}
+              </div>
+            </div>
+          );
+        })}
     </StyledWrapper>
   );
 };
--- packages/bruno-electron/src/ipc/network/index.js
+++ packages/bruno-electron/src/ipc/network/index.js
@@ -1111,7 +1111,9 @@ const registerNetworkIpc = (mainWindow) => {
                 credentials: request?.oauth2Credentials?.credentials,
                 url: request?.oauth2Credentials?.url,
                 collectionUid,
-                credentialsId: request?.oauth2Credentials?.credentialsId
+                credentialsId: request?.oauth2Credentials?.credentialsId,
+                ...(request?.oauth2Credentials?.folderUid ? { folderUid: request.oauth2Credentials.folderUid } : { itemUid: item.uid }),
+                debugInfo: request?.oauth2Credentials?.debugInfo,
               });
             }
 
@@ -1158,7 +1160,8 @@ const registerNetworkIpc = (mainWindow) => {
                   dataBuffer: dataBuffer.toString('base64'),
                   size: Buffer.byteLength(dataBuffer),
                   data: response.data,
-                  responseTime: response.headers.get('request-duration')
+                  responseTime: response.headers.get('request-duration'),
+                  timeline: response.timeline
                 },
                 ...eventData
               });
@@ -1176,7 +1179,8 @@ const registerNetworkIpc = (mainWindow) => {
                   dataBuffer: dataBuffer.toString('base64'),
                   size: Buffer.byteLength(dataBuffer),
                   data: error.response.data,
-                  responseTime: error.response.headers.get('request-duration')
+                  responseTime: error.response.headers.get('request-duration'),
+                  timeline: error.response.timeline
                 };
 
                 // if we get a response from the server, we consider it as a success
