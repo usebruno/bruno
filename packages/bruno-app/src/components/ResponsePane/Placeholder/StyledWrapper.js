bacd6ba33429756fc75583868ae2568701db955e
Anoop M D <anoop.md1421@gmail.com>
2025-06-22 19:12:33 +0530

    feat: implement vertical layout for response pane and enhance drag

--- packages/bruno-app/src/components/ResponsePane/Placeholder/StyledWrapper.js
+++ packages/bruno-app/src/components/ResponsePane/Placeholder/StyledWrapper.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   padding-top: 20%;
   width: 100%;
 
   .send-icon {
     color: ${(props) => props.theme.requestTabPanel.responseSendIcon};
+  }
+
+  &.vertical-layout {
+    padding: 1rem;
+    justify-content: center;
   }
 `;
 
