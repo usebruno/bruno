89442ed8e6c21dff7279216c5b20f5b5e63753f2
sanjai0py <sanjailucifer666@gmail.com>
2025-06-22 17:03:08 +0530

    refactor: enhance query and request header management with bulk editing capabilities

--- packages/bruno-app/src/components/RequestPane/QueryParams/index.js
+++ packages/bruno-app/src/components/RequestPane/QueryParams/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import get from 'lodash/get';
+import cloneDeep from 'lodash/cloneDeep';
 import InfoTip from 'components/InfoTip';
-import { useDispatch } from 'react-redux';
+import { IconTrash } from '@tabler/icons';
+import { useDispatch, useSelector } from 'react-redux';
 import { useTheme } from 'providers/Theme';
 import {
   addQueryParam,
@@ -15,135 +17,150 @@ import SingleLineEditor from 'components/SingleLineEditor';
 import { saveRequest, sendRequest } from 'providers/ReduxStore/slices/collections/actions';
 import StyledWrapper from './StyledWrapper';
 import Table from 'components/Table/index';
-import BulkEditableTable from 'components/BulkEditableTable';
+import ReorderTable from 'components/ReorderTable';
+import CodeEditor from 'components/CodeEditor';
+import { parseBulkKeyValue, serializeBulkKeyValue } from '../../../utils/common/editor';
 
-const parseBulkQueryParams = (value) => {
-  return value
-    .split(/\r?\n/)
-    .map((pair) => {
-      const isEnabled = !pair.trim().startsWith('//');
-      const cleanPair = pair.replace(/^\/\/\s*/, '');
-      const sep = cleanPair.indexOf(':');
-      if (sep < 0) return null;
-      return {
-        name: cleanPair.slice(0, sep).trim(),
-        value: cleanPair.slice(sep + 1).trim(),
-        enabled: isEnabled,
-        type: 'query'
-      };
-    })
-    .filter(Boolean);
-};
-
-const serializeBulkQueryParams = (params) =>
-  params
-    .map((param) => `${param.enabled ? '' : '//'}${param.name}:${param.value}`)
-    .join('\n');
+const parseBulkQueryParams = (value) =>
+  parseBulkKeyValue(value).map((item) => ({ ...item, type: 'query' }));
 
 const QueryParams = ({ item, collection }) => {
   const dispatch = useDispatch();
-  const { displayedTheme } = useTheme();
+  const { storedTheme } = useTheme();
+  const preferences = useSelector((state) => state.app.preferences);
   const params = item.draft ? get(item, 'draft.request.params') : get(item, 'request.params');
   const queryParams = params.filter((param) => param.type === 'query');
   const pathParams = params.filter((param) => param.type === 'path');
+  const [bulkEdit, setBulkEdit] = useState(false);
+  const [bulkText, setBulkText] = useState('');
+
+  const handleAddQueryParam = () => {
+    dispatch(
+      addQueryParam({
+        itemUid: item.uid,
+        collectionUid: collection.uid
+      })
+    );
+  };
 
   const onSave = () => dispatch(saveRequest(item.uid, collection.uid));
   const handleRun = () => dispatch(sendRequest(item, collection.uid));
 
-  const handleAddQueryParam = () => {
-    dispatch(addQueryParam({ itemUid: item.uid, collectionUid: collection.uid }));
+  const handleQueryParamChange = (e, data, key) => {
+    let value;
+    switch (key) {
+      case 'name': {
+        value = e.target.value;
+        break;
+      }
+      case 'value': {
+        value = e.target.value;
+        break;
+      }
+      case 'enabled': {
+        value = e.target.checked;
+        break;
+      }
+    }
+    let queryParam = cloneDeep(data);
+    if (queryParam[key] === value) {
+      return;
+    }
+    queryParam[key] = value;
+    dispatch(
+      updateQueryParam({
+        queryParam,
+        itemUid: item.uid,
+        collectionUid: collection.uid
+      })
+    );
   };
 
-  const handleEditQueryParam = (param, key, value) => {
-    const updated = { ...param, [key]: value };
-    dispatch(updateQueryParam({ queryParam: updated, itemUid: item.uid, collectionUid: collection.uid }));
+  const handlePathParamChange = (e, data) => {
+    let value = e.target.value;
+    let pathParam = cloneDeep(data);
+    if (pathParam['value'] === value) {
+      return;
+    }
+    pathParam['value'] = value;
+    dispatch(
+      updatePathParam({
+        pathParam,
+        itemUid: item.uid,
+        collectionUid: collection.uid
+      })
+    );
   };
 
   const handleRemoveQueryParam = (param) => {
-    dispatch(deleteQueryParam({ paramUid: param.uid, itemUid: item.uid, collectionUid: collection.uid }));
+    dispatch(
+      deleteQueryParam({
+        paramUid: param.uid,
+        itemUid: item.uid,
+        collectionUid: collection.uid
+      })
+    );
   };
 
-  const handleEnableQueryParam = (param, enabled) => {
-    const updated = { ...param, enabled };
-    dispatch(updateQueryParam({ queryParam: updated, itemUid: item.uid, collectionUid: collection.uid }));
+  const handleQueryParamDrag = ({ updateReorderedItem }) => {
+    dispatch(
+      moveQueryParam({
+        collectionUid: collection.uid,
+        itemUid: item.uid,
+        updateReorderedItem
+      })
+    );
   };
 
-  const handleReorderQueryParam = ({ updateReorderedItem }) => {
-    dispatch(moveQueryParam({ collectionUid: collection.uid, itemUid: item.uid, updateReorderedItem }));
+  const handleBulkEdit = (value) => {
+    setBulkText(value);
+    const parsed = parseBulkQueryParams(value);
+    dispatch(setQueryParams({ collectionUid: collection.uid, itemUid: item.uid, params: parsed }));
   };
 
-  const handleBulkSetQueryParams = (params) => {
-    dispatch(setQueryParams({ collectionUid: collection.uid, itemUid: item.uid, params }));
+  const toggleBulkEdit = () => {
+    if (!bulkEdit) {
+      setBulkText(serializeBulkKeyValue(queryParams));
+    }
+    setBulkEdit(!bulkEdit);
   };
+
+  if (bulkEdit) {
+    return (
+      <StyledWrapper className="w-full h-full flex flex-col absolute">
+        <div className="h-[200px]">
+          <CodeEditor
+            mode="text/plain"
+            theme={storedTheme}
+            font={preferences.codeFont || 'default'}
+            value={bulkText}
+            onEdit={handleBulkEdit}
+          />
+        </div>
+        <div className="flex justify-between items-center mt-3">
+          <button className="text-link select-none ml-auto" onClick={toggleBulkEdit}>
+            Key/Value Edit
+          </button>
+        </div>
+      </StyledWrapper>
+    );
+  }
 
   return (
-    <StyledWrapper className="w-full h-full flex flex-col absolute">
-      <div className="flex-1">
-        <div>
-          <div className="mt-3 mb-1 title text-xs">Query</div>
-          <BulkEditableTable
-            items={queryParams}
-            columns={[
-              { name: 'Name', accessor: 'name', width: '30%' },
-              { name: 'Value', accessor: 'value', width: '60%' },
-              { name: '', accessor: '', width: '10%' }
-            ]}
-            bulkLabel="Param"
-            onAdd={handleAddQueryParam}
-            onEdit={handleEditQueryParam}
-            onRemove={handleRemoveQueryParam}
-            onEnable={handleEnableQueryParam}
-            onReorder={handleReorderQueryParam}
-            onBulkSet={handleBulkSetQueryParams}
-            parseBulk={parseBulkQueryParams}
-            serializeBulk={serializeBulkQueryParams}
-            renderNameEditor={(param) => (
-              <input
-                type="text"
-                autoComplete="off"
-                autoCorrect="off"
-                autoCapitalize="off"
-                spellCheck="false"
-                value={param.name}
-                className="mousetrap"
-                onChange={(e) => handleEditQueryParam(param, 'name', e.target.value)}
-              />
-            )}
-            renderValueEditor={(param) => (
-              <SingleLineEditor
-                value={param.value}
-                theme={displayedTheme}
-                onSave={onSave}
-                onChange={(newValue) => handleEditQueryParam(param, 'value', newValue)}
-                onRun={handleRun}
-                collection={collection}
-                variablesAutocomplete={true}
-              />
-            )}
-          />
-          {pathParams && pathParams.length > 0 && (
-            <div className="mt-6">
-              <div className="mb-2 title text-xs flex items-stretch">
-                <span>Path</span>
-                <InfoTip infotipId="path-param-InfoTip">
-                  <div>
-                    Path variables are automatically added whenever the
-                    <code className="font-mono mx-2">:name</code>
-                    template is used in the URL. <br /> For example:
-                    <code className="font-mono mx-2">
-                      https://example.com/v1/users/<span>:id</span>
-                    </code>
-                  </div>
-                </InfoTip>
-              </div>
-              <Table
-                headers={[
-                  { name: 'Name', accessor: 'name', width: '40%' },
-                  { name: 'Value', accessor: 'value', width: '60%' }
-                ]}
-              >
-                {pathParams.map((path) => (
-                  <tr key={path.uid}>
+    <StyledWrapper className="w-full flex flex-col absolute">
+      <div className="flex-1 mt-2">
+        <div className="mb-1 title text-xs">Query</div>
+        <Table
+          headers={[
+            { name: 'Name', accessor: 'name', width: '31%' },
+            { name: 'Path', accessor: 'path', width: '56%' },
+            { name: '', accessor: '', width: '13%' }
+          ]}
+        >
+          <ReorderTable updateReorderedItem={handleQueryParamDrag}>
+            {queryParams && queryParams.length
+              ? queryParams.map((param, index) => (
+                  <tr key={param.uid} data-uid={param.uid}>
                     <td className="flex relative">
                       <input
                         type="text"
@@ -151,30 +168,111 @@ const QueryParams = ({ item, collection }) => {
                         autoCorrect="off"
                         autoCapitalize="off"
                         spellCheck="false"
-                        value={path.name}
-                        className="mousetrap w-full"
-                        readOnly={true}
+                        value={param.name}
+                        className="mousetrap"
+                        onChange={(e) => handleQueryParamChange(e, param, 'name')}
                       />
                     </td>
                     <td>
                       <SingleLineEditor
-                        value={path.value}
-                        theme={displayedTheme}
+                        value={param.value}
+                        theme={storedTheme}
                         onSave={onSave}
-                        onChange={(newValue) =>
-                          dispatch(updatePathParam({ pathParam: { ...path, value: newValue }, itemUid: item.uid, collectionUid: collection.uid }))
-                        }
+                        onChange={(newValue) => handleQueryParamChange({ target: { value: newValue } }, param, 'value')}
                         onRun={handleRun}
                         collection={collection}
-                        item={item}
+                        variablesAutocomplete={true}
                       />
                     </td>
+                    <td>
+                      <div className="flex items-center justify-center">
+                        <input
+                          type="checkbox"
+                          checked={param.enabled}
+                          tabIndex="-1"
+                          className="mr-3 mousetrap"
+                          onChange={(e) => handleQueryParamChange(e, param, 'enabled')}
+                        />
+                        <button tabIndex="-1" onClick={() => handleRemoveQueryParam(param)}>
+                          <IconTrash strokeWidth={1.5} size={20} />
+                        </button>
+                      </div>
+                    </td>
                   </tr>
-                ))}
-              </Table>
+                ))
+              : null}
+          </ReorderTable>
+        </Table>
+        <button className="btn-add-param text-link pr-2 py-3 mt-2 select-none" onClick={handleAddQueryParam}>
+          +&nbsp;<span>Add Param</span>
+        </button>
+        <button className="text-link select-none ml-auto mt-2" onClick={toggleBulkEdit}>
+          Bulk Edit
+        </button>
+        <div className="mb-2 title text-xs flex items-stretch">
+          <span>Path</span>
+          <InfoTip infotipId="path-param-InfoTip">
+            <div>
+              Path variables are automatically added whenever the
+              <code className="font-mono mx-2">:name</code>
+              template is used in the URL. <br/> For example:
+              <code className="font-mono mx-2">
+                https://example.com/v1/users/<span>:id</span>
+              </code>
             </div>
-          )}
+          </InfoTip>
         </div>
+        <table>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Value</td>
+            </tr>
+          </thead>
+          <tbody>
+            {pathParams && pathParams.length
+              ? pathParams.map((path, index) => {
+                  return (
+                    <tr key={path.uid}>
+                      <td>
+                        <input
+                          type="text"
+                          autoComplete="off"
+                          autoCorrect="off"
+                          autoCapitalize="off"
+                          spellCheck="false"
+                          value={path.name}
+                          className="mousetrap"
+                          readOnly={true}
+                        />
+                      </td>
+                      <td>
+                        <SingleLineEditor
+                          value={path.value}
+                          theme={storedTheme}
+                          onSave={onSave}
+                          onChange={(newValue) =>
+                            handlePathParamChange(
+                              {
+                                target: {
+                                  value: newValue
+                                }
+                              },
+                              path
+                            )
+                          }
+                          onRun={handleRun}
+                          collection={collection}
+                          item={item}
+                        />
+                      </td>
+                    </tr>
+                  );
+                })
+              : null}
+          </tbody>
+        </table>
+        {!(pathParams && pathParams.length) ? <div className="title pr-2 py-3 mt-2 text-xs"></div> : null}
       </div>
     </StyledWrapper>
   );
--- packages/bruno-app/src/components/RequestPane/RequestHeaders/index.js
+++ packages/bruno-app/src/components/RequestPane/RequestHeaders/index.js
@@ -1,125 +1,205 @@
 import React, { useState } from 'react';
 import get from 'lodash/get';
+import cloneDeep from 'lodash/cloneDeep';
+import { IconTrash } from '@tabler/icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTheme } from 'providers/Theme';
-import {
-  addRequestHeader,
-  updateRequestHeader,
-  deleteRequestHeader,
-  moveRequestHeader,
-  setRequestHeaders
-} from 'providers/ReduxStore/slices/collections';
+import { addRequestHeader, updateRequestHeader, deleteRequestHeader, moveRequestHeader, setRequestHeaders } from 'providers/ReduxStore/slices/collections';
 import { sendRequest, saveRequest } from 'providers/ReduxStore/slices/collections/actions';
 import SingleLineEditor from 'components/SingleLineEditor';
 import StyledWrapper from './StyledWrapper';
 import { headers as StandardHTTPHeaders } from 'know-your-http-well';
-import BulkEditableTable from 'components/BulkEditableTable';
+import { MimeTypes } from 'utils/codemirror/autocompleteConstants';
+import Table from 'components/Table/index';
+import ReorderTable from 'components/ReorderTable/index';
+import CodeEditor from 'components/CodeEditor';
+import { parseBulkKeyValue, serializeBulkKeyValue } from '../../../utils/common/editor';
 
 const headerAutoCompleteList = StandardHTTPHeaders.map((e) => e.header);
-import { MimeTypes } from 'utils/codemirror/autocompleteConstants';
-
-const parseBulkHeaders = (value) => {
-  return value
-    .split(/\r?\n/)
-    .map((pair) => {
-      const isEnabled = !pair.trim().startsWith('//');
-      const cleanPair = pair.replace(/^\/\/\s*/, '');
-      const sep = cleanPair.indexOf(':');
-      if (sep < 0) return null;
-      return {
-        name: cleanPair.slice(0, sep).trim(),
-        value: cleanPair.slice(sep + 1).trim(),
-        enabled: isEnabled
-      };
-    })
-    .filter(Boolean);
-};
-
-const serializeBulkHeaders = (headers) =>
-  headers
-    .map((header) => `${header.enabled ? '' : '//'}${header.name}:${header.value}`)
-    .join('\n');
 
 const RequestHeaders = ({ item, collection }) => {
   const dispatch = useDispatch();
-  const { displayedTheme } = useTheme();
+  const { storedTheme } = useTheme();
   const preferences = useSelector((state) => state.app.preferences);
   const headers = item.draft ? get(item, 'draft.request.headers') : get(item, 'request.headers');
+  const [bulkEdit, setBulkEdit] = useState(false);
+  const [bulkText, setBulkText] = useState('');
+
+  const addHeader = () => {
+    dispatch(
+      addRequestHeader({
+        itemUid: item.uid,
+        collectionUid: collection.uid
+      })
+    );
+  };
 
   const onSave = () => dispatch(saveRequest(item.uid, collection.uid));
   const handleRun = () => dispatch(sendRequest(item, collection.uid));
-
-  const handleAddHeader = () => {
-    dispatch(addRequestHeader({ itemUid: item.uid, collectionUid: collection.uid }));
-  };
-
-  const handleEditHeader = (header, key, value) => {
-    const updated = { ...header, [key]: value };
-    dispatch(updateRequestHeader({ header: updated, itemUid: item.uid, collectionUid: collection.uid }));
+  const handleHeaderValueChange = (e, _header, type) => {
+    const header = cloneDeep(_header);
+    switch (type) {
+      case 'name': {
+        header.name = e.target.value;
+        break;
+      }
+      case 'value': {
+        header.value = e.target.value;
+        break;
+      }
+      case 'enabled': {
+        header.enabled = e.target.checked;
+        break;
+      }
+    }
+    dispatch(
+      updateRequestHeader({
+        header: header,
+        itemUid: item.uid,
+        collectionUid: collection.uid
+      })
+    );
   };
 
   const handleRemoveHeader = (header) => {
-    dispatch(deleteRequestHeader({ headerUid: header.uid, itemUid: item.uid, collectionUid: collection.uid }));
+    dispatch(
+      deleteRequestHeader({
+        headerUid: header.uid,
+        itemUid: item.uid,
+        collectionUid: collection.uid
+      })
+    );
   };
 
-  const handleEnableHeader = (header, enabled) => {
-    const updated = { ...header, enabled };
-    dispatch(updateRequestHeader({ header: updated, itemUid: item.uid, collectionUid: collection.uid }));
+  const handleHeaderDrag = ({ updateReorderedItem }) => {
+    dispatch(
+      moveRequestHeader({
+        collectionUid: collection.uid,
+        itemUid: item.uid,
+        updateReorderedItem
+      })
+    );
   };
 
-  const handleReorderHeader = ({ updateReorderedItem }) => {
-    dispatch(moveRequestHeader({ collectionUid: collection.uid, itemUid: item.uid, updateReorderedItem }));
+  const handleBulkEdit = (value) => {
+    setBulkText(value);
+    const parsed = parseBulkKeyValue(value);
+    dispatch(setRequestHeaders({ collectionUid: collection.uid, itemUid: item.uid, headers: parsed }));
   };
 
-  const handleBulkSetHeaders = (headers) => {
-    dispatch(setRequestHeaders({ collectionUid: collection.uid, itemUid: item.uid, headers }));
+  const toggleBulkEdit = () => {
+    if (!bulkEdit) {
+      setBulkText(serializeBulkKeyValue(headers));
+    }
+    setBulkEdit(!bulkEdit);
   };
+
+  if (bulkEdit) {
+    return (
+      <StyledWrapper className="w-full">
+        <div className="h-[200px]">
+          <CodeEditor
+            mode="text/plain"
+            theme={storedTheme}
+            font={preferences.codeFont || 'default'}
+            value={bulkText}
+            onEdit={handleBulkEdit}
+          />
+        </div>
+        <div className="flex justify-between items-center mt-3">
+          <button className="text-link select-none ml-auto" onClick={toggleBulkEdit}>
+            Key/Value Edit
+          </button>
+        </div>
+      </StyledWrapper>
+    );
+  }
 
   return (
-    <StyledWrapper className="w-full h-full">
-      <BulkEditableTable
-        items={headers}
-        columns={[
-          { name: 'Key', accessor: 'key', width: '30%' },
-          { name: 'Value', accessor: 'value', width: '60%' },
-          { name: '', accessor: '', width: '10%' }
+    <StyledWrapper className="w-full">
+      <Table
+        headers={[
+          { name: 'Key', accessor: 'key', width: '34%' },
+          { name: 'Value', accessor: 'value', width: '46%' },
+          { name: '', accessor: '', width: '20%' }
         ]}
-        bulkLabel="Header"
-        onAdd={handleAddHeader}
-        onEdit={handleEditHeader}
-        onRemove={handleRemoveHeader}
-        onEnable={handleEnableHeader}
-        onReorder={handleReorderHeader}
-        onBulkSet={handleBulkSetHeaders}
-        parseBulk={parseBulkHeaders}
-        serializeBulk={serializeBulkHeaders}
-        renderNameEditor={(header) => (
-          <SingleLineEditor
-            value={header.name}
-            theme={displayedTheme}
-            onSave={onSave}
-            onChange={(newValue) => handleEditHeader(header, 'name', newValue)}
-            autocomplete={headerAutoCompleteList}
-            onRun={handleRun}
-            collection={collection}
-          />
-        )}
-        renderValueEditor={(header) => (
-          <SingleLineEditor
-            value={header.value}
-            theme={displayedTheme}
-            onSave={onSave}
-            onChange={(newValue) => handleEditHeader(header, 'value', newValue)}
-            onRun={handleRun}
-            autocomplete={MimeTypes}
-            allowNewlines={true}
-            collection={collection}
-            item={item}
-          />
-        )}
-      />
+      >
+        <ReorderTable updateReorderedItem={handleHeaderDrag}>
+        {headers && headers.length
+            ? headers.map((header) => {
+                return (
+                  <tr key={header.uid} data-uid={header.uid}>
+                    <td className='flex relative'>
+                      <SingleLineEditor
+                        value={header.name}
+                        theme={storedTheme}
+                        onSave={onSave}
+                        onChange={(newValue) =>
+                          handleHeaderValueChange(
+                            {
+                              target: {
+                                value: newValue
+                              }
+                            },
+                            header,
+                            'name'
+                          )
+                        }
+                        autocomplete={headerAutoCompleteList}
+                        onRun={handleRun}
+                        collection={collection}
+                      />
+                    </td>
+                    <td>
+                      <SingleLineEditor
+                        value={header.value}
+                        theme={storedTheme}
+                        onSave={onSave}
+                        onChange={(newValue) =>
+                          handleHeaderValueChange(
+                            {
+                              target: {
+                                value: newValue
+                              }
+                            },
+                            header,
+                            'value'
+                          )
+                        }
+                        onRun={handleRun}
+                        autocomplete={MimeTypes}
+                        allowNewlines={true}
+                        collection={collection}
+                        item={item}
+                      />
+                    </td>
+                    <td>
+                      <div className="flex items-center">
+                        <input
+                          type="checkbox"
+                          checked={header.enabled}
+                          tabIndex="-1"
+                          className="mr-3 mousetrap"
+                          onChange={(e) => handleHeaderValueChange(e, header, 'enabled')}
+                        />
+                        <button tabIndex="-1" onClick={() => handleRemoveHeader(header)}>
+                          <IconTrash strokeWidth={1.5} size={20} />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })
+            : null}
+        </ReorderTable>
+      </Table>
+      <button className="btn-add-header text-link pr-2 py-3 mt-2 select-none" onClick={addHeader}>
+        + Add Header
+      </button>
+      <button className="text-link select-none ml-auto mt-2" onClick={toggleBulkEdit}>
+        Bulk Edit
+      </button>
     </StyledWrapper>
   );
 };
-
 export default RequestHeaders;
