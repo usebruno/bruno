64ce10c7e8a843533cdfcadb89e1312b64244911
Stephen Breakey <stephen.breakey@yoco.com>
2025-06-27 16:05:21 +0200

    feat(environments): basic support for env specific .env files

--- packages/bruno-electron/src/store/process-env.js
+++ packages/bruno-electron/src/store/process-env.js
@@ -1,34 +1,41 @@
 /**
  * This file stores all the process.env variables under collection scope
  *
- * process.env variables are sourced from 2 places:
- * 1. .env file in the root of the project
- * 2. process.env variables set in the OS
+ * process.env variables are sourced in the following order, where each source overrides the previous one:
+ * 1. process.env variables set in the OS, applied as global vars for the collection
+ * 2. .env file in the root of the project, applied as global vars for the collection
+ * 3. .env.{env-name} in the root of the project, applied as environment specific vars for the collection
  *
  * Multiple collections can be opened in the same electron app.
- * Each collection's .env file can have different values for the same process.env variable.
+ * Each collection's .env files can have different values for the same process.env variable.
  */
 
 const dotEnvVars = {};
 
 // collectionUid is a hash based on the collection path
-const getProcessEnvVars = (collectionUid) => {
-  // if there are no .env vars for this collection, return the process.env
-  if (!dotEnvVars[collectionUid]) {
-    return {
-      ...process.env
-    };
-  }
-
-  // if there are .env vars for this collection, return the process.env merged with the .env vars
-  return {
+const getProcessEnvVars = (collectionUid, envName) => {
+  // return the process.env merged with the global .env vars (if any) and the env specific .env vars (if any)
+  const envVars = {
     ...process.env,
-    ...dotEnvVars[collectionUid]
+    ...dotEnvVars[collectionUid].global,
+    ...dotEnvVars[collectionUid].envs[envName]
   };
+  return envVars;
 };
 
-const setDotEnvVars = (collectionUid, envVars) => {
-  dotEnvVars[collectionUid] = envVars;
+const setDotEnvVars = (collectionUid, envName, envVars) => {
+  // Initialize collection if it doesn't exist
+  if (!dotEnvVars[collectionUid]) {
+    dotEnvVars[collectionUid] = { global: {}, envs: {} };
+  }
+  
+  if (envName === null) {
+    // Set global environment variables
+    dotEnvVars[collectionUid].global = envVars;
+  } else {
+    // Set environment-specific variables
+    dotEnvVars[collectionUid].envs[envName] = envVars;
+  }
 };
 
 module.exports = {
