32225a03964455a4ca25405710bf482ada03cd1c
pradyumnac26 <pradyumnac.ec17@rvce.edu.in>
2025-07-02 11:38:17 -0600

    param table changes

--- packages/bruno-app/src/components/RequestPane/QueryParams/index.js
+++ packages/bruno-app/src/components/RequestPane/QueryParams/index.js
@@ -146,9 +146,9 @@ const QueryParams = ({ item, collection }) => {
         <div className="mb-1 title text-xs">Query</div>
         <Table
           headers={[
-            { name: 'Name', accessor: 'name', width: '31%' },
-            { name: 'Path', accessor: 'path', width: '56%' },
-            { name: '', accessor: '', width: '13%' }
+            { name: 'Name', accessor: 'name', width: '34%' },
+            { name: 'Path', accessor: 'path', width: '46%' },
+            { name: '', accessor: '', width: '20%' }
           ]}
         >
           <ReorderTable updateReorderedItem={handleQueryParamDrag}>
@@ -178,8 +178,8 @@ const QueryParams = ({ item, collection }) => {
                         variablesAutocomplete={true}
                       />
                     </td>
-                    <td>
-                      <div className="flex items-center">
+                    <td >
+                      <div className="flex items-center justify-center">
                         <input
                           type="checkbox"
                           checked={param.enabled}
