38e0b0d11ea6aa38584233cdb5347b471c002948
pooja-bruno <pooja@usebruno.com>
2025-07-04 14:15:25 +0530

    improve: runner timeline

--- packages/bruno-app/src/components/ResponsePane/RunnerTimeline/index.js
+++ packages/bruno-app/src/components/ResponsePane/RunnerTimeline/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import forOwn from 'lodash/forOwn';
 import StyledWrapper from './StyledWrapper';
 import TimelineItem from '../Timeline/TimelineItem';
 
-const RunnerTimeline = ({ request, response, item, collection, width }) => {
+const RunnerTimeline = ({ request = {}, response = {}, item, collection, width }) => {
   const requestHeaders = [];
-
-  request = request || {};
-  response = response || {};
 
   forOwn(request.headers, (value, key) => {
     requestHeaders.push({
@@ -15,6 +12,14 @@ const RunnerTimeline = ({ request, response, item, collection, width }) => {
       value
     });
   });
+
+  const oauth2Events = useMemo(
+    () =>
+      collection?.timeline?.filter(
+        (event) => event.type === 'oauth2' && event.itemUid === item.uid
+      ) || [],
+    [collection?.timeline, item.uid]
+  );
 
   return (
     <StyledWrapper className="pb-4 w-full">
@@ -29,40 +34,38 @@ const RunnerTimeline = ({ request, response, item, collection, width }) => {
       />
       
       {/* Show OAuth2 debug info if available for this specific request */}
-      {collection.timeline && collection.timeline
-        .filter(event => event.type === 'oauth2' && event.itemUid === item.uid)
-        .map((event, index) => {
-          const { data, timestamp } = event;
-          const { debugInfo } = data;
-          return (
-            <div key={`oauth2-${index}`} className="timeline-event mt-4">
-              <div className="timeline-event-header cursor-pointer flex items-center">
-                <div className="flex items-center">
-                  <span className="font-bold">OAuth2.0 Calls</span>
-                </div>
-              </div>
-              <div className="mt-2">
-                {debugInfo && debugInfo.length > 0 ? (
-                  debugInfo.map((data, idx) => (
-                    <div key={idx} className='ml-4'>
-                      <TimelineItem
-                        timestamp={timestamp}
-                        request={data?.request}
-                        response={data?.response}
-                        item={item}
-                        collection={collection}
-                        width={width - 50}
-                        isOauth2={true}
-                      />
-                    </div>
-                  ))
-                ) : (
-                  <div>No debug information available.</div>
-                )}
+      {oauth2Events.map((event, index) => {
+        const { data, timestamp } = event;
+        const { debugInfo } = data;
+        return (
+          <div key={`oauth2-${index}`} className="timeline-event mt-4">
+            <div className="timeline-event-header cursor-pointer flex items-center">
+              <div className="flex items-center">
+                <span className="font-bold">OAuth2.0 Calls</span>
               </div>
             </div>
-          );
-        })}
+            <div className="mt-2">
+              {debugInfo && debugInfo.length > 0 ? (
+                debugInfo.map((data, idx) => (
+                  <div key={idx} className='ml-4'>
+                    <TimelineItem
+                      timestamp={timestamp}
+                      request={data?.request}
+                      response={data?.response}
+                      item={item}
+                      collection={collection}
+                      width={width - 50}
+                      isOauth2={true}
+                    />
+                  </div>
+                ))
+              ) : (
+                <div>No debug information available.</div>
+              )}
+            </div>
+          </div>
+        );
+      })}
     </StyledWrapper>
   );
 };
