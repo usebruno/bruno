692d29aa35329b7a408453b74d9b375dd92d51f7
Pooja <pooja@usebruno.com>
2025-07-11 13:58:25 +0530

    fix: code generator headers and multipartForm bug (#5056)
    * fix: code generator headers and multipartForm bug

--- packages/bruno-app/src/components/Sidebar/Collections/Collection/CollectionItem/GenerateCodeItem/utils/interpolation.js
+++ packages/bruno-app/src/components/Sidebar/Collections/Collection/CollectionItem/GenerateCodeItem/utils/interpolation.js
@@ -43,20 +43,24 @@ export const interpolateBody = (body, variables = {}) => {
       break;
 
     case 'formUrlEncoded':
-      interpolatedBody.formUrlEncoded = body.formUrlEncoded.map((param) => ({
-        ...param,
-        value: param.enabled ? interpolate(param.value, variables) : param.value
-      }));
+      interpolatedBody.formUrlEncoded = Array.isArray(body.formUrlEncoded) 
+        ? body.formUrlEncoded.map((param) => ({
+            ...param,
+            value: param.enabled ? interpolate(param.value, variables) : param.value
+          }))
+        : [];
       break;
 
     case 'multipartForm':
-      interpolatedBody.multipartForm = body.multipartForm.map((param) => ({
-        ...param,
-        value:
-          param.type === 'text' && param.enabled
-            ? interpolate(param.value, variables)
-            : param.value
-      }));
+      interpolatedBody.multipartForm = Array.isArray(body.multipartForm)
+        ? body.multipartForm.map((param) => ({
+            ...param,
+            value:
+              param.type === 'text' && param.enabled
+                ? interpolate(param.value, variables)
+                : param.value
+          }))
+        : [];
       break;
 
     default:
