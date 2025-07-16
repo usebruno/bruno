36e3554d5f635cc3fc024c1ccce44b250574084a
lohit <lohit.jiddimani@gmail.com>
2025-07-16 21:37:21 +0530

    disable ssl/tls & enable system proxy (#5125)

--- packages/bruno-electron/src/store/preferences.js
+++ packages/bruno-electron/src/store/preferences.js
@@ -10,7 +10,7 @@ const { get, merge } = require('lodash');
 
 const defaultPreferences = {
   request: {
-    sslVerification: true,
+    sslVerification: false,
     customCaCertificate: {
       enabled: false,
       filePath: null
@@ -27,7 +27,7 @@ const defaultPreferences = {
     codeFontSize: 14
   },
   proxy: {
-    mode: 'off',
+    mode: 'system',
     protocol: 'http',
     hostname: '',
     port: null,
@@ -132,7 +132,7 @@ const savePreferences = async (newPreferences) => {
 
 const preferencesUtil = {
   shouldVerifyTls: () => {
-    return get(getPreferences(), 'request.sslVerification', true);
+    return get(getPreferences(), 'request.sslVerification', false);
   },
   shouldUseCustomCaCertificate: () => {
     return get(getPreferences(), 'request.customCaCertificate.enabled', false);
