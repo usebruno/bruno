3664c39143fffce7d74e31f5987dad72a1073b03
pooja-bruno <pooja@usebruno.com>
2025-07-25 12:29:29 +0530

    add: tests in script

--- packages/bruno-electron/src/ipc/network/index.js
+++ packages/bruno-electron/src/ipc/network/index.js
@@ -897,6 +897,9 @@ const registerNetworkIpc = (mainWindow) => {
           scriptType: 'test',
           error: testError
         });
+
+        const domainsWithCookiesTest = await getDomainsWithCookies();
+        mainWindow.webContents.send('main:cookies-update', safeParseJSON(safeStringifyJSON(domainsWithCookiesTest)));
       }
 
       return {
@@ -1098,6 +1101,9 @@ const registerNetworkIpc = (mainWindow) => {
               error: preRequestError
             });
 
+            const domainsWithCookiesPreRequest = await getDomainsWithCookies();
+            mainWindow.webContents.send('main:cookies-update', safeParseJSON(safeStringifyJSON(domainsWithCookiesPreRequest)));
+
             if (preRequestError) {
               throw preRequestError;
             }
@@ -1285,6 +1291,9 @@ const registerNetworkIpc = (mainWindow) => {
               error: postResponseError
             });
 
+            const domainsWithCookiesPostResponse = await getDomainsWithCookies();
+            mainWindow.webContents.send('main:cookies-update', safeParseJSON(safeStringifyJSON(domainsWithCookiesPostResponse)));
+
             if (postResponseScriptResult?.nextRequestName !== undefined) {
               nextRequestName = postResponseScriptResult.nextRequestName;
             }
@@ -1389,6 +1398,9 @@ const registerNetworkIpc = (mainWindow) => {
                 scriptType: 'test',
                 error: testError
               });
+
+              const domainsWithCookiesTest = await getDomainsWithCookies();
+              mainWindow.webContents.send('main:cookies-update', safeParseJSON(safeStringifyJSON(domainsWithCookiesTest)));
             }
           } catch (error) {
             mainWindow.webContents.send('main:run-folder-event', {
