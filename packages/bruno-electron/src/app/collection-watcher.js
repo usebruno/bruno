bd8eecac22179fc38a1f31a64ef6b9f05065ef46
Stephen Breakey <stephen.breakey@yoco.com>
2025-08-04 21:43:20 +0200

    chore(process-env-vars): remove dead active env code in collection-watcher

--- packages/bruno-electron/src/app/collection-watcher.js
+++ packages/bruno-electron/src/app/collection-watcher.js
@@ -184,8 +184,6 @@ const unlinkEnvironmentFile = async (win, pathname, collectionUid) => {
 const add = async (win, pathname, collectionUid, collectionPath, useWorkerThread, watcher) => {
   console.log(`watcher add: ${pathname}`);
 
-  const activeEnvironmentName = watcher.activeEnvironments[collectionUid];
-
   if (isBrunoConfigFile(pathname, collectionPath)) {
     try {
       const content = fs.readFileSync(pathname, 'utf8');
@@ -575,7 +573,6 @@ class CollectionWatcher {
   constructor() {
     this.watchers = {};
     this.loadingStates = {};
-    this.activeEnvironments = {};
   }
 
   // Initialize loading state tracking for a collection
@@ -758,15 +755,6 @@ class CollectionWatcher {
     return Object.entries(this.watchers)
       .filter(([path, watcher]) => !!watcher)
       .map(([path, _watcher]) => path);
-  }
-  
-  updateActiveEnvironmentForCollection(win, collectionUid, activeEnvironmentName) {
-    this.activeEnvironments[collectionUid] = activeEnvironmentName;
-    const payload = {
-      collectionUid,
-      processEnvVariables: getProcessEnvVars(collectionUid, activeEnvironmentName)
-    };
-    win.webContents.send('main:process-env-update', payload);
   }
 }
 
