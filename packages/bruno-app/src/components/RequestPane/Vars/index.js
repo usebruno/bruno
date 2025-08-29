c12551787fe5638953b8c748250d70c6b30a7f51
jayakrishnancn <jayakrishnancn@gmail.com>
2025-08-29 23:38:09 +0530

    feat: added a table to understand the final variables and its values and its source

--- packages/bruno-app/src/components/RequestPane/Vars/index.js
+++ packages/bruno-app/src/components/RequestPane/Vars/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import get from 'lodash/get';
 import VarsTable from './VarsTable';
+import FinalVarsTable from './FinalVarsTable';
 import StyledWrapper from './StyledWrapper';
 
 const Vars = ({ item, collection }) => {
+  const [showFinalVars, setShowFinalVars] = React.useState(false);
   const requestVars = item.draft ? get(item, 'draft.request.vars.req') : get(item, 'request.vars.req');
   const responseVars = item.draft ? get(item, 'draft.request.vars.res') : get(item, 'request.vars.res');
 
@@ -16,6 +18,19 @@ const Vars = ({ item, collection }) => {
       <div>
         <div className="mt-1 mb-1 title text-xs">Post Response</div>
         <VarsTable item={item} collection={collection} vars={responseVars} varType="response" />
+      </div>
+      <hr/>
+      <div>
+        <div className="mt-2 mb-1 flex items-center justify-between mb-2">
+          <div className="title text-xs">Final Variables (All Sources)</div>
+          <button
+            className="text-xs text-blue-600 hover:text-blue-800 underline"
+            onClick={() => setShowFinalVars(!showFinalVars)}
+          >
+            {showFinalVars ? 'Hide' : 'Show'}
+          </button>
+        </div>
+        {showFinalVars && <FinalVarsTable item={item} collection={collection} />}
       </div>
     </StyledWrapper>
   );
