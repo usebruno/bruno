56f074112122e5574cd7a10615de7a3553bc866e
Siddharth Gelera (reaper) <ahoy@barelyhuman.dev>
2025-09-30 11:23:47 +0530

    chore: extract ts support for aslant from feat/websocket-engine (#5664)
    * chore: extract ts support

--- eslint.config.js
+++ eslint.config.js
@@ -16,9 +16,16 @@ module.exports = runESMImports().then(() => defineConfig([
       'diff': fixupPluginRules(eslintPluginDiff),
       '@stylistic': stylistic,
     },
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
     files: [
       './eslint.config.js',
-      'tests/**/*.spec.{ts,js}',
+      'tests/**/*.{ts,js}',
       'packages/bruno-app/**/*.{js,jsx,ts}',
       'packages/bruno-app/src/test-utils/mocks/codemirror.js',
       'packages/bruno-cli/**/*.js',
