54277b9cace9c66eed99d181e2cbfa9fc5280711
lohit-bruno <lohit@usebruno.com>
2025-10-08 12:36:52 +0530

    fix(bru-1928): fix debug library dependency for bruno-requests package

--- packages/bruno-requests/rollup.config.js
+++ packages/bruno-requests/rollup.config.js
@@ -39,6 +39,6 @@ module.exports = [
       typescript({ tsconfig: './tsconfig.json' }),
       terser(),
     ],
-    external: ['axios', 'qs', 'ws']
+    external: ['axios', 'qs', 'ws', 'debug']
   }
 ];
