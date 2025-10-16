ed56417a378775f90819b6a0ece2e2c47cd7e36d
pooja-bruno <pooja@usebruno.com>
2025-10-16 19:44:04 +0530

    add: unit test for multi line test

--- packages/bruno-lang/v2/tests/bruToJson.spec.js
+++ packages/bruno-lang/v2/tests/bruToJson.spec.js
@@ -38,4 +38,83 @@ settings {
       expect(output).toEqual(expected);
     });
   });
+
+  describe('multi-line values', () => {
+    it('parses multi-line values in URL, headers, params, and vars', () => {
+      const input = `
+meta {
+  name: new-line
+  type: http
+  seq: 1
+}
+
+get {
+  url: '''
+    https://httpbin.io/anything?foo=hello
+    world
+'''
+  body: none
+  auth: oauth2
+}
+
+params:query {
+  foo: '''
+    hello
+    world
+  '''
+}
+
+headers {
+  "test header": '''
+    t1
+    t2
+  '''
+}
+
+vars:pre-request {
+  test-var: '''
+    t1
+    t2
+  '''
+}
+
+settings {
+  encodeUrl: false
+}
+`;
+
+      const output = parser(input);
+
+      expect(output.meta).toEqual({
+        name: 'new-line',
+        type: 'http',
+        seq: '1'
+      });
+
+      expect(output.http.url).toBe('https://httpbin.io/anything?foo=hello\nworld');
+
+      expect(output.params).toHaveLength(1);
+      expect(output.params[0]).toEqual({
+        name: 'foo',
+        value: 'hello\nworld',
+        enabled: true,
+        type: 'query'
+      });
+
+      expect(output.headers).toHaveLength(1);
+      expect(output.headers[0]).toEqual({
+        name: 'test header',
+        value: 't1\nt2',
+        enabled: true
+      });
+
+      expect(output.vars.req).toHaveLength(1);
+      expect(output.vars.req[0]).toEqual({
+        name: 'test-var',
+        value: 't1\nt2',
+        enabled: true,
+        local: false
+      });
+    });
+  });
 });
