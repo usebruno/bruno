eb671fee52b1d17f060ff66a1ac29126f6bc6231
Bijin Bruno <bijin@usebruno.com>
2025-10-27 17:16:43 +0530

    fix: revert increasing playwright worker count

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,8 +9,8 @@ if (process.env.CI) {
 export default defineConfig({
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? undefined : 3,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? undefined : 1,
   reporter,
 
   use: {
