30c3d3b8e45782eb8d6dbc98acf7e2f36c81f679
sanish-bruno <sanish@usebruno.com>
2025-10-31 15:37:31 +0530

    review fixes

--- packages/bruno-app/src/components/ResponseExample/ResponseExampleRequestPane/ResponseExampleMultipartFormParams/StyledWrapper.js
+++ packages/bruno-app/src/components/ResponseExample/ResponseExampleRequestPane/ResponseExampleMultipartFormParams/StyledWrapper.js
@@ -19,8 +19,8 @@ const Wrapper = styled.div`
     }
     td {
       padding: 6px 10px;
-      }
     }
+  }
 
   .btn-add-param {
     font-size: 0.8125rem;
--- packages/bruno-filestore/src/types/bruno-lang.d.ts
+++ packages/bruno-filestore/src/types/bruno-lang.d.ts
@@ -1,5 +1,5 @@
 declare module '@usebruno/lang' {
-  export function bruToJsonV2(bruContent: string, isExample?: boolean): any;
+  export function bruToJsonV2(bruContent: string): any;
   export function jsonToBruV2(jsonData: any): string;
   export function bruToEnvJsonV2(bruContent: string): any;
   export function envJsonToBruV2(jsonData: any): string;
