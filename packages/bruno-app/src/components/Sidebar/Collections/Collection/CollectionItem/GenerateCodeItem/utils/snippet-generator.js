403c288809cfdf984b423e1a56bd5a22d9f7836d
Siddharth Gelera <ahoy@barelyhuman.dev>
2025-11-06 12:15:55 +0530

    feat: implement template variable hashing for request URLs

--- packages/bruno-app/src/components/Sidebar/Collections/Collection/CollectionItem/GenerateCodeItem/utils/snippet-generator.js
+++ packages/bruno-app/src/components/Sidebar/Collections/Collection/CollectionItem/GenerateCodeItem/utils/snippet-generator.js
@@ -3,6 +3,56 @@ import { getAuthHeaders } from 'utils/codegenerator/auth';
 import { getAllVariables, getTreePathFromCollectionToItem } from 'utils/collections/index';
 import { interpolateHeaders, interpolateBody } from './interpolation';
 
+const VARIABLE_REGEX = /\{\{([^}]+)\}\}/g;
+
+/**
+ * Was implemented specifically for request.url where the url might have variables
+ * that might need to be sanitised before being passed to a URL validator that doesn't
+ * allow special characters that bruno uses as variables (`{{var_name}}`)
+ *
+ * The function replaces the input string with a unique hash that can be restored
+ * later by the helper returned by this function
+ */
+function templateVariableHasher(input) {
+  function hash(toHash) {
+    let hash = 5381;
+    let c;
+    for (let i = 0; i < toHash.length; i++) {
+      c = toHash.charCodeAt(i);
+      hash = (hash << 5) + hash + c;
+    }
+    return '' + hash;
+  }
+
+  const prefix = `bruno-var-hash-`;
+  const hashToOriginal = {};
+  let result = input;
+  let hashed = false;
+  if (VARIABLE_REGEX.test(input)) {
+    hashed = true;
+    result = input.replace(VARIABLE_REGEX, function (matchedVar) {
+      const hashedValue = `${prefix}${hash(matchedVar)}`;
+      hashToOriginal[hashedValue] = matchedVar;
+      return hashedValue;
+    });
+  }
+  return {
+    hashed: result,
+    /** @param {string} current */
+    restore(current) {
+      if (!hashed) {
+        return current;
+      }
+      let clone = current;
+      for (const hash in hashToOriginal) {
+        const value = hashToOriginal[hash];
+        clone = clone.replace(hash, value);
+      }
+      return clone;
+    }
+  };
+}
+
 // Merge headers from collection, folders, and request
 const mergeHeaders = (collection, request, requestTreePath) => {
   let headers = new Map();
@@ -50,6 +100,11 @@ const generateSnippet = ({ language, item, collection, shouldInterpolate = false
 
     const request = item.request;
 
+    // request.url should already be interpolated even before reaching here, this is a fail
+    // safe to make sure any non interpolated values stays consistent in the snippet
+    const { hashed: _hashedUrl, restore: urlHashRestorer } = templateVariableHasher(request.url);
+    request.url = _hashedUrl;
+
     // Get the request tree path and merge headers
     const requestTreePath = getTreePathFromCollectionToItem(collection, item);
     let headers = mergeHeaders(collection, request, requestTreePath);
@@ -78,6 +133,10 @@ const generateSnippet = ({ language, item, collection, shouldInterpolate = false
     // Generate snippet using HTTPSnippet
     const snippet = new HTTPSnippet(harRequest);
     const result = snippet.convert(language.target, language.client);
+
+    if (urlHashRestorer) {
+      return urlHashRestorer(result);
+    }
 
     return result;
   } catch (error) {
