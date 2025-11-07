53732cb0a2aa1ab02f7c958e9acd30c1f441d6c5
pooja-bruno <pooja@usebruno.com>
2025-11-07 18:00:29 +0530

    fix: unit test

--- packages/bruno-electron/tests/network/index.spec.js
+++ packages/bruno-electron/tests/network/index.spec.js
@@ -3,13 +3,13 @@ const { configureRequest } = require('../../src/ipc/network/index');
 describe('index: configureRequest', () => {
   it("Should add 'http://' to the URL if no protocol is specified", async () => {
     const request = { method: 'GET', url: 'test-domain', body: {} };
-    await configureRequest(null, request, null, null, null, null);
+    await configureRequest(null, null, request, null, null, null, null);
     expect(request.url).toEqual('http://test-domain');
   });
 
   it("Should NOT add 'http://' to the URL if a protocol is specified", async () => {
     const request = { method: 'GET', url: 'ftp://test-domain', body: {} };
-    await configureRequest(null, request, null, null, null, null);
+    await configureRequest(null, null, request, null, null, null, null);
     expect(request.url).toEqual('ftp://test-domain');
   });
 });
