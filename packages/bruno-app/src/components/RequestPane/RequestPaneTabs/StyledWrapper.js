243fdd7295e50096723f8925094527a8ca62b0c6
naman-bruno <naman@usebruno.com>
2025-12-09 16:39:29 +0530

    improve: tabs design

--- packages/bruno-app/src/components/RequestPane/RequestPaneTabs/StyledWrapper.js
+++ packages/bruno-app/src/components/RequestPane/RequestPaneTabs/StyledWrapper.js
@@ -1,14 +1,28 @@
 import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
-  div.tabs {
+  &.tabs {
+    div.more-tabs {
+      color: ${(props) => props.theme.text} !important;
+      background-color: ${(props) => props.theme.requestTabs.bg} !important;
+      &:hover {
+        background-color: ${(props) => props.theme.requestTabs.icon.hoverBg} !important;
+      }
+    }
+
     div.tab {
+      display: inline-flex;
+      align-items: center;
+      gap: 0.25rem;
       padding: 6px 0px;
       border: none;
       border-bottom: solid 2px transparent;
       margin-right: ${(props) => props.theme.tabs.marginRight};
       color: var(--color-tab-inactive);
       cursor: pointer;
+      white-space: nowrap;
+      vertical-align: middle;
+      flex-shrink: 0;
 
       &:focus,
       &:active,
@@ -25,7 +39,15 @@ const StyledWrapper = styled.div`
       }
 
       .content-indicator {
-        color: ${(props) => props.theme.text}
+        color: ${(props) => props.theme.text};
+      }
+
+      sup {
+        display: inline-flex;
+        align-items: center;
+        line-height: 1;
+        vertical-align: baseline;
+        margin-left: 0;
       }
     }
   }
