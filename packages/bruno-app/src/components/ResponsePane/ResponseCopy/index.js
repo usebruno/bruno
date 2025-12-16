6f774fad7de85c03720e716ee565f569eefec059
Abhishek S Lal <abhishek@usebruno.com>
2025-12-16 13:50:39 +0530

    fix: correct tabIndex logic in ResponseCopy component for improved accessibility

--- packages/bruno-app/src/components/ResponsePane/ResponseCopy/index.js
+++ packages/bruno-app/src/components/ResponsePane/ResponseCopy/index.js
@@ -64,7 +64,7 @@ const ResponseCopy = forwardRef(({ item, children }, ref) => {
     <div
       ref={elementRef}
       role={!!children ? 'button' : undefined}
-      tabIndex={!!children || isDisabled ? 0 : -1}
+      tabIndex={!!children || !isDisabled ? 0 : -1}
       onClick={handleClick}
       title={!children ? 'Copy response to clipboard' : null}
       onKeyDown={handleKeyDown}
