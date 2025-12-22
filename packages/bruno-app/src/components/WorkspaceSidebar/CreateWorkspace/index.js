70178f60b377437ae4d45063718806b26b0cfee0
naman-bruno <naman@usebruno.com>
2025-12-22 18:29:33 +0530

    add: rename folder option while creating workspace (#6481)

--- packages/bruno-app/src/components/WorkspaceSidebar/CreateWorkspace/index.js
+++ packages/bruno-app/src/components/WorkspaceSidebar/CreateWorkspace/index.js
@@ -3,23 +3,28 @@ import { useFormik } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
 import * as Yup from 'yup';
 import toast from 'react-hot-toast';
+import { IconArrowBackUp, IconEdit } from '@tabler/icons';
 import Modal from 'components/Modal';
+import Help from 'components/Help';
+import PathDisplay from 'components/PathDisplay/index';
 import { createWorkspaceAction } from 'providers/ReduxStore/slices/workspaces/actions';
 import { browseDirectory } from 'providers/ReduxStore/slices/collections/actions';
 import { multiLineMsg } from 'utils/common/index';
 import { formatIpcError } from 'utils/common/error';
-import Help from 'components/Help';
+import { sanitizeName, validateName, validateNameError } from 'utils/common/regex';
 
 const CreateWorkspace = ({ onClose }) => {
   const inputRef = useRef();
   const dispatch = useDispatch();
   const workspaces = useSelector((state) => state.workspaces.workspaces);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: {
       workspaceName: '',
+      workspaceFolderName: '',
       workspaceLocation: ''
     },
     validationSchema: Yup.object({
@@ -33,6 +38,14 @@ const CreateWorkspace = ({ onClose }) => {
           return !workspaces.some((w) =>
             w.name.toLowerCase() === value.toLowerCase());
         }),
+      workspaceFolderName: Yup.string()
+        .min(1, 'must be at least 1 character')
+        .max(255, 'must be 255 characters or less')
+        .test('is-valid-folder-name', function (value) {
+          const isValid = validateName(value);
+          return isValid ? true : this.createError({ message: validateNameError(value) });
+        })
+        .required('folder name is required'),
       workspaceLocation: Yup.string().min(1, 'location is required').required('location is required')
     }),
     onSubmit: async (values) => {
@@ -41,7 +54,7 @@ const CreateWorkspace = ({ onClose }) => {
       try {
         setIsSubmitting(true);
 
-        await dispatch(createWorkspaceAction(values.workspaceName, values.workspaceName, values.workspaceLocation));
+        await dispatch(createWorkspaceAction(values.workspaceName, values.workspaceFolderName, values.workspaceLocation));
         toast.success('Workspace created!');
         onClose();
       } catch (error) {
@@ -98,13 +111,70 @@ const CreateWorkspace = ({ onClose }) => {
               autoCorrect="off"
               autoCapitalize="off"
               spellCheck="false"
-              onChange={formik.handleChange}
+              onChange={(e) => {
+                formik.handleChange(e);
+                if (!isEditing) {
+                  formik.setFieldValue('workspaceFolderName', sanitizeName(e.target.value));
+                }
+              }}
               value={formik.values.workspaceName || ''}
             />
             {formik.touched.workspaceName && formik.errors.workspaceName ? (
               <div className="text-red-500 text-sm mt-1">{formik.errors.workspaceName}</div>
             ) : null}
           </div>
+
+          {formik.values.workspaceName?.trim()?.length > 0 && (
+            <div className="mb-4">
+              <div className="flex items-center justify-between mb-2">
+                <label htmlFor="workspaceFolderName" className="flex items-center font-semibold">
+                  Folder Name
+                  <Help width="300">
+                    <p>
+                      The name of the folder used to store the workspace.
+                    </p>
+                    <p className="mt-2">
+                      You can choose a folder name different from your workspace's name or one compatible with filesystem rules.
+                    </p>
+                  </Help>
+                </label>
+                {isEditing ? (
+                  <IconArrowBackUp
+                    className="cursor-pointer opacity-50 hover:opacity-80"
+                    size={16}
+                    strokeWidth={1.5}
+                    onClick={() => setIsEditing(false)}
+                  />
+                ) : (
+                  <IconEdit
+                    className="cursor-pointer opacity-50 hover:opacity-80"
+                    size={16}
+                    strokeWidth={1.5}
+                    onClick={() => setIsEditing(true)}
+                  />
+                )}
+              </div>
+              {isEditing ? (
+                <input
+                  id="workspace-folder-name"
+                  type="text"
+                  name="workspaceFolderName"
+                  className="block textbox w-full"
+                  onChange={formik.handleChange}
+                  autoComplete="off"
+                  autoCorrect="off"
+                  autoCapitalize="off"
+                  spellCheck="false"
+                  value={formik.values.workspaceFolderName || ''}
+                />
+              ) : (
+                <PathDisplay baseName={formik.values.workspaceFolderName} />
+              )}
+              {formik.touched.workspaceFolderName && formik.errors.workspaceFolderName ? (
+                <div className="text-red-500 text-sm mt-1">{formik.errors.workspaceFolderName}</div>
+              ) : null}
+            </div>
+          )}
 
           <div className="mb-4">
             <label htmlFor="workspaceLocation" className="font-semibold mb-2 flex items-center">
