b88ab965c48e6436815a7c4f19057936c9c1299a
gopu-bruno <gopu@usebruno.com>
2026-01-01 15:03:50 +0530

    fix: ensure folder level documentation save button is visible

--- packages/bruno-app/src/components/FolderSettings/Documentation/index.js
+++ packages/bruno-app/src/components/FolderSettings/Documentation/index.js
@@ -3,16 +3,16 @@ import get from 'lodash/get';
 import { updateFolderDocs } from 'providers/ReduxStore/slices/collections';
 import { useTheme } from 'providers/Theme';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { saveFolderRoot } from 'providers/ReduxStore/slices/collections/actions';
 import Markdown from 'components/MarkDown';
 import CodeEditor from 'components/CodeEditor';
+import Button from 'ui/Button';
 import StyledWrapper from './StyledWrapper';
 
 const Documentation = ({ collection, folder }) => {
   const dispatch = useDispatch();
   const { displayedTheme } = useTheme();
-  const preferences = useSelector((state) => state.app.preferences);
   const [isEditing, setIsEditing] = useState(false);
   const docs = folder.draft ? get(folder, 'draft.docs', '') : get(folder, 'root.docs', '');
 
@@ -37,27 +37,33 @@ const Documentation = ({ collection, folder }) => {
   }
 
   return (
-    <StyledWrapper className="mt-1 h-full w-full relative flex flex-col">
-      <div className="editing-mode flex justify-between items-center" role="tab" onClick={toggleViewMode}>
+    <StyledWrapper className="mt-1 w-full relative flex flex-col">
+      <div className="editing-mode flex justify-between items-center flex-shrink-0" role="tab" onClick={toggleViewMode}>
         {isEditing ? 'Preview' : 'Edit'}
       </div>
 
       {isEditing ? (
-        <div className="mt-2 flex-1 max-h-[70vh]">
-          <CodeEditor
-            collection={collection}
-            theme={displayedTheme}
-            value={docs || ''}
-            onEdit={onEdit}
-            onSave={onSave}
-            mode="application/text"
-          />
-          <button type="submit" className="submit btn btn-sm btn-secondary my-6" onClick={onSave}>
-            Save
-          </button>
+        <div className="flex flex-col flex-1 min-h-0">
+          <div className="mt-2 flex-1 overflow-auto min-h-0">
+            <CodeEditor
+              collection={collection}
+              theme={displayedTheme}
+              value={docs || ''}
+              onEdit={onEdit}
+              onSave={onSave}
+              mode="application/text"
+            />
+          </div>
+          <div className="mt-6 flex-shrink-0">
+            <Button type="submit" size="sm" onClick={onSave}>
+              Save
+            </Button>
+          </div>
         </div>
       ) : (
-        <Markdown collectionPath={collection.pathname} onDoubleClick={toggleViewMode} content={docs} />
+        <div className="h-full">
+          <Markdown collectionPath={collection.pathname} onDoubleClick={toggleViewMode} content={docs} />
+        </div>
       )}
     </StyledWrapper>
   );
