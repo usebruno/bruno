49f09aa38fe5f8e10fbb53578f00530661db761e
Sid <siddharth@usebruno.com>
2026-01-02 19:11:20 +0530

    fix: a possible null case exception
    Though we always create an empty data buffer at source so shouldn't happen unless that is modified

--- packages/bruno-app/src/providers/ReduxStore/slices/collections/index.js
+++ packages/bruno-app/src/providers/ReduxStore/slices/collections/index.js
@@ -3133,7 +3133,10 @@ export const collectionsSlice = createSlice({
             timestamp: timestamp || Date.now()
           }].concat(item.response.data);
         }
-        item.response.dataBuffer = Buffer.concat([Buffer.from(item.response.dataBuffer), Buffer.from(data.dataBuffer)]);
+        if (item.response.dataBuffer && item.response.dataBuffer.length && data.dataBuffer) {
+          item.response.dataBuffer = Buffer.concat([Buffer.from(item.response.dataBuffer), Buffer.from(data.dataBuffer)]);
+        }
+
         item.response.size = data.data?.length + (item.response.size || 0);
       }
     },
