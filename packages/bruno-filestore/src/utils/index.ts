dfeacd8c287c9f27dee686697494066a7bdd6264
naman-bruno <naman@usebruno.com>
2026-01-21 13:32:58 +0530

    fix

--- packages/bruno-filestore/src/utils/index.ts
+++ packages/bruno-filestore/src/utils/index.ts
@@ -13,10 +13,7 @@ export const ensureString = (value: unknown, fallback: string = ''): string => {
   if (typeof value === 'string') {
     return value;
   }
-  if (typeof value === 'number' || typeof value === 'boolean') {
-    return String(value);
-  }
-  return fallback;
+  return String(value);
 };
 
 export const uuid = () => {
