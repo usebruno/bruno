da557b9c196b80c87a5587d733f08c3f6f49e5a7
Asadullah-nadeem <asadullahnadeem48@gmail.com>
2026-01-23 00:35:24 +0530

    Fix collection search visibility and case sensitivity

--- packages/bruno-app/src/utils/collections/search.js
+++ packages/bruno-app/src/utils/collections/search.js
@@ -1,6 +1,6 @@
-import { flattenItems, isItemARequest } from './index';
 import filter from 'lodash/filter';
 import find from 'lodash/find';
+import { flattenItems, isItemARequest } from './index';
 
 export const doesRequestMatchSearchText = (request, searchText = '') => {
   return request?.name?.toLowerCase().includes(searchText.toLowerCase());
@@ -14,6 +14,9 @@ export const doesFolderHaveItemsMatchSearchText = (item, searchText = '') => {
 };
 
 export const doesCollectionHaveItemsMatchingSearchText = (collection, searchText = '') => {
+  if (collection?.name?.toLowerCase().includes(searchText.toLowerCase())) {
+    return true;
+  }
   let flattenedItems = flattenItems(collection.items);
   let requestItems = filter(flattenedItems, (item) => isItemARequest(item));
 
