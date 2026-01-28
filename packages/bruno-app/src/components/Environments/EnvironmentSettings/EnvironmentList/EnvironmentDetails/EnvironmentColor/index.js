ba7c91836ddcadcb12e99549bfc7c4339244086b
Mathieu D <mathieu.dreano@decathlon.com>
2026-01-28 08:16:39 +0100

    use @uiw/reac-color instead of react-color
    Signed-off-by: Mathieu D <mathieu.dreano@decathlon.com>

--- packages/bruno-app/src/components/Environments/EnvironmentSettings/EnvironmentList/EnvironmentDetails/EnvironmentColor/index.js
+++ packages/bruno-app/src/components/Environments/EnvironmentSettings/EnvironmentList/EnvironmentDetails/EnvironmentColor/index.js
@@ -3,28 +3,29 @@ import { useCallback } from 'react';
 import toast from 'react-hot-toast';
 import { useDispatch } from 'react-redux';
 import { saveEnvironmentColor } from 'providers/ReduxStore/slices/collections/actions';
-import { CirclePicker } from 'react-color';
+import { Circle } from '@uiw/react-color';
 
 const EnvironmentColor = ({ environment, collectionUid }) => {
   const dispatch = useDispatch();
 
   const onColorChange = useCallback(
     (color) => {
+      if (color == environment.color) return;
       dispatch(saveEnvironmentColor(color, environment.uid, collectionUid))
         .then(() => toast.success('Environment color changed successfully'))
         .catch(() => toast.error('An error occurred while changing the environment color'));
     },
-    [dispatch, environment.uid, collectionUid]
+    [dispatch, environment.uid, environment.color, collectionUid]
   );
 
   return (
-    <CirclePicker
+    <Circle
       id="environment-color"
-      circleSize={14}
-      circleSpacing={3}
+      style={{ gap: 3 }}
+      pointProps={{ style: { width: 14, height: 14, borderRadius: 10 } }}
       colors={['#000000','#9c27b0','#3f51b5','#03a9f4','#009688','#8bc34a','#ffeb3b','#ff9800','#ff5722','#795548','#607d8b']}
       color={environment.color}
-      onChangeComplete={(color) => onColorChange(color.hex)}
+      onChange={(color) => onColorChange(color.hex)}
     />
   );
 };
