2bf4669282e96402fa85cd99832a332ab335002a
naman-bruno <naman@usebruno.com>
2026-01-29 16:45:51 +0530

    fixes

--- packages/bruno-app/src/ui/MenuDropdown/index.js
+++ packages/bruno-app/src/ui/MenuDropdown/index.js
@@ -1,6 +1,6 @@
 import React, { forwardRef, useRef, useCallback, useState, useImperativeHandle, useEffect, useMemo } from 'react';
-import { IconChevronRight, IconChevronLeft } from '@tabler/icons';
 import Dropdown from 'components/Dropdown';
+import SubMenuItem from './SubMenuItem';
 
 // Constants
 const NAVIGATION_KEYS = ['ArrowDown', 'ArrowUp', 'Home', 'End', 'Escape'];
@@ -386,65 +386,20 @@ const MenuDropdown = forwardRef(({
     </>
   );
 
-  // Render submenu item (only used when item has submenu)
-  const SubmenuItem = ({ item, onRootClose }) => {
-    const [submenuOpen, setSubmenuOpen] = useState(false);
-    const isLeftPlacement = submenuPlacement === 'left';
-    const submenuTippyPlacement = isLeftPlacement ? 'left-start' : 'right-start';
-    const ArrowIcon = isLeftPlacement ? IconChevronLeft : IconChevronRight;
-
-    const submenuItemsWithClose = item.submenu.map((subItem) => {
-      if (subItem.type === 'divider') return subItem;
-      return {
-        ...subItem,
-        onClick: () => {
-          subItem.onClick?.();
-          onRootClose();
-        }
-      };
-    });
-
-    const itemProps = getMenuItemProps(item, {
-      'className': 'has-submenu',
-      'aria-haspopup': 'true',
-      'aria-expanded': submenuOpen,
-      'aria-current': undefined // submenu triggers don't need aria-current
-    });
-
-    const arrowElement = (
-      <span className="submenu-arrow">
-        <ArrowIcon size={14} />
-      </span>
-    );
-
-    return (
-      <div
-        className="submenu-trigger"
-        onMouseEnter={() => setSubmenuOpen(true)}
-        onMouseLeave={() => setSubmenuOpen(false)}
-      >
-        <MenuDropdown
-          items={submenuItemsWithClose}
-          placement={submenuTippyPlacement}
-          opened={submenuOpen}
-          onChange={setSubmenuOpen}
-          showTickMark={false}
-          submenuPlacement={submenuPlacement}
-          appendTo={() => document.body}
-          offset={[0, 0]}
-        >
-          <div {...itemProps}>
-            {renderMenuItemContent(item, arrowElement)}
-          </div>
-        </MenuDropdown>
-      </div>
-    );
-  };
-
   // Render menu item
   const renderMenuItem = (item) => {
     if (item.submenu) {
-      return <SubmenuItem key={item.id} item={item} onRootClose={() => updateOpenState(false)} />;
+      return (
+        <SubMenuItem
+          key={item.id}
+          item={item}
+          onRootClose={() => updateOpenState(false)}
+          submenuPlacement={submenuPlacement}
+          getMenuItemProps={getMenuItemProps}
+          renderMenuItemContent={renderMenuItemContent}
+          MenuDropdownComponent={MenuDropdown}
+        />
+      );
     }
 
     const itemProps = getMenuItemProps(item);
