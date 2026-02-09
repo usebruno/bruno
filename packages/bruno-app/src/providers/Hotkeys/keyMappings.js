40915da4969ef4c332f1c93bcd9354373e195173
shubh-bruno <shubh-bruno@shubh-bruno.local>
2026-02-09 11:12:35 +0530

    fix: moved keybindings to preferences redux-slice with default and customized

--- packages/bruno-app/src/providers/Hotkeys/keyMappings.js
+++ packages/bruno-app/src/providers/Hotkeys/keyMappings.js
@@ -1,4 +1,4 @@
-const KeyMapping = {
+export const DEFAULT_KEY_BINDINGS = {
   save: { mac: 'command+bind+s', windows: 'ctrl+bind+s', name: 'Save' },
   sendRequest: { mac: 'command+bind+enter', windows: 'ctrl+bind+enter', name: 'Send Request' },
   editEnvironment: { mac: 'command+bind+e', windows: 'ctrl+bind+e', name: 'Edit Environment' },
@@ -8,7 +8,7 @@ const KeyMapping = {
   sidebarSearch: { mac: 'command+bind+f', windows: 'ctrl+bind+f', name: 'Search Sidebar' },
   closeTab: { mac: 'command+bind+w', windows: 'ctrl+bind+w', name: 'Close Tab' },
   openPreferences: { mac: 'command+bind+,', windows: 'ctrl+bind+,', name: 'Open Preferences' },
-  changeLayout: { mac: 'x+bind+v', windows: 'x+bind+v', name: 'Change Orientation' },
+  changeLayout: { mac: 'command+bind+j', windows: 'ctrl+bind+j', name: 'Change Orientation' },
   closeBruno: {
     mac: 'command+bind+Q',
     windows: 'ctrl+bind+shift+bind+q',
@@ -16,12 +16,12 @@ const KeyMapping = {
   },
   switchToPreviousTab: {
     mac: 'command+bind+pageup',
-    windows: 'fn+bind+ctrl+bind+arrowup',
+    windows: 'ctrl+bind+pageup',
     name: 'Switch to Previous Tab'
   },
   switchToNextTab: {
     mac: 'command+bind+pagedown',
-    windows: 'fn+bind+ctrl+bind+arrowdown',
+    windows: 'ctrl+bind+pagedown',
     name: 'Switch to Next Tab'
   },
   moveTabLeft: {
@@ -36,46 +36,10 @@ const KeyMapping = {
   },
   closeAllTabs: { mac: 'command+bind+shift+bind+w', windows: 'ctrl+bind+shift+bind+w', name: 'Close All Tabs' },
   collapseSidebar: { mac: 'command+bind+\\', windows: 'ctrl+bind+\\', name: 'Collapse Sidebar' },
-  zoomIn: { mac: 'command+bind+=', windows: 'ctrl+bind+=', name: 'Zoom In' },
+  zoomIn: { mac: 'command+bind+=', windows: 'ctrl+bind+=', name: 'Zoom 2 In' },
   zoomOut: { mac: 'command+bind+-', windows: 'ctrl+bind+-', name: 'Zoom Out' },
   resetZoom: { mac: 'command+bind+0', windows: 'ctrl+bind+0', name: 'Reset Zoom' },
   cloneItem: { mac: 'command+bind+d', windows: 'ctrl+bind+d', name: 'Clone Item' },
   copyItem: { mac: 'command+bind+c', windows: 'ctrl+bind+c', name: 'Copy Item' },
   pasteItem: { mac: 'command+bind+v', windows: 'ctrl+bind+v', name: 'Paste Item' }
-};
-
-/**
- * Retrieves the key bindings for a specific operating system.
- *
- * @param {string} os - The operating system (e.g., 'mac', 'windows').
- * @returns {Object} An object containing the key bindings for the specified OS.
- */
-export const getKeyBindingsForOS = (os) => {
-  const keyBindings = {};
-  for (const [action, { name, ...keys }] of Object.entries(KeyMapping)) {
-    if (keys[os]) {
-      keyBindings[action] = {
-        keys: keys[os],
-        name
-      };
-    }
-  }
-  return keyBindings;
-};
-
-/**
- * Retrieves the key bindings for a specific action across all operating systems.
- *
- * @param {string} action - The action for which to retrieve key bindings.
- * @returns {Object|null} An object containing the key bindings for macOS, Windows, or null if the action is not found.
- */
-export const getKeyBindingsForActionAllOS = (action) => {
-  const actionBindings = KeyMapping[action];
-
-  if (!actionBindings) {
-    console.warn(`Action "${action}" not found in KeyMapping.`);
-    return null;
-  }
-
-  return [actionBindings.mac, actionBindings.windows];
 };
--- packages/bruno-electron/src/store/preferences.js
+++ packages/bruno-electron/src/store/preferences.js
@@ -56,6 +56,51 @@ const defaultPreferences = {
   autoSave: {
     enabled: false,
     interval: 1000
+  },
+  keyBindings: {
+    save: { mac: 'command+bind+s', windows: 'ctrl+bind+s', name: 'Save' },
+    sendRequest: { mac: 'command+bind+enter', windows: 'ctrl+bind+enter', name: 'Send Request' },
+    editEnvironment: { mac: 'command+bind+e', windows: 'ctrl+bind+e', name: 'Edit Environment' },
+    newRequest: { mac: 'command+bind+n', windows: 'ctrl+bind+n', name: 'New Request' },
+    importCollection: { mac: 'command+bind+o', windows: 'ctrl+bind+o', name: 'Import Collection' },
+    globalSearch: { mac: 'command+bind+k', windows: 'ctrl+bind+k', name: 'Global Search' },
+    sidebarSearch: { mac: 'command+bind+f', windows: 'ctrl+bind+f', name: 'Search Sidebar' },
+    closeTab: { mac: 'command+bind+w', windows: 'ctrl+bind+w', name: 'Close Tab' },
+    openPreferences: { mac: 'command+bind+,', windows: 'ctrl+bind+,', name: 'Open Preferences' },
+    changeLayout: { mac: 'command+bind+j', windows: 'ctrl+bind+j', name: 'Change Orientation' },
+    closeBruno: {
+      mac: 'command+bind+Q',
+      windows: 'ctrl+bind+shift+bind+q',
+      name: 'Close Bruno'
+    },
+    switchToPreviousTab: {
+      mac: 'command+bind+pageup',
+      windows: 'ctrl+bind+pageup',
+      name: 'Switch to Previous Tab'
+    },
+    switchToNextTab: {
+      mac: 'command+bind+pagedown',
+      windows: 'ctrl+bind+pagedown',
+      name: 'Switch to Next Tab'
+    },
+    moveTabLeft: {
+      mac: 'command+bind+shift+bind+pageup',
+      windows: 'ctrl+bind+shift+bind+pageup',
+      name: 'Move Tab Left'
+    },
+    moveTabRight: {
+      mac: 'command+bind+shift+bind+pagedown',
+      windows: 'ctrl+bind+shift+bind+pagedown',
+      name: 'Move Tab Right'
+    },
+    closeAllTabs: { mac: 'command+bind+shift+bind+w', windows: 'ctrl+bind+shift+bind+w', name: 'Close All Tabs' },
+    collapseSidebar: { mac: 'command+bind+\\', windows: 'ctrl+bind+\\', name: 'Collapse Sidebar' },
+    zoomIn: { mac: 'command+bind++', windows: 'ctrl+bind++', name: 'Zoom In' },
+    zoomOut: { mac: 'command+bind+-', windows: 'ctrl+bind+-', name: 'Zoom Out' },
+    resetZoom: { mac: 'command+bind+0', windows: 'ctrl+bind+0', name: 'Reset Zoom' },
+    cloneItem: { mac: 'command+bind+d', windows: 'ctrl+bind+d', name: 'Clone Item' },
+    copyItem: { mac: 'command+bind+c', windows: 'ctrl+bind+c', name: 'Copy Item' },
+    pasteItem: { mac: 'command+bind+v', windows: 'ctrl+bind+v', name: 'Paste Item' }
   }
 };
 
