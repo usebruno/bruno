95aebb9558bf913a798e4f41531d815b09804280
Sid <siddharth@usebruno.com>
2026-03-06 11:23:52 +0530

    fix: adjust width properties in StyledWrapper component

--- packages/bruno-app/src/components/Preferences/Keybindings/StyledWrapper.js
+++ packages/bruno-app/src/components/Preferences/Keybindings/StyledWrapper.js
@@ -14,7 +14,6 @@ const StyledWrapper = styled.div`
   -ms-overflow-style: none;
 
   .section-header {
-    width: 60%;
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -148,7 +147,6 @@ const StyledWrapper = styled.div`
   }
 
   .table-container {
-    max-width: 60%;
     flex: 1 1 auto;
     min-height: 0;
     margin-bottom: 10px;
