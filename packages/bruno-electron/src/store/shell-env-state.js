9c3eabdda21df7b222ad5895a9cd3a44b52dbeab
Sid <siddharth@usebruno.com>
2026-04-01 20:41:30 +0530

    chore: add a promise based wait group for the shell variables (#7647)

---
+++ packages/bruno-electron/src/store/shell-env-state.js
@@ -0,0 +1,27 @@
+const { initializeShellEnv: _initializeShellEnv } = require('@usebruno/requests');
+
+const TIMEOUT_MS = 60_000;
+
+let _promise = null;
+
+const _initWithTimeout = () => {
+  let timer;
+  const timeout = new Promise((_, reject) => {
+    timer = setTimeout(() => {
+      _promise = null;
+      reject(new Error('Shell environment initialization timed out'));
+    }, TIMEOUT_MS);
+  });
+  return Promise.race([_initializeShellEnv(), timeout]).finally(() => clearTimeout(timer));
+};
+
+const initializeShellEnv = () => {
+  if (!_promise) _promise = _initWithTimeout();
+};
+
+const waitForShellEnv = () => {
+  if (!_promise) _promise = _initWithTimeout();
+  return _promise;
+};
+
+module.exports = { initializeShellEnv, waitForShellEnv };
