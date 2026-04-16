9c41f7ab78fc598d8fc50ec9dbefd3caebda78cb
Sid <siddharth@usebruno.com>
2026-04-16 13:32:02 +0530

    chore: smoothen the animation

--- packages/bruno-app/src/components/EnvironmentVariablesTable/StyledWrapper.js
+++ packages/bruno-app/src/components/EnvironmentVariablesTable/StyledWrapper.js
@@ -15,7 +15,7 @@ const Wrapper = styled.div`
     overflow-y: auto;
     border-radius: 8px;
     border: solid 1px ${(props) => props.theme.border.border0};
-    transition: height 0.15s ease;
+    transition: height 75ms cubic-bezier(0,1.12,.84,.64);
   }
 
   table {
