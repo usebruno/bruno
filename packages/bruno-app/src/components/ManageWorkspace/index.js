a03a714702b6010082c3442623a2518e9c4f3c59
Prateek Sunal <prateek@usebruno.com>
2026-04-28 18:12:20 +0530

    fix: correct formatting in ManageWorkspace component menu options

--- packages/bruno-app/src/components/ManageWorkspace/index.js
+++ packages/bruno-app/src/components/ManageWorkspace/index.js
@@ -175,7 +175,6 @@ const ManageWorkspace = () => {
                       </button>
                     </MenuDropdown>
                   )}
-
                 </div>
               </div>
             );
