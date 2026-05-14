f073e61f4238e54ab2191308143e5eb47cc40c77
Sid <siddharth@usebruno.com>
2026-05-14 20:51:12 +0530

    test: non serial

--- tests/transient-requests/transient-request-quit-flow.spec.ts
+++ tests/transient-requests/transient-request-quit-flow.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '../../playwright';
 import { createCollection, createTransientRequest, fillRequestUrl, closeAllCollections } from '../utils/page';
 import { buildCommonLocators } from '../utils/page/locators';
 
-test.describe.serial('Transient Requests - Quit Flow', () => {
+test.describe('Transient Requests - Quit Flow', () => {
   test('should open transient save modal when saving during app quit flow', async ({ page, electronApp, createTmpDir }) => {
     const locators = buildCommonLocators(page);
     const collectionPath = await createTmpDir('transient-quit-flow');
