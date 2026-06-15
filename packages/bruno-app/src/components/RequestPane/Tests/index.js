93ba26cd5e65745d6de66c98ac954a30d9198717
Pooja Belaramani <pooja@usebruno.com>
2026-06-15 12:58:57 +0530

    rm: lint error

--- packages/bruno-app/src/components/RequestPane/Tests/index.js
+++ packages/bruno-app/src/components/RequestPane/Tests/index.js
@@ -37,7 +37,7 @@ const Tests = ({ item, collection }) => {
     editorRef: testsEditorRef,
     scriptPhase: 'test'
   });
-  
+
   const requestContext = useMemo(() => buildRequestContextFromItem(item), [item]);
 
   return (
