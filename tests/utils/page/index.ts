85b23a297e5056a71e7f5f1ff1cabf19a671ffe3
aman-bruno <aman@usebruno.com>
2026-06-29 03:38:29 +0530

    moved locator,actions and made changes to match test steps

--- tests/utils/page/index.ts
+++ tests/utils/page/index.ts
@@ -8,6 +8,7 @@ export * from './locators';
 export * from './websocket';
 export * from './sidebar';
 export * from './git/clone-git-repository';
+export * from './collection';
 export * from './mounting';
 export * from './collection/migrate-to-yml';
 export * from './preferences';
