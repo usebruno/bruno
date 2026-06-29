b357d08baf8978d01471813dc3545c858c0643a4
aman-bruno <aman@usebruno.com>
2026-06-29 12:09:34 +0530

    added open collection testcase and few import testcases

--- tests/collection/create/create-collection.spec.ts
+++ tests/collection/create/create-collection.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '../../../playwright';
-import { closeAllCollections, createCollection, createRequest } from '../../utils/page';
+import { buildCommonLocators, closeAllCollections, createCollection } from '../../utils/page';
 
 test.describe('Create collection', () => {
   test.afterEach(async ({ page }) => {
@@ -53,27 +53,11 @@ test.describe('Create collection', () => {
     await createCollectionModal.getByRole('button', { name: 'Cancel' }).click();
   });
 
-  test('Create collection and add a simple HTTP request', async ({ page, createTmpDir }) => {
+  test('TC99: Verify user able to Create a new collection', { tag: '@sanity' }, async ({ page, createTmpDir }) => {
     const collectionName = 'test-collection';
-    const requestName = 'ping';
-
+    const locators = buildCommonLocators(page);
     await createCollection(page, collectionName, await createTmpDir(collectionName));
-
-    // Create a new request using the dialog/modal flow
-    await createRequest(page, requestName, collectionName);
-
-    // Set the URL
-    await page.locator('#request-url .CodeMirror').click();
-    await page.locator('#request-url').locator('textarea').fill('http://localhost:8081');
-    await page.locator('#request-actions').getByTitle('Save Request').click();
-
-    // Send a request
-    await page.locator('#request-url .CodeMirror').click();
-    await page.locator('#request-url').locator('textarea').fill('/ping');
-    await page.locator('#request-actions').getByTitle('Save Request').click();
-    await page.getByTestId('send-arrow-icon').click();
-
-    // Verify the response
-    await expect(page.getByRole('main')).toContainText('200 OK');
+    await expect(locators.toast.collectionCreated()).toBeVisible();
+    await expect(locators.sidebar.collection(collectionName)).toBeVisible();
   });
 });
