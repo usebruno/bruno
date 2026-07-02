0e4da6f0f94eb20e44283340ee2ccdd1ca517a56
ravindra-bruno <ravindra@usebruno.com>
2026-07-02 15:15:58 +0530

    Added the import

--- packages/bruno-app/src/components/AIAssist/index.spec.js
+++ packages/bruno-app/src/components/AIAssist/index.spec.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { ThemeProvider } from 'styled-components';
 import { aiGenerateScript, stopAiGeneration } from 'utils/ai';
+import { getPlatformModifierKey } from 'utils/common/platform';
 import AIAssist from './index';
 
 jest.mock('utils/ai', () => ({
