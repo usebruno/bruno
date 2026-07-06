4dd403b2897424321941186219c8cd43c16e5cda
guru-sharan-bruno <guru@usebruno.com>
2026-07-06 18:21:39 +0530

    test(OpenWorkspace): Automation script for Open workspace

---
+++ tests/utils/page/workspace/open-workspace.ts
@@ -0,0 +1,44 @@
+import path from 'path';
+import { ElectronApplication, Page, test } from '../../../../playwright';
+import { buildCommonLocators } from '../locators';
+import { buildTitleBarLocators } from '../title-bar';
+
+export const initUserDataPath = path.join(__dirname, '../../../workspace/create-workspace/init-user-data');
+export const WORKSPACE_NAME = 'my-workspace';
+
+export const buildOpenWorkspaceLocators = (page: Page) => ({
+  confirmTextContent: () => page.getByText('Workspace opened successfully', { exact: true })
+});
+
+type OpenWorkspaceDialogOptions = {
+  canceled: boolean;
+  filePaths: string[];
+};
+
+export const stubElectronOpenDialog = async (
+  app: ElectronApplication,
+  options: OpenWorkspaceDialogOptions
+) => {
+  await app.evaluate(({ dialog }, { canceled, filePaths }) => {
+    (dialog as { showOpenDialog: typeof dialog.showOpenDialog }).showOpenDialog = () =>
+      Promise.resolve({ canceled, filePaths });
+  }, options);
+};
+
+export const triggerOpenWorkspaceFromMenu = async (page: Page) => {
+  const commLocators = buildCommonLocators(page);
+  const titleBarLocators = buildTitleBarLocators(page);
+  await test.step('Open Workspace from My Workspace menu', async () => {
+    await titleBarLocators.workspaceMenuTrigger().click();
+    await commLocators.dropdown.item('Open workspace').click();
+  });
+};
+
+type OpenWorkspaceOptions = OpenWorkspaceDialogOptions & {
+  app: ElectronApplication;
+};
+
+export const openWorkspaceFromMenu = async (page: Page, opts: OpenWorkspaceOptions) => {
+  await stubElectronOpenDialog(opts.app, { canceled: opts.canceled, filePaths: opts.filePaths });
+  await triggerOpenWorkspaceFromMenu(page);
+};
