cb1948bd64e546fe3ac75060ffea8d8ceae265a9
Sachin Thakur <sachin.thakur@usebruno.com>
2026-07-30 09:52:45 +0530

    updated icons

--- packages/bruno-app/src/components/CollectionSettings/Presets/constants.jsx
+++ packages/bruno-app/src/components/CollectionSettings/Presets/constants.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import { IconWorld, IconBolt, IconCirclesRelation, IconPlugConnected } from '@tabler/icons';
+import { IconApi, IconBrandGraphql, IconCode, IconPlugConnected } from '@tabler/icons';
 import { PRESET_REQUEST_TYPES } from 'utils/common/constants';
 
 export const requestTypeItems = [
   {
     'value': PRESET_REQUEST_TYPES.HTTP,
     'label': 'HTTP',
-    'icon': <IconWorld size={15} strokeWidth={1.5} />,
+    'icon': <IconApi size={18} strokeWidth={1.5} />,
     'data-testid': 'presets-request-type-http'
   },
   {
     'value': PRESET_REQUEST_TYPES.GRAPHQL,
     'label': 'GraphQL',
-    'icon': <IconBolt size={15} strokeWidth={1.5} />,
+    'icon': <IconBrandGraphql size={16} strokeWidth={1.5} />,
     'data-testid': 'presets-request-type-graphql'
   },
   {
     'value': PRESET_REQUEST_TYPES.GRPC,
     'label': 'gRPC',
-    'icon': <IconCirclesRelation size={15} strokeWidth={1.5} />,
+    'icon': <IconCode size={16} strokeWidth={1.5} />,
     'data-testid': 'presets-request-type-grpc'
   },
   {
     'value': PRESET_REQUEST_TYPES.WS,
     'label': 'WebSocket',
-    'icon': <IconPlugConnected size={15} strokeWidth={1.5} />,
+    'icon': <IconPlugConnected size={16} strokeWidth={1.5} />,
     'data-testid': 'presets-request-type-ws'
   }
 ];
