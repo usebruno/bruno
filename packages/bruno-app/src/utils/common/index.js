d5613ad287447ecb58cb6e7639d25a93b62de620
abhishekp-bruno <abhishek.p@usebruno.com>
2026-08-04 17:08:42 +0530

    fix(proxy): update timestamp label format and enhance timestamp utility function

--- packages/bruno-app/src/utils/common/index.js
+++ packages/bruno-app/src/utils/common/index.js
@@ -556,12 +556,14 @@ export function isHexFormat(str) {
 }
 
 export const formatProxyTimestamp = (timestamp) => {
-  return new Date(timestamp).toLocaleString('en-GB', {
-    day: '2-digit',
-    month: 'short',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: true
-  });
+  return new Date(timestamp)
+    .toLocaleString('en-GB', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true
+    })
+    .replace(/\b(a|p)m\b/gi, (match) => match.toUpperCase());
 };
