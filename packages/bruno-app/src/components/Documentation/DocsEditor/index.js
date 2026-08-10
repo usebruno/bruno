d9ef7c8118835da14b14f97626861adf44e267fc
Gopi-bruno <gopi@usebruno.com>
2026-08-10 17:27:32 +0530

    refactor: remove AI documentation assistant and related context payload generation from documentation components

--- packages/bruno-app/src/components/Documentation/DocsEditor/index.js
+++ packages/bruno-app/src/components/Documentation/DocsEditor/index.js
@@ -3,7 +3,6 @@ import { useTheme } from 'providers/Theme';
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import CodeEditor from 'components/CodeEditor';
-import AIAssist from 'components/AIAssist';
 import RichTextEditor from 'ui/RichTextEditor';
 import ModeSwitch from 'components/ModeSwitch';
 import { useEditor } from '@tiptap/react';
@@ -22,9 +21,6 @@ const DocsEditor = ({
   isEditing,
   collection,
   collectionPath,
-  requestContext,
-  docsContext,
-  variables,
   emptyPreviewContent,
   onRequestEdit,
   initialScroll,
@@ -167,14 +163,6 @@ const DocsEditor = ({
             mode="gfm"
             initialScroll={initialScroll}
             onScroll={onScroll}
-          />
-          <AIAssist
-            scriptType="docs"
-            currentScript={docs || ''}
-            requestContext={requestContext}
-            docsContext={docsContext}
-            variables={variables}
-            onApply={onEdit}
           />
         </div>
       )}
