3d95e837b1e0af5656af8438ccc09c14b1e91817
utkarsh-bruno <utkarsh@usebruno.com>
2026-08-13 17:31:35 +0530

    fix: spelling error on a comment

--- packages/bruno-electron/src/app/mock-server/mock-server.js
+++ packages/bruno-electron/src/app/mock-server/mock-server.js
@@ -398,7 +398,7 @@ const handleRequest = (mockServerUid, req, res) => {
     }
   };
 
-  // addign a try-catch block to handle any errors that may occur during the response
+  // adding a try-catch block to handle any errors that may occur during the response
   if (delay > 0) {
     setTimeout(() => {
       try {
