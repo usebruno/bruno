1f853dc54a2ad6202a4c8d992a0cd6f7226687dc
utkarsh-bruno <utkarsh@usebruno.com>
2026-08-25 18:05:10 +0530

    fix: fixing duplicate variables

--- packages/bruno-app/src/components/MockServer/MockServerDashboard/index.js
+++ packages/bruno-app/src/components/MockServer/MockServerDashboard/index.js
@@ -106,11 +106,6 @@ const MockServerDashboard = ({ instance, collection }) => {
     .map((i) => Number(i.port))
     .join(',');
 
-  const conflictingPortsKey = workspaceInstances
-    .filter((i) => i.uid !== storedInstance.uid)
-    .map((i) => Number(i.port))
-    .join(',');
-
   useEffect(() => {
     let isCurrent = true;
     resolvePortError(activePort).then((error) => {
