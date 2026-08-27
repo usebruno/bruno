2300ef50aa6f5d5c6abc9f26af619aa3cec03037
sanish-bruno <sanish@usebruno.com>
2026-08-27 09:43:28 +0530

    feat(translator): add global variable translation methods for Postman to Bruno conversion

--- packages/bruno-converters/src/utils/postman-to-bruno-translator.js
+++ packages/bruno-converters/src/utils/postman-to-bruno-translator.js
@@ -150,6 +150,10 @@ const simpleTranslations = {
   'pm.setEnvironmentVariable': 'bru.setEnvVar',
   'pm.getEnvironmentVariable': 'bru.getEnvVar',
   'pm.clearEnvironmentVariable': 'bru.deleteEnvVar',
+  'pm.setGlobalVariable': 'bru.setGlobalEnvVar',
+  'pm.getGlobalVariable': 'bru.getGlobalEnvVar',
+  'pm.clearGlobalVariable': 'bru.deleteGlobalEnvVar',
+  'pm.clearGlobalVariables': 'bru.deleteAllGlobalEnvVars',
 
   // Legacy response properties
   'responseCode.code': 'res.getStatus()',
