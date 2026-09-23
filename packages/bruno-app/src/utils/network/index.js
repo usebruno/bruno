00d76a0d2256681c7b4aabd54a1a4f6519d319b2
Shaharyar Amjad <shaharyaramjad@Shaharyars-MacBook-Pro.local>
2026-09-23 18:57:57 +0500

    fix: show error when JSON body is invalid before sending request (#8215)

--- packages/bruno-app/src/utils/network/index.js
+++ packages/bruno-app/src/utils/network/index.js
@@ -1,6 +1,21 @@
 export const sendNetworkRequest = async (item, collection, environment, runtimeVariables) => {
   return new Promise((resolve, reject) => {
     if (['http-request', 'graphql-request'].includes(item.type)) {
+      // Validate JSON body before sending
+      const bodyMode = item.draft?.request?.body?.mode ?? item.request?.body?.mode;
+      const bodyJson = item.draft?.request?.body?.json ?? item.request?.body?.json;
+
+      if (bodyMode === 'json' && bodyJson && bodyJson.trim() !== '') {
+        try {
+          const stripped = bodyJson
+            .replace(/\/\/.*$/gm, '')
+            .replace(/\/\*[\s\S]*?\*\//g, '');
+          JSON.parse(stripped);
+        } catch (e) {
+          return reject(new Error(`Invalid JSON in request body: ${e.message}`));
+        }
+      }
+
       sendHttpRequest(item, collection, environment, runtimeVariables)
         .then((response) => {
           // if there is an error, we return the response object as is
