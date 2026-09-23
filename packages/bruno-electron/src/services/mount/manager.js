8efe082a915c8fe68e17e2772c4f6ebe971b7311
Sid <siddharth@usebruno.com>
2026-09-23 13:02:49 +0530

    feat: force close db (#9334)

--- packages/bruno-electron/src/services/mount/manager.js
+++ packages/bruno-electron/src/services/mount/manager.js
@@ -133,11 +133,11 @@ class MountManager {
     } catch (_) {}
   }
 
-  async shutdown() {
+  async shutdown({ force = false } = {}) {
     await Promise.all(
       Array.from(this.#mounts.keys()).map((uid) => this.unmount(uid).catch(() => {}))
     );
-    await destroyPool().catch(() => {});
+    await destroyPool({ force }).catch(() => {});
     if (this.#index) {
       this.#index.close();
       this.#index = null;
