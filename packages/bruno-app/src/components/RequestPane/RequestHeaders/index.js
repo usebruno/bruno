7efd3af3f67cee0c6e1dce2d32b8b78b49197900
Pooja Belaramani <pooja@usebruno.com>
2026-09-23 11:14:24 +0530

    fix

--- packages/bruno-app/src/components/RequestPane/RequestHeaders/index.js
+++ packages/bruno-app/src/components/RequestPane/RequestHeaders/index.js
@@ -211,7 +211,7 @@ const RequestHeaders = ({ item, collection, addHeaderText }) => {
         uid: 'inherited-headers-section',
         rowType: ROW_TYPE.SECTION,
         section: ROW_TYPE.INHERITED,
-        label: 'Runtime & Inherited Headers',
+        label: 'Inherited & Runtime Headers',
         count: allInheritedHeaders.length,
         expanded: isInheritedHeadersExpanded
       },
