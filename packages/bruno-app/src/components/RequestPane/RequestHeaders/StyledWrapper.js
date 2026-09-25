5c239faa642ae34670b13aab0d0b4d1bdce45dfa
Pooja Belaramani <pooja@usebruno.com>
2026-09-25 10:57:02 +0530

    fix(headers): allow long runtime header values to scroll

--- packages/bruno-app/src/components/RequestPane/RequestHeaders/StyledWrapper.js
+++ packages/bruno-app/src/components/RequestPane/RequestHeaders/StyledWrapper.js
@@ -52,13 +52,23 @@ const Wrapper = styled.div`
 
   .default-header-row,
   .inherited-header-row {
-    .default-header-value {
-      font-style: italic;
-      color: ${(props) => props.theme.colors.text.muted};
-    }
-
     input[type='checkbox']:disabled {
       opacity: 0.55;
+    }
+  }
+
+  .default-header-row {
+    .default-header-value {
+      display: block;
+      overflow-x: auto;
+      white-space: nowrap;
+      text-overflow: clip;
+      scrollbar-width: none;
+      color: ${(props) => props.theme.colors.text.muted};
+
+      &::-webkit-scrollbar {
+        display: none;
+      }
     }
   }
 
